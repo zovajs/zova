76b264caea72af30054667de1cea171b308676cc
zhennann <zhen.nann@icloud.com>
2024-12-25 11:09:24 +0800

    feat: home-api

---
+++ zova-dev/src/suite/a-home/modules/home-api/src/.metadata/index.ts
@@ -0,0 +1,21 @@
+/** service: begin */
+import service_book from '../service/book.js';
+export const services = {
+  book: service_book,
+};
+/** service: end */
+/** scope: begin */
+import { BeanScopeBase, Scope, TypeModuleResource } from 'zova';
+
+@Scope()
+export class ScopeModuleHomeApi extends BeanScopeBase {}
+
+export interface ScopeModuleHomeApi extends TypeModuleResource<never, never, never, never, typeof services> {}
+
+import 'zova';
+declare module 'zova' {
+  export interface IBeanScopeRecord {
+    'home-api': ScopeModuleHomeApi;
+  }
+}
+/** scope: end */
