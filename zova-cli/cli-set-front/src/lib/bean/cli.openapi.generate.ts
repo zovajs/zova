748d6b98c016fdf38a614baf74d5aee67c558ac7
zhennann <zhen.nann@icloud.com>
2024-12-25 21:33:06 +0800

    feat: generate

--- zova-cli/cli-set-front/src/lib/bean/cli.openapi.generate.ts
+++ zova-cli/cli-set-front/src/lib/bean/cli.openapi.generate.ts
@@ -4,6 +4,7 @@ import { __ThisSetName__ } from '../this.js';
 import path from 'node:path';
 import openapiTS, { astToString, OpenAPITSOptions } from 'openapi-typescript';
 import ts from 'typescript';
+import { toLowerCaseFirstChar } from '@cabloy/word-utils';
 
 declare module '@cabloy/cli' {
   interface ICommandArgv {}
@@ -31,6 +32,47 @@ export class CliOpenapiGenerate extends BeanCliBase {
   }
 
   async _generate(ast: ts.Node[]) {
-    console.log(ast);
+    const { argv } = this.context;
+    const nodeServices = this._getNodeServices(ast);
+    if (!nodeServices) return;
+    for (const serviceName in nodeServices) {
+      const serviceNameLower = toLowerCaseFirstChar(serviceName);
+      const nodeService = nodeServices[serviceName];
+      const serviceFile = path.join(
+        argv.projectPath,
+        `src/suite/a-home/modules/home-api/src/service/${serviceNameLower}.ts`,
+      );
+      const serviceContent = this._generateService(ast, serviceName, nodeService);
+      await fse.outputFile(serviceFile, serviceContent);
+      await this.helper.formatFile({ fileName: serviceFile });
+    }
+  }
+
+  _generateService(_ast: ts.Node[], _serviceName: string, _nodeService: Record<string, ts.PropertySignature>) {
+    const serviceContent = "import { ZovaApplication } from 'zova';";
+    return serviceContent;
+  }
+
+  _getNodeServices(ast: ts.Node[]) {
+    const nodeOperations = ast.find(
+      node => ts.isInterfaceDeclaration(node) && node.name.escapedText === 'operations',
+    ) as ts.InterfaceDeclaration | undefined;
+    if (!nodeOperations) return;
+    const services: Record<string, Record<string, ts.PropertySignature>> = {};
+    for (let index = 0; index < nodeOperations.members.length; index++) {
+      const node = nodeOperations.members[index];
+      if (!ts.isPropertySignature(node)) continue;
+      const operationId = (<ts.Identifier>node.name).text;
+      let [service, method] = operationId.toString().split('_');
+      if (!method) {
+        method = service;
+        service = 'default';
+      }
+      if (!services[service]) {
+        services[service] = {};
+      }
+      services[service][method] = node;
+    }
+    return services;
   }
 }
---
+++ zova-dev/src/suite/a-home/modules/home-api/src/service/onion copy.ts
@@ -0,0 +1,33 @@
+import { ZovaApplication } from 'zova';
+import type { paths } from './_openapi_.js';
+
+export type ServiceOnionEcho2Method = 'post';
+export type ServiceOnionEcho2Path = '/api/vona/test/onion/echo2/{userId}/{userName}';
+export type ServiceOnionEcho2RequestParams =
+  paths[ServiceOnionEcho2Path][ServiceOnionEcho2Method]['parameters']['path'];
+export type ServiceOnionEcho2RequestQuery =
+  paths[ServiceOnionEcho2Path][ServiceOnionEcho2Method]['parameters']['query'];
+export type ServiceOnionEcho2RequestHeaders =
+  paths[ServiceOnionEcho2Path][ServiceOnionEcho2Method]['parameters']['header'];
+export type ServiceOnionEcho2RequestBody =
+  paths[ServiceOnionEcho2Path][ServiceOnionEcho2Method]['requestBody']['content']['application/json'];
+export type ServiceOnionEcho2ResponseBody =
+  paths[ServiceOnionEcho2Path][ServiceOnionEcho2Method]['responses']['200']['content']['application/json']['data'];
+
+export default (app: ZovaApplication) => {
+  return {
+    echo2: (
+      body: ServiceOnionEcho2RequestBody,
+      options?: {
+        params?: ServiceOnionEcho2RequestParams;
+        query?: ServiceOnionEcho2RequestQuery;
+        headers?: ServiceOnionEcho2RequestHeaders;
+      },
+    ) =>
+      app.meta.$api.post<any, ServiceOnionEcho2ResponseBody>(
+        app.util.apiTranslatePath('/api/vona/test/onion/echo2/{userId}/{userName}', options?.params),
+        body,
+        app.util.apiInvokeConfig(options),
+      ),
+  };
+};
