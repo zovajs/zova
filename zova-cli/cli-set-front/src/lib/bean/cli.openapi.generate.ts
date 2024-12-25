f27f45d60ef6e522aaf03bef4d222146447ce364
zhennann <zhen.nann@icloud.com>
2024-12-25 15:42:33 +0800

    Update cli.openapi.generate.ts

--- zova-cli/cli-set-front/src/lib/bean/cli.openapi.generate.ts
+++ zova-cli/cli-set-front/src/lib/bean/cli.openapi.generate.ts
@@ -3,6 +3,7 @@ import fse from 'fs-extra';
 import { __ThisSetName__ } from '../this.js';
 import path from 'node:path';
 import openapiTS, { astToString, OpenAPITSOptions } from 'openapi-typescript';
+import ts from 'typescript';
 
 declare module '@cabloy/cli' {
   interface ICommandArgv {}
@@ -22,7 +23,13 @@ export class CliOpenapiGenerate extends BeanCliBase {
       const config = (await instance.default()) as { source: string; options: OpenAPITSOptions };
       const ast = await openapiTS(config.source, config.options);
       const contents = astToString(ast);
-      console.log(contents);
+      const outputFile = path.join(argv.projectPath, 'src/suite/a-home/modules/home-api/src/service/.openapi.ts');
+      await fse.outputFile(outputFile, contents);
+      await this._generate(ast);
     });
+  }
+
+  async _generate(ast: ts.Node[]) {
+    console.log(ast);
   }
 }
