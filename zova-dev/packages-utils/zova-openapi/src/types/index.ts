7603c20ae737a202fb65dfbfb9f37848b4830ac9
zhennann <zhen.nann@icloud.com>
2024-12-27 17:20:42 +0800

    feat: openapi

--- zova-dev/packages-utils/zova-openapi/src/types/index.ts
+++ zova-dev/packages-utils/zova-openapi/src/types/index.ts
@@ -1,20 +1,14 @@
-export type ZovaMetaFlavor = 'front' | 'admin' | keyof ZovaMetaFlavorExtend;
-export type ZovaMetaMode = 'development' | 'production';
-export type ZovaMetaAppMode =
-  | 'spa'
-  | 'ssr'
-  | 'pwa'
-  | 'cordova'
-  | 'capacitor'
-  | 'electron'
-  | 'bex'
-  | keyof ZovaMetaAppModeExtend;
+import { OpenAPITSOptions } from 'openapi-typescript';
 
-export interface ZovaConfigMeta {
-  flavor: ZovaMetaFlavor;
-  mode: ZovaMetaMode;
-  appMode: ZovaMetaAppMode;
+export type TypeOpenapiConfigMatchRule = string | RegExp | (string | RegExp)[];
+
+export interface ZovaOpenapiConfigModule {
+  source: string;
+  options?: OpenAPITSOptions;
+  match?: TypeOpenapiConfigMatchRule;
+  ignore?: TypeOpenapiConfigMatchRule;
 }
 
-export interface ZovaMetaFlavorExtend {}
-export interface ZovaMetaAppModeExtend {}
+export interface ZovaOpenapiConfig {
+  modules: Record<string, ZovaOpenapiConfigModule>;
+}
