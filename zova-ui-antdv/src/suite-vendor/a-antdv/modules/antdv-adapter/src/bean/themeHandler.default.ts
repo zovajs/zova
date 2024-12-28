113fee856f093a5875369051d6d7f6e1e768e690
zhennann <zhen.nann@icloud.com>
2024-12-28 16:57:20 +0800

    feat: update

--- zova-ui-antdv/src/suite-vendor/a-antdv/modules/antdv-adapter/src/bean/themeHandler.default.ts
+++ zova-ui-antdv/src/suite-vendor/a-antdv/modules/antdv-adapter/src/bean/themeHandler.default.ts
@@ -1,8 +1,8 @@
-import { BeanBase, ThemeHandle } from 'zova';
+import { BeanBase, ThemeHandler } from 'zova';
 import { ScopeModule } from '../.metadata/this.js';
 import { IThemeHandler, IThemeHandlerApplyParams } from 'zova-module-a-style';
 
-@ThemeHandle()
+@ThemeHandler()
 export class ThemeHandlerDefault extends BeanBase<ScopeModule> implements IThemeHandler {
   async apply({ token: _token }: IThemeHandlerApplyParams): Promise<void> {
     // do nothing
