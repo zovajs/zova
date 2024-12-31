b661aff81662b75c7d523998e7b4aa5d21ba2e11
zhennann <zhen.nann@icloud.com>
2024-12-31 17:51:43 +0800

    Update index.ts

--- zova-dev/packages-utils/babel-plugin-zova-bean-module/src/index.ts
+++ zova-dev/packages-utils/babel-plugin-zova-bean-module/src/index.ts
@@ -2,51 +2,24 @@ import { /*template,*/ types as t, type PluginPass } from '@babel/core';
 import type { NodePath, Visitor } from '@babel/traverse';
 import { parseInfoFromPath } from '@cabloy/module-info';
 
-const __Decorator_Supported = [
-  'Bean',
-  'Local',
-  'Scope',
-  'Store',
-  'Data',
-  'Model',
-  'Style',
-  'Service',
-  'Theme',
-  'ThemeHandler',
-  'Tool',
-  'Virtual',
-];
-
 export default function () {
   const visitor: Visitor<PluginPass> = {
     ClassDeclaration(path: NodePath<t.ClassDeclaration>, state) {
-      let moduleName: string | undefined;
-      function getModuleName() {
-        if (moduleName) return moduleName;
-        const sourceFileName = state.file.opts.sourceFileName || state.file.opts.filename;
-        if (!__checkIfValid(sourceFileName)) return;
-        const moduleInfo = parseInfoFromPath(sourceFileName);
-        if (!moduleInfo) return;
-        moduleName = moduleInfo.relativeName;
-        return moduleName;
-      }
       const decorators = path.node.decorators;
       if (!decorators || decorators.length === 0) return;
-      for (const decorator of decorators) {
-        if (t.isCallExpression(decorator.expression)) {
-          const expression: t.CallExpression = decorator.expression;
-          const decoratorName = (<any>expression.callee).name;
-          if (__Decorator_Supported.includes(decoratorName)) {
-            const moduleName = getModuleName();
-            if (moduleName) {
-              __applyDecorator(expression, moduleName);
-            }
-          }
-        }
-      }
+      const sourceFileName = state.file.opts.sourceFileName || state.file.opts.filename;
+      const moduleName = _getModuleName(sourceFileName);
+      if (!moduleName) return;
     },
   };
   return { visitor };
+}
+
+function _getModuleName(sourceFileName: string | null | undefined) {
+  if (!__checkIfValid(sourceFileName)) return;
+  const moduleInfo = parseInfoFromPath(sourceFileName);
+  if (!moduleInfo) return;
+  return moduleInfo.relativeName;
 }
 
 function __applyDecorator(expression: t.CallExpression, moduleName: string) {
