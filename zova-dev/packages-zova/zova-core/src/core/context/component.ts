c4035bd5eff9a937aa962c2dbdc2665e47408887
zhennann <zhen.nann@icloud.com>
2025-01-04 19:46:59 +0800

    Update component.ts

--- zova-dev/packages-zova/zova-core/src/core/context/component.ts
+++ zova-dev/packages-zova/zova-core/src/core/context/component.ts
@@ -1,6 +1,5 @@
-import { BeanRenderBase } from '../../bean/beanRenderBase.js';
 import { BeanSimple } from '../../bean/beanSimple.js';
-import { BeanRenderIdentifier } from '../../bean/type.js';
+import { BeanControllerIdentifier, BeanRenderIdentifier } from '../../bean/type.js';
 import { cast } from '../../types/utils/cast.js';
 
 export class CtxComponent extends BeanSimple {
@@ -16,7 +15,10 @@ export class CtxComponent extends BeanSimple {
       if (!self.ctx.meta.state.inited.state) {
         return self._bean_render_original.call(this, ...args);
       }
-      const render: BeanRenderBase = self.ctx.bean._getBeanSyncOnly(BeanRenderIdentifier);
+      // todo: BeanControllerPageBase/BeanControllerBase需要增加render方法的类型定义
+      // protected render()
+      // BeanRenderBase不需要render方法的定义
+      const render = self._getRender();
       if (!render) {
         return self._bean_render_original.call(this, ...args);
         // throw new Error('render bean not found');
@@ -33,5 +35,12 @@ export class CtxComponent extends BeanSimple {
     const instance = cast(this.ctx.instance);
     instance[renderMethod] = this._bean_render_original;
     this._bean_render_original = null;
+  }
+
+  private _getRender() {
+    const render = this.bean._getBeanSyncOnly<any>(BeanControllerIdentifier);
+    if (!render) return;
+    if (render.render) return render;
+    return this.bean._getBeanSyncOnly(BeanRenderIdentifier);
   }
 }
