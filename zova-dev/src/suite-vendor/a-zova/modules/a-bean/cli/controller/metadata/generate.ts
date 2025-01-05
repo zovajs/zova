7b8e1c72160faee3e42dd60d8cb59a321f6f1734
zhennann <zhen.nann@icloud.com>
2025-01-05 15:42:35 +0800

    feat: update

--- zova-dev/src/suite-vendor/a-zova/modules/a-bean/cli/controller/metadata/generate.ts
+++ zova-dev/src/suite-vendor/a-zova/modules/a-bean/cli/controller/metadata/generate.ts
@@ -3,22 +3,8 @@ import { IGlobBeanFile, IMetadataCustomGenerateOptions } from '@cabloy/module-in
 import { toUpperCaseFirstChar } from '@cabloy/word-utils';
 import path from 'node:path';
 import { BeanCliBase } from '@cabloy/cli';
-
-interface IControllerInfo {
-  type: string;
-  name: string;
-  nameCapitalize: string;
-  nameProps: string;
-  hasProps: boolean;
-  nameEmits: string;
-  hasEmits: boolean;
-  nameSlots: string;
-  hasSlots: boolean;
-  fileRender: string;
-  importRender: string;
-  fileStyle: string;
-  importStyle: string;
-}
+import { IControllerInfo } from './types.js';
+import { generateMetaComponent } from './generateMetaComponent.js';
 
 export default async function (options: IMetadataCustomGenerateOptions): Promise<string> {
   const { globFiles } = options;
@@ -36,80 +22,13 @@ export default async function (options: IMetadataCustomGenerateOptions): Promise
     }
   }
   const contentMetaPage = _generateMetaPage(options, globFilesPage);
-  const contentMetaComponent = _generateMetaComponent(options, globFilesComponent);
+  const contentMetaComponent = generateMetaComponent(options, globFilesComponent);
   const content = `${contentMetaPage}\n${contentMetaComponent}`;
   return content;
 }
 
 function _generateMetaPage(_options: IMetadataCustomGenerateOptions, _globFiles: [IGlobBeanFile, IControllerInfo][]) {
   return '// -- page -- ';
-}
-
-function _generateMetaComponent(
-  options: IMetadataCustomGenerateOptions,
-  globFiles: [IGlobBeanFile, IControllerInfo][],
-) {
-  const { moduleName } = options;
-  const contentImports: string[] = [];
-  const contentImports2: string[] = [];
-  const contentComponents: string[] = [];
-  const contentRecords: string[] = [];
-  const contentRecords2: string[] = [];
-  for (const [globFile, controllerInfo] of globFiles) {
-    const { className } = globFile;
-    const { name, nameCapitalize, nameProps, hasProps, nameEmits, hasEmits, nameSlots, hasSlots } = controllerInfo;
-    const componentFullName = `${moduleName}:${name}`;
-    const componentName2 = 'Z' + nameCapitalize;
-    const _contentImports_parts: string[] = [];
-    if (hasEmits) _contentImports_parts.push(nameEmits);
-    if (hasSlots) _contentImports_parts.push(nameSlots);
-    if (_contentImports_parts.length > 0) {
-      contentImports.push(`import { ${_contentImports_parts.join(', ')} } from '../component/${name}/controller.jsx';`);
-    }
-    contentImports2.push(`export { default as ${componentName2} } from './component/${name}.vue';`);
-    contentImports2.push(`import { default as ${componentName2} } from './component/${name}.vue';`);
-    contentComponents.push(`'${name}': ${componentName2},`);
-    contentRecords.push(`'${componentFullName}': ${className};`);
-    if (hasProps) {
-      contentRecords2.push(`export interface ${nameProps} {
-        controllerRef?: (ref: ${className}) => void;
-        ${hasSlots ? `slots?: ${nameSlots};` : ''} 
-      }
-      `);
-    }
-    const _contentRecords_parts: string[] = [];
-    if (hasProps)
-      _contentRecords_parts.push(`$props: RequiredSome<${nameProps}, keyof typeof ${className}.$propsDefault>;`);
-    if (hasEmits) _contentRecords_parts.push(`$emit: ${nameEmits};`);
-    if (hasSlots) _contentRecords_parts.push(`$slots: ${nameSlots};`);
-    if (_contentRecords_parts.length > 0) {
-      contentRecords2.push(`export interface ${className} {
-        ${_contentRecords_parts.join('\n')}
-      }`);
-    }
-  }
-  // combine
-  let content = `/** components: begin */
-${contentImports.join('\n')}
-${contentImports2.join('\n')}
-export const components = {
-  ${contentComponents.join('\n')}
-};
-import 'zova';
-declare module 'zova' {
-export interface IComponentRecord {
-  ${contentRecords.join('\n')}
-}
-}
-declare module 'zova-module-${moduleName}' {
-  ${contentRecords2.join('\n')} 
-}  
-/** components: end */
-`;
-  if (content.includes('RequiredSome')) {
-    content = `import { RequiredSome } from 'zova';\n${content}`;
-  }
-  return content;
 }
 
 async function _generateFileVue(
---
+++ zova-dev/src/suite-vendor/a-zova/modules/a-bean/cli/controller/metadata/types.ts
@@ -0,0 +1,15 @@
+export interface IControllerInfo {
+  type: string;
+  name: string;
+  nameCapitalize: string;
+  nameProps: string;
+  hasProps: boolean;
+  nameEmits: string;
+  hasEmits: boolean;
+  nameSlots: string;
+  hasSlots: boolean;
+  fileRender: string;
+  importRender: string;
+  fileStyle: string;
+  importStyle: string;
+}
