39a23dc88ff6ae83c57aaa8c437144489f1cb7ef
zhennann <zhen.nann@icloud.com>
2025-01-06 21:13:24 +0800

    Update 2-controller.cjs

--- zova-cli/cli-set-front/cli/templates/refactor/componentProps/snippets/2-controller.cjs
+++ zova-cli/cli-set-front/cli/templates/refactor/componentProps/snippets/2-controller.cjs
@@ -3,37 +3,13 @@ module.exports = {
     return argv.controllerFileName;
   },
   parseOptions: { language: 'plain' },
-  async transform({ cli, ast }) {
-    if (ast.includes('export interface Props')) throw new Error('Props exists');
-    const matchController = ast.match(/export class ([^< ]*)(.*?) extends/);
-    const className = matchController[1];
-    const hasGeneric = !!matchController[2];
-    const genericT = hasGeneric ? '<T>' : '';
-    const genericT2 = hasGeneric ? '<_T>' : '';
-    const hasSlots = ast.includes('export interface Slots');
-    // PropsBase
-    if (!ast.match(/import \{[^\}]*PropsBase[^\}]*\} from 'zova';/)) {
-      ast = ast.replace(/import \{ ([^\}]*) \} from 'zova';/, (_, $1) => {
-        return `import { ${$1}, PropsBase } from 'zova';`;
-      });
-    }
-    // RequiredSome
-    if (!ast.match(/import \{[^\}]*RequiredSome[^\}]*\} from 'zova';/)) {
-      ast = ast.replace(/import \{ ([^\}]*) \} from 'zova';/, (_, $1) => {
-        return `import { ${$1}, RequiredSome } from 'zova';`;
-      });
-    }
+  async transform({ ast, argv }) {
+    if (ast.includes(`${argv.controllerClassName}Props`)) throw new Error('Props exists');
+    const matchGeneric = ast.match(/interface [^<]*Props<(.*?)> \{/);
+    const hasGeneric = !!matchGeneric;
+    const genericT = hasGeneric ? '<T = unknown>' : '';
     // Props
-    ast = ast.replace(
-      '@Local',
-      `export interface Props${hasSlots ? genericT : genericT2} extends PropsBase<${className}${genericT}${hasSlots ? `, Slots${genericT}` : ''}> {}\n\n@Local`,
-    );
-    // BeanControllerBase
-    ast = ast.replace(/BeanControllerBase<([\s\S]*?)> \{/, (_, $1) => {
-      const parts = cli.helper.safeSplit($1, ',');
-      parts[1] = ` RequiredSome<Props${genericT}, keyof typeof ${className}.$propsDefault>`;
-      return `BeanControllerBase<${parts.join(',')}> {\n  static $propsDefault = {};\n\n`;
-    });
+    ast = ast.replace('@Controller', `export interface ${argv.controllerClassName}Props${genericT} {}\n\n@Controller`);
     // ok
     return ast;
   },
