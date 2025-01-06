4be5d7aaaf52b7e0c3b3a577abeb3e32116b3fea
zhennann <zhen.nann@icloud.com>
2025-01-06 17:59:12 +0800

    feat: update

--- zova-dev/src/suite/a-demo/modules/demo-basic/src/page/pinia/controller.tsx
+++ zova-dev/src/suite/a-demo/modules/demo-basic/src/page/pinia/controller.tsx
@@ -1,6 +1,7 @@
 import { BeanControllerPageBase, Use } from 'zova';
 import { Controller } from 'zova-module-a-bean';
 import { StoreCounter } from '../../bean/store.counter.js';
+import { ZPage } from 'zova-module-home-base';
 
 @Controller()
 export class ControllerPagePinia extends BeanControllerPageBase {
@@ -8,4 +9,22 @@ export class ControllerPagePinia extends BeanControllerPageBase {
   $$counter: StoreCounter;
 
   protected async __init__() {}
+
+  protected render() {
+    return (
+      <ZPage>
+        <div>{`count: ${this.$$counter.count}`}</div>
+        <div>{`doubleCount: ${this.$$counter.doubleCount}`}</div>
+        <div>{`name: ${this.$$counter.name}`}</div>
+        <button
+          class="btn btn-primary"
+          onClick={() => {
+            this.$$counter.increment();
+          }}
+        >
+          Increment
+        </button>
+      </ZPage>
+    );
+  }
 }
