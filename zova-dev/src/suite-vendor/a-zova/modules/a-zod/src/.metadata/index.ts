e1128169edbd87b684787ccb02f2326853c9eab8
zhennann <zhen.nann@icloud.com>
2025-01-07 15:05:34 +0800

    feat: zod error

--- zova-dev/src/suite-vendor/a-zova/modules/a-zod/src/.metadata/index.ts
+++ zova-dev/src/suite-vendor/a-zova/modules/a-zod/src/.metadata/index.ts
@@ -1,8 +1,16 @@
+/** locale: begin */
+import locale_en_us from '../config/locale/en-us.js';
+import locale_zh_cn from '../config/locale/zh-cn.js';
+export const locales = {
+  'en-us': locale_en_us,
+  'zh-cn': locale_zh_cn,
+};
+/** locale: end */
 /** main: begin */
 export * from '../main.js';
 /** main: end */
 /** scope: begin */
-import { BeanScopeBase, BeanScopeUtil } from 'zova';
+import { BeanScopeBase, BeanScopeUtil, TypeModuleLocales, TypeLocaleBase } from 'zova';
 import { Scope } from 'zova-module-a-bean';
 
 @Scope()
@@ -10,6 +18,7 @@ export class ScopeModuleAZod extends BeanScopeBase {}
 
 export interface ScopeModuleAZod {
   util: BeanScopeUtil;
+  locale: TypeModuleLocales<(typeof locales)[TypeLocaleBase]>;
 }
 
 import 'zova';
@@ -17,6 +26,13 @@ declare module 'zova' {
   export interface IBeanScopeRecord {
     'a-zod': ScopeModuleAZod;
   }
+
+  export interface IBeanScopeLocale {
+    'a-zod': (typeof locales)[TypeLocaleBase];
+  }
 }
 
+export function locale<K extends keyof (typeof locales)[TypeLocaleBase]>(key: K): `a-zod::${K}` {
+  return `a-zod::${key}`;
+}
 /** scope: end */
