11eb601faf038562d2b3760fcd9c1bcb6ee03bf8
zhennann <zhen.nann@icloud.com>
2025-01-07 14:45:56 +0800

    Update errorsAdapter.ts

--- zova-dev/src/suite-vendor/a-zova/modules/a-zod/src/lib/zod/errorsAdapter.ts
+++ zova-dev/src/suite-vendor/a-zova/modules/a-zod/src/lib/zod/errorsAdapter.ts
@@ -3,9 +3,9 @@ import { setErrorMapDefault, setErrorMapSchema } from '@cabloy/zod-errors-custom
 
 export function errorsAdapter(app: ZovaApplication) {
   setErrorMapDefault((text: string, ...args: any[]) => {
-    return app.text(text, ...args);
+    return app.meta.text(text, ...args);
   });
   setErrorMapSchema((text: string, ...args: any[]) => {
-    return app.text(text, ...args);
+    return app.meta.text(text, ...args);
   });
 }
