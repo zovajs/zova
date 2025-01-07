0fa592a7d3c8155c855c15a16f23b5eb0918b4b2
zhennann <zhen.nann@icloud.com>
2025-01-07 21:50:43 +0800

    Update beanBaseSimple.ts

--- zova-dev/packages-zova/zova-core/src/bean/beanBaseSimple.ts
+++ zova-dev/packages-zova/zova-core/src/bean/beanBaseSimple.ts
@@ -3,28 +3,21 @@ import { StateLock } from '../utils/stateLock.js';
 import { BeanSimple } from './beanSimple.js';
 
 export const SymbolBeanFullName = Symbol('SymbolBeanFullName');
-export const SymbolModuleBelongInner = Symbol('SymbolModuleBelongInner');
 export const SymbolModuleBelong = Symbol('SymbolModuleBelong');
 export const SymbolModuleName = Symbol('SymbolModuleName');
 export const SymbolInited = Symbol('SymbolInited');
 
 export class BeanBaseSimple extends BeanSimple {
   private [SymbolBeanFullName]: string;
-  private [SymbolModuleBelongInner]?: string;
   // @ts-ignore: ignore
   private [SymbolInited]: StateLock;
 
-  constructor(moduleBelong?: string) {
+  constructor() {
     super();
-    if (moduleBelong && typeof moduleBelong !== 'string') {
-      throw new Error(`moduleBelong not valid: ${moduleBelong}`);
-    }
-    this[SymbolModuleBelongInner] = moduleBelong;
     this[SymbolInited] = StateLock.create();
   }
 
   protected get [SymbolModuleBelong]() {
-    if (this[SymbolModuleBelongInner]) return this[SymbolModuleBelongInner];
     const moduleBelong = appResource._getModuleBelong(this[SymbolBeanFullName]);
     if (!moduleBelong) throw new Error(`not found module belong: ${this[SymbolBeanFullName]}`);
     return moduleBelong;
