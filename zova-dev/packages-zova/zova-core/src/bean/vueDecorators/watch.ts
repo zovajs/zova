745fc9170675ce0c7343aef854d1c0d692a2f823
zhennann <zhen.nann@icloud.com>
2025-01-08 21:46:40 +0800

    Update watch.ts

--- zova-dev/packages-zova/zova-core/src/bean/vueDecorators/watch.ts
+++ zova-dev/packages-zova/zova-core/src/bean/vueDecorators/watch.ts
@@ -1,16 +1,43 @@
+import { getProperty } from '@cabloy/utils';
 import { IDecoratorVueElement, IDecoratorVueWatchOptions } from '../../decorator/vue/types.js';
+import { toLowerCaseFirstChar } from '@cabloy/word-utils';
+import { watch as vueWatch } from 'vue';
 
 export function watch(
   beanInstance,
-  beanFullName: string,
+  _beanFullName: string,
   prop: string,
   vueElement: IDecoratorVueElement<IDecoratorVueWatchOptions>,
-  index: number,
+  _index: number,
 ) {
   const { descriptor, options } = vueElement;
   // getter
+  let keySource = `${prop}Source`;
   let getter;
   if (options?.path) {
-    ///getter=
+    getter = () => {
+      return getProperty(beanInstance, options.path!, '.');
+    };
+  } else if (beanInstance[keySource]) {
+    getter = () => {
+      return beanInstance[keySource]();
+    };
+  } else {
+    if (prop.startsWith('watch')) {
+      keySource = toLowerCaseFirstChar(prop.substring('watch'.length));
+    } else {
+      keySource = prop;
+    }
+    getter = () => {
+      return beanInstance[keySource];
+    };
   }
+  // watch
+  vueWatch(
+    getter,
+    (newValue, oldValue) => {
+      descriptor.value.call(beanInstance, newValue, oldValue);
+    },
+    options?.watchOptions,
+  );
 }
