9e3fc8567c9efc165e13b6b90c6e23fa196bb6a3
zhennann <zhen.nann@icloud.com>
2025-01-16 00:56:38 +0800

    Update service.onion_.ts

--- zova-dev/src/suite-vendor/a-zova/modules/a-bean/src/bean/service.onion_.ts
+++ zova-dev/src/suite-vendor/a-zova/modules/a-bean/src/bean/service.onion_.ts
@@ -3,12 +3,13 @@ import { compose as _compose } from '@cabloy/compose';
 import {
   IOnionExecuteCustom,
   IOnionItem,
+  IOnionOptionsDeps,
   IOnionOptionsEnable,
   IOnionOptionsMatch,
   IOnionSlice,
 } from '../types/onion.js';
 import { BeanOnion } from './bean.onion.js';
-import { swapDeps } from '@cabloy/deps';
+import { ISwapDepsItem, swapDeps } from '@cabloy/deps';
 import { getOnionScenesMeta, OnionSceneMeta } from '@cabloy/module-info';
 import { evaluate } from '@cabloy/utils';
 
@@ -21,6 +22,8 @@ export class ServiceOnion<OPTIONS, ONIONNAME extends string> extends BeanSimple 
   sceneName: string;
   sceneMeta: OnionSceneMeta;
 
+  onionsAll: IOnionItem<OPTIONS, ONIONNAME>[];
+
   // private [SymbolOnionsEnabled]: Record<string, IOnionSlice<OPTIONS, ONIONNAME>[]> = {};
   // private [SymbolOnionsEnabledWrapped]: Record<string, Function[]> = {};
 
@@ -28,6 +31,56 @@ export class ServiceOnion<OPTIONS, ONIONNAME extends string> extends BeanSimple 
     this.beanOnion = beanOnion;
     this.sceneName = sceneName;
     this.sceneMeta = getOnionScenesMeta(this.app.meta.module.modulesMeta.modules)[this.sceneName];
+    if (this.sceneMeta.optionsPackage) {
+      this._initOnionsAll();
+      this._swapOnions(this.onionsAll);
+    }
+  }
+
+  private _initOnionsAll() {
+    for (const moduleName in this.app.meta.module.modulesMeta.modules) {
+      const module = this.app.meta.module.modulesMeta.modules[moduleName];
+      const nodeItems = module.info.onionsMeta?.onionsConfig?.[this.sceneName];
+      if (!nodeItems) continue;
+      for (const itemName in nodeItems) {
+        let itemOptions = nodeItems[itemName];
+        for (const key of ['match', 'ignore']) {
+          // value
+          let value = itemOptions[key];
+          if (value === undefined) continue;
+          if (Array.isArray(value)) {
+            value = value.map(item => (typeof item === 'string' && item.startsWith('/') ? evaluate(item) : item));
+          } else {
+            value = typeof value === 'string' && value.startsWith('/') ? evaluate(value) : value;
+          }
+          itemOptions[key] = value;
+        }
+        // extend config
+        const onionName = `${moduleName}:${itemName}`;
+        const optionsConfig = this.app.config.onions[this.sceneName]?.[onionName];
+        if (optionsConfig) {
+          itemOptions = deepExtend({}, itemOptions, optionsConfig);
+        }
+        this.onionsAll.push({
+          name: onionName,
+          options: itemOptions,
+        } as any);
+      }
+    }
+  }
+
+  private _swapOnions(onions: IOnionItem<OPTIONS, ONIONNAME>[]) {
+    swapDeps(onions as ISwapDepsItem[], {
+      name: 'name',
+      dependencies: item => {
+        const onionOptions = cast<IOnionItem<OPTIONS, ONIONNAME>>(item).options as IOnionOptionsDeps<string>;
+        return onionOptions.dependencies as any;
+      },
+      dependents: item => {
+        const onionOptions = cast<IOnionItem<OPTIONS, ONIONNAME>>(item).options as IOnionOptionsDeps<string>;
+        return onionOptions.dependents as any;
+      },
+    });
   }
 
   // getOnionsEnabled(selector?: string) {
@@ -64,45 +117,17 @@ export class ServiceOnion<OPTIONS, ONIONNAME extends string> extends BeanSimple 
   //   return this.getOnionSlice(onionName).beanOptions.options as OPTIONS | undefined;
   // }
 
-  async collectOnionsFromPackage(selector?: string) {
-    const onionsMatched: IOnionItem<OPTIONS, ONIONNAME>[] = [];
-    for (const moduleName in this.app.meta.module.modulesMeta.modules) {
-      const module = this.app.meta.module.modulesMeta.modules[moduleName];
-      const nodeItems = module.info.onionsMeta?.onionsConfig?.[this.sceneName];
-      if (!nodeItems) continue;
-      for (const itemName in nodeItems) {
-        let itemOptions = nodeItems[itemName];
-        for (const key of ['match', 'ignore']) {
-          // value
-          let value = itemOptions[key];
-          if (value === undefined) continue;
-          if (Array.isArray(value)) {
-            value = value.map(item => (typeof item === 'string' && item.startsWith('/') ? evaluate(item) : item));
-          } else {
-            value = typeof value === 'string' && value.startsWith('/') ? evaluate(value) : value;
-          }
-          itemOptions[key] = value;
-        }
-        // extend config
-        const onionName = `${moduleName}:${itemName}`;
-        const optionsConfig = this.app.config.onions[this.sceneName]?.[onionName];
-        if (optionsConfig) {
-          itemOptions = deepExtend({}, itemOptions, optionsConfig);
-        }
-        // check
-        if (this.beanOnion.checkOnionOptionsEnabled(itemOptions, selector)) {
-          onionsMatched.push({
-            name: onionName,
-            options: itemOptions,
-          } as any);
-        }
-      }
-    }
-    // ok
-    return onionsMatched;
+  async loadOnionsFromPackage(selector?: string): Promise<IOnionSlice<OPTIONS, ONIONNAME>[]> {
+    // onionItems
+    const onionItems = this.getOnionsEnabled(this.onionsAll, selector);
+    // loadOnions
+    return await this.loadOnions(onionItems, selector);
   }
 
-  async loadOnions(onionItems: IOnionItem<OPTIONS, ONIONNAME> | IOnionItem<OPTIONS, ONIONNAME>[], selector?: string) {
+  async loadOnions(
+    onionItems: IOnionItem<OPTIONS, ONIONNAME> | IOnionItem<OPTIONS, ONIONNAME>[],
+    selector?: string,
+  ): Promise<IOnionSlice<OPTIONS, ONIONNAME>[]> {
     if (!Array.isArray(onionItems)) onionItems = [onionItems];
     // load modules
     const moduleNames = onionItems.map(item => item.name.split(':')[0]);
@@ -123,24 +148,18 @@ export class ServiceOnion<OPTIONS, ONIONNAME extends string> extends BeanSimple 
       }
       onionSlices.push({ name: item.name, options, beanFullName });
     }
+    // optionsPackage
+    if (this.sceneMeta.optionsPackage) return onionSlices;
     // swap
-    swapDeps(onionSlices, {
-      name: 'name',
-      dependencies: item => {
-        return cast(cast<IOnionSlice<OPTIONS, ONIONNAME>>(item).options).dependencies;
-      },
-      dependents: item => {
-        return cast(cast<IOnionSlice<OPTIONS, ONIONNAME>>(item).options).dependents;
-      },
-    });
+    this._swapOnions(onionSlices);
     // filter
     return this.getOnionsEnabled(onionSlices, selector);
   }
 
-  getOnionsEnabled(onions: IOnionSlice<OPTIONS, ONIONNAME>[], selector?: string) {
+  getOnionsEnabled(onions: IOnionItem<OPTIONS, ONIONNAME>[], selector?: string) {
     if (!selector) selector = '';
-    return onions.filter(onionSlice => {
-      const onionOptions = onionSlice.options as IOnionOptionsEnable & IOnionOptionsMatch<string>;
+    return onions.filter(onionItem => {
+      const onionOptions = onionItem.options as IOnionOptionsEnable & IOnionOptionsMatch<string>;
       return this.beanOnion.checkOnionOptionsEnabled(onionOptions, selector);
     }) as unknown as IOnionSlice<OPTIONS, ONIONNAME>[];
   }
