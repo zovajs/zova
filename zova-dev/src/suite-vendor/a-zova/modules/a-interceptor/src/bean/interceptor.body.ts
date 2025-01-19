32c2dfe30dfa31ec1e3b825c61dd84415ce2e082
zhennann <zhen.nann@icloud.com>
2025-01-19 20:04:59 +0800

    feat: update

--- zova-dev/src/suite-vendor/a-zova/modules/a-interceptor/src/bean/interceptor.body.ts
+++ zova-dev/src/suite-vendor/a-zova/modules/a-interceptor/src/bean/interceptor.body.ts
@@ -1,6 +1,31 @@
-import { BeanInterceptorBase, IDecoratorInterceptorOptions, Interceptor } from 'zova-module-a-fetch';
+import { AxiosResponse } from 'axios';
+import {
+  BeanInterceptorBase,
+  IDecoratorInterceptorOptions,
+  IInterceptorResponse,
+  Interceptor,
+  NextInterceptorResponse,
+} from 'zova-module-a-fetch';
 
 export interface IInterceptorOptionsBody extends IDecoratorInterceptorOptions {}
 
 @Interceptor<IInterceptorOptionsBody>()
-export class InterceptorBody extends BeanInterceptorBase {}
+export class InterceptorBody extends BeanInterceptorBase implements IInterceptorResponse {
+  async onResponse(
+    response: AxiosResponse,
+    _options: IInterceptorOptionsBody,
+    next: NextInterceptorResponse,
+  ): Promise<AxiosResponse> {
+    response = await next();
+    const contentType = response.headers['content-type'];
+    if (!contentType || contentType.indexOf('application/json') === -1) return response;
+    if (response.data.code !== 0) {
+      const error = new Error();
+      error.code = response.data.code;
+      error.message = response.data.message;
+      throw error;
+    }
+    // return data
+    return response.data.data;
+  }
+}
