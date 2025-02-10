8fc0ffc7e0baac13f7de1e2dcfc8d27c2581aa12
zhennann <zhen.nann@icloud.com>
2025-02-10 13:02:08 +0800

    feat: freeze

--- zova-dev/packages-zova/zova-core/src/bootstrap.ts
+++ zova-dev/packages-zova/zova-core/src/bootstrap.ts
@@ -1,6 +1,6 @@
 import { App } from 'vue';
+import { PluginFreeze } from 'vue-plugin-render-freeze';
 import { PluginBean } from './plugins/bean.js';
-import { PluginFreeze } from './plugins/freeze.js';
 
 export async function bootstrap({ app }: { app: App }) {
   app.use(PluginBean);
