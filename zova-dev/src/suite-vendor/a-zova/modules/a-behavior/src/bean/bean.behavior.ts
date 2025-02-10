1ca06f94b2d131959708762903d21bc4a8ba50d8
zhennann <zhen.nann@icloud.com>
2025-02-10 20:39:20 +0800

    feat: composer load

--- zova-dev/src/suite-vendor/a-zova/modules/a-behavior/src/bean/bean.behavior.ts
+++ zova-dev/src/suite-vendor/a-zova/modules/a-behavior/src/bean/bean.behavior.ts
@@ -1,60 +1,16 @@
-import { BeanBase, cast, Use } from 'zova';
-import { Bean, BeanOnion, IOnionItem } from 'zova-module-a-bean';
-import { IBehaviorRecord, IBehaviors, IDecoratorBehaviorOptions } from '../types/behavior.js';
+import { BeanBase, Use } from 'zova';
+import { Bean, BeanOnion } from 'zova-module-a-bean';
+import { IBehaviors } from '../types/behavior.js';
 import { Composer } from '../lib/composer.js';
-import { BeanBehaviorBase } from './bean.behaviorBase.js';
-
-const SymbolSliceOptionsOriginal = Symbol('SymbolSliceOptionsOriginal');
 
 @Bean()
 export class BeanBehavior extends BeanBase {
   @Use()
   $$beanOnion: BeanOnion;
 
-  protected async __init__() {}
-
   public async createComposer(behaviors: IBehaviors): Promise<Composer> {
-    // onionItems
-    const onionItems = this._prepareOnionItems(behaviors);
-    // load onions
-    const onionSlices = await this.$$beanOnion.behavior.loadOnions<BeanBehaviorBase>(onionItems);
-    // create behaviors
-    for (const onionSlice of onionSlices) {
-      onionSlice.beanInstance = await this.bean._newBean(onionSlice.beanFullName as any, true, onionSlice.options);
-      onionSlice.beanInstance![SymbolSliceOptionsOriginal] = onionSlice.options;
-    }
-    // compose
-    const composer = this.$$beanOnion.behavior.compose(onionSlices, (onionSlice, props: any, next) => {
-      const beanInstance = cast<BeanBehaviorBase>(onionSlice.beanInstance);
-      return cast(beanInstance).render(props, next as any);
-    });
-    return this.bean._newBeanSimple(Composer, false, composer);
-  }
-
-  private _prepareOnionItems(behaviors: IBehaviors) {
-    const onionItems: IOnionItem<IDecoratorBehaviorOptions, keyof IBehaviorRecord>[] = [];
-    return this._prepareOnionItemsInner(onionItems, behaviors);
-  }
-
-  private _prepareOnionItemsInner(
-    onionItems: IOnionItem<IDecoratorBehaviorOptions, keyof IBehaviorRecord>[],
-    behaviors: IBehaviors,
-  ) {
-    if (!Array.isArray(behaviors)) behaviors = [behaviors];
-    for (const behaviorItem of behaviors) {
-      if (typeof behaviorItem === 'string') {
-        onionItems.push({ name: behaviorItem as unknown as keyof IBehaviorRecord, options: undefined });
-      } else if (Array.isArray(behaviorItem)) {
-        this._prepareOnionItemsInner(onionItems, behaviorItem);
-      } else if (typeof behaviorItem === 'object') {
-        for (const key in behaviorItem) {
-          let options = behaviorItem[key];
-          if (options === false) continue;
-          if (options === true) options = undefined;
-          onionItems.push({ name: key as unknown as keyof IBehaviorRecord, options });
-        }
-      }
-    }
-    return onionItems;
+    const composer = this.bean._newBeanSimple(Composer, false, this.$$beanOnion);
+    await composer.load(behaviors);
+    return composer;
   }
 }
--- zova-dev/src/suite-vendor/a-zova/modules/a-behavior/src/lib/composer.ts
+++ zova-dev/src/suite-vendor/a-zova/modules/a-behavior/src/lib/composer.ts
@@ -1,16 +1,64 @@
-import { BeanSimple } from 'zova';
-import { NextBehavior } from '../types/behavior.js';
+import { BeanSimple, cast } from 'zova';
+import { IBehaviorRecord, IBehaviors, IDecoratorBehaviorOptions, NextBehavior } from '../types/behavior.js';
 import { VNode } from 'vue';
-import { TypeComposer } from 'zova-module-a-bean';
+import { BeanOnion, IOnionItem, TypeComposer } from 'zova-module-a-bean';
+import { BeanBehaviorBase } from '../bean/bean.behaviorBase.js';
+
+const SymbolSliceOptionsOriginal = Symbol('SymbolSliceOptionsOriginal');
 
 export class Composer extends BeanSimple {
   private _composer: TypeComposer;
+  private _beanOnion: BeanOnion;
 
-  protected async __init__(composer: TypeComposer) {
-    this._composer = composer;
+  protected async __init__(beanOnion: BeanOnion) {
+    this._beanOnion = beanOnion;
+  }
+
+  public async load(behaviors: IBehaviors) {
+    // onionItems
+    const onionItems = this._prepareOnionItems(behaviors);
+    // load onions
+    const onionSlices = await this._beanOnion.behavior.loadOnions<BeanBehaviorBase>(onionItems);
+    // create behaviors
+    for (const onionSlice of onionSlices) {
+      onionSlice.beanInstance = await this.bean._newBean(onionSlice.beanFullName as any, true, onionSlice.options);
+      onionSlice.beanInstance![SymbolSliceOptionsOriginal] = onionSlice.options;
+    }
+    // compose
+    this._composer = this._beanOnion.behavior.compose(onionSlices, (onionSlice, props: any, next) => {
+      const beanInstance = cast<BeanBehaviorBase>(onionSlice.beanInstance);
+      return cast(beanInstance).render(props, next as any);
+    });
   }
 
   public render<PROPS_INPUT, PROPS_OUTPUT = PROPS_INPUT>(props: PROPS_INPUT, next: NextBehavior<PROPS_OUTPUT>): VNode {
     return this._composer(props, next);
+  }
+
+  private _prepareOnionItems(behaviors: IBehaviors) {
+    const onionItems: IOnionItem<IDecoratorBehaviorOptions, keyof IBehaviorRecord>[] = [];
+    return this._prepareOnionItemsInner(onionItems, behaviors);
+  }
+
+  private _prepareOnionItemsInner(
+    onionItems: IOnionItem<IDecoratorBehaviorOptions, keyof IBehaviorRecord>[],
+    behaviors: IBehaviors,
+  ) {
+    if (!Array.isArray(behaviors)) behaviors = [behaviors];
+    for (const behaviorItem of behaviors) {
+      if (typeof behaviorItem === 'string') {
+        onionItems.push({ name: behaviorItem as unknown as keyof IBehaviorRecord, options: undefined });
+      } else if (Array.isArray(behaviorItem)) {
+        this._prepareOnionItemsInner(onionItems, behaviorItem);
+      } else if (typeof behaviorItem === 'object') {
+        for (const key in behaviorItem) {
+          let options = behaviorItem[key];
+          if (options === false) continue;
+          if (options === true) options = undefined;
+          onionItems.push({ name: key as unknown as keyof IBehaviorRecord, options });
+        }
+      }
+    }
+    return onionItems;
   }
 }
