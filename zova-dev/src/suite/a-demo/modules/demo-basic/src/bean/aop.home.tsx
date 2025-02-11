a0c9d786502e1f63662e86d86b8af542a16a3bb9
zhennann <zhen.nann@icloud.com>
2025-02-11 10:47:02 +0800

    Update aop.home.tsx

--- zova-dev/src/suite/a-demo/modules/demo-basic/src/bean/aop.home.tsx
+++ zova-dev/src/suite/a-demo/modules/demo-basic/src/bean/aop.home.tsx
@@ -3,6 +3,11 @@ import { Aop } from 'zova-module-a-bean';
 
 @Aop({ match: 'home-index.controller.pageHome' })
 export class AopHome extends BeanBase {
+  protected async __init__(_args, next) {
+    const result = await next();
+    return result + '!';
+  }
+
   render(_args, next) {
     const result = next();
     return <div class="aop-home">{result}</div>;
