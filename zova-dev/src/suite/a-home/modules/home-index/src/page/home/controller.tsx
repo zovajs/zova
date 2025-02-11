05a66bee728d7e8561b592f6efcf4bf8cd2bc94d
zhennann <zhen.nann@icloud.com>
2025-02-11 10:45:25 +0800

    Update controller.tsx

--- zova-dev/src/suite/a-home/modules/home-index/src/page/home/controller.tsx
+++ zova-dev/src/suite/a-home/modules/home-index/src/page/home/controller.tsx
@@ -3,11 +3,17 @@ import { Controller } from 'zova-module-a-bean';
 
 @Controller()
 export class ControllerPageHome extends BeanControllerPageBase {
+  private message: string;
+
+  protected async __init__() {
+    this.message = 'Hello Zova';
+  }
+
   protected render() {
     return (
       <div style="text-align: center;">
         <div>
-          <div style="font-size: 36px;">Hello Zova</div>
+          <div style="font-size: 36px;">{this.message}</div>
           <div style="font-size: 24px;opacity:.4;">Less is more, while more is less</div>
         </div>
       </div>
