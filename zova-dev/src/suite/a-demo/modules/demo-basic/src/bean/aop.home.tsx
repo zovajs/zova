71d40a02b5ea60bfce4382e9c62cb161c08b55b2
zhennann <zhen.nann@icloud.com>
2025-02-11 11:51:26 +0800

    Update aop.home.tsx

--- zova-dev/src/suite/a-demo/modules/demo-basic/src/bean/aop.home.tsx
+++ zova-dev/src/suite/a-demo/modules/demo-basic/src/bean/aop.home.tsx
@@ -4,18 +4,22 @@ import { ControllerPageHome } from 'zova-module-home-index';
 
 @Aop({ match: 'home-index.controller.pageHome' })
 export class AopHome extends BeanAopBase {
-  protected async __init__(_args, next, receiver: ControllerPageHome) {
+  protected async __init__(
+    _args: Parameters<ControllerPageHome['__init__']>,
+    next: Function,
+    receiver: ControllerPageHome,
+  ) {
     await next();
     receiver.message += '!';
     Object.getPrototypeOf(receiver)['__dispose__'] = () => {};
   }
 
-  protected __dispose__(_args, next, receiver: ControllerPageHome) {
+  protected __dispose__(_args: [], next: Function, receiver: ControllerPageHome) {
     receiver.message = receiver.message.substring(0, receiver.message.length - 1);
     next();
   }
 
-  render(_args, next) {
+  render(_args: [], next: Function) {
     const result = next();
     return <div class="aop-home">{result}</div>;
   }
