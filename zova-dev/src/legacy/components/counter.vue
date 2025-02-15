02b818f9a88bb1d9d9b7712cb13302e7a29318ef
zhennann <zhen.nann@icloud.com>
2025-02-15 22:38:02 +0800

    feat: eslint

--- zova-dev/src/legacy/components/counter.vue
+++ zova-dev/src/legacy/components/counter.vue
@@ -1,11 +1,17 @@
 <script setup>
 import { useCounter } from '@/composables/counter';
+
 const { count, increment, decrement } = useCounter();
 </script>
+
 <template>
   <div>
     <div>{{ `count: ${count}` }}</div>
-    <button class="btn btn-primary" @click="increment">Increment</button>
-    <button class="btn btn-secondary" @click="decrement">Decrement</button>
+    <button class="btn btn-primary" @click="increment">
+      Increment
+    </button>
+    <button class="btn btn-secondary" @click="decrement">
+      Decrement
+    </button>
   </div>
 </template>
