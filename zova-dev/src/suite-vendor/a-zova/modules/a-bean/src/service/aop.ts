eb98b464efd210d60e3bd781a1e716a1cad22094
zhennann <zhen.nann@icloud.com>
2025-03-02 19:24:22 +0800

    feat: update

--- zova-dev/src/suite-vendor/a-zova/modules/a-bean/src/service/aop.ts
+++ zova-dev/src/suite-vendor/a-zova/modules/a-bean/src/service/aop.ts
@@ -1,9 +1,10 @@
 import type { Constructable, IBeanRecord } from 'zova';
 import type { IAopRecord, IDecoratorAopOptions } from '../types/aop.js';
 import type { IOnionSlice } from '../types/onion.js';
-import { appResource, BeanBase, SymbolProxyDisable, Use } from 'zova';
+import { appMetadata, appResource, BeanBase, SymbolProxyDisable, Use } from 'zova';
 import { BeanOnion } from '../bean/bean.onion.js';
 import { Service } from '../lib/bean.js';
+import { SymbolDecoratorUseAopMethod } from '../types/aopMethod.js';
 
 @Service()
 export class ServiceAop extends BeanBase {
@@ -29,5 +30,17 @@ export class ServiceAop extends BeanBase {
     if (!beanOptions) return;
     // loadOnions
     return await this.$$beanOnion.aop.loadOnionsFromPackage(beanOptions.beanFullName);
+  }
+
+  async hasAopMethods<T>(A: Constructable<T>): Promise<boolean>;
+  async hasAopMethods<K extends keyof IBeanRecord>(beanFullName: K): Promise<boolean>;
+  async hasAopMethods(beanFullName: string): Promise<boolean>;
+  async hasAopMethods<T>(beanFullName: Constructable<T> | string): Promise<boolean> {
+    // beanOptions
+    const beanOptions = appResource.getBean(beanFullName as any);
+    if (!beanOptions) return false;
+    const uses = appMetadata.getMetadata<IUseAopMethodPropMetadata<T>[]>(SymbolDecoratorUseAopMethod, beanOptions.beanClass.prototype);
+
+    return !!uses;
   }
 }
