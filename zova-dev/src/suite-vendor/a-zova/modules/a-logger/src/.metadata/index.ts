2d626ade2cbe70a4f67588c56da298f60a567132
zhennann <zhen.nann@icloud.com>
2025-03-02 19:44:06 +0800

    feat: update

---
+++ zova-dev/src/suite-vendor/a-zova/modules/a-logger/src/.metadata/index.ts
@@ -0,0 +1,50 @@
+/** aopMethod: end */
+/** scope: begin */
+import { BeanScopeBase, BeanScopeUtil } from 'zova';
+import { Scope } from 'zova-module-a-bean';
+/** aopMethod: begin */
+import { IAopMethodOptionsLog } from '../bean/aopMethod.log.js';
+/** aopMethod: end */
+/** aopMethod: begin */
+import { AopMethodLog } from '../bean/aopMethod.log.js';
+import 'zova';
+import 'zova';
+
+import 'zova';
+
+export * from '../bean/aopMethod.log.js';
+declare module 'zova-module-a-bean' {
+
+  export interface IAopMethodRecord {
+    'a-logger:log': IAopMethodOptionsLog;
+  }
+
+}
+declare module 'zova-module-a-logger' {
+
+  export interface AopMethodLog {
+    /** @internal */
+    get scope(): ScopeModuleALogger;
+  }
+}
+declare module 'zova' {
+  export interface IBeanRecordLocal {
+    'a-logger.aopMethod.log': AopMethodLog;
+  }
+}
+
+@Scope()
+export class ScopeModuleALogger extends BeanScopeBase {}
+
+export interface ScopeModuleALogger {
+  util: BeanScopeUtil;
+}
+
+declare module 'zova' {
+  export interface IBeanScopeRecord {
+    'a-logger': ScopeModuleALogger;
+  }
+
+}
+
+/** scope: end */
---
+++ zova-dev/src/suite-vendor/a-zova/modules/a-logger/src/bean/aopMethod.log.ts
@@ -0,0 +1,14 @@
+import type { Next, NextSync } from 'zova';
+import type { IAopMethodExecute, IDecoratorAopMethodOptions } from 'zova-module-a-bean';
+import { BeanAopMethodBase } from 'zova';
+import { AopMethod } from 'zova-module-a-bean';
+
+export interface IAopMethodOptionsLog extends IDecoratorAopMethodOptions {}
+
+@AopMethod<IAopMethodOptionsLog>()
+export class AopMethodLog extends BeanAopMethodBase implements IAopMethodExecute {
+  execute(_options: IAopMethodOptionsLog, _args: [], next: Next | NextSync, _receiver: any, _prop: string): Promise<any> | any {
+    // next
+    return next();
+  }
+}
