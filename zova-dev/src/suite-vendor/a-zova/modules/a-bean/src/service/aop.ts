0b1417543710e65775849ce5a59903cd9a05dc31
zhennann <zhen.nann@icloud.com>
2025-03-02 20:47:47 +0800

    Update aop.ts

--- zova-dev/src/suite-vendor/a-zova/modules/a-bean/src/service/aop.ts
+++ zova-dev/src/suite-vendor/a-zova/modules/a-bean/src/service/aop.ts
@@ -6,6 +6,8 @@ import { BeanOnion } from '../bean/bean.onion.js';
 import { Service } from '../lib/bean.js';
 import { IAopMethodRecord, IDecoratorAopMethodOptions, IUseAopMethodPropMetadata, SymbolDecoratorUseAopMethod } from '../types/aopMethod.js';
 
+type AopMethodsMatchedAll = Record<string, IUseAopMethodPropMetadata[]>;
+
 @Service()
 export class ServiceAop extends BeanBase {
   protected [SymbolProxyDisable]: boolean = true;
@@ -32,73 +34,41 @@ export class ServiceAop extends BeanBase {
     return await this.$$beanOnion.aop.loadOnionsFromPackage(beanOptions.beanFullName);
   }
 
-  async hasAopMethods<T>(A: Constructable<T>): Promise<boolean>;
-  async hasAopMethods<K extends keyof IBeanRecord>(beanFullName: K): Promise<boolean>;
-  async hasAopMethods(beanFullName: string): Promise<boolean>;
-  async hasAopMethods<T>(beanFullName: Constructable<T> | string): Promise<boolean> {
-    // beanOptions
-    const beanOptions = appResource.getBean(beanFullName as any);
-    if (!beanOptions) return false;
-    const uses = appMetadata.getMetadata<Record<string, IUseAopMethodPropMetadata[]>>(
-      SymbolDecoratorUseAopMethod,
-      beanOptions.beanClass.prototype,
-    );
-
-    if (uses) {
-      const onionItems: IOnionItem<IDecoratorAopMethodOptions, keyof IAopMethodRecord>[] = [];
-      for (const prop in uses) {
-        const aopMethods: IUseAopMethodPropMetadata[] = uses[prop];
-        for (const aopMethod of aopMethods) {
-          onionItems.push({
-            name: aopMethod.onionName,
-            options: aopMethod.options,
-          });
-        }
-      }
-      // load onions
-      const onionSlices = await this.$$beanOnion.aopMethod.loadOnions<BeanAopMethodBase>(onionItems);
-      for (const onionSlice of onionSlices) {
-        await this.app.bean._getBean(onionSlice.beanFullName as any, true);
-      }
-    }
-    return !!uses;
-  }
-
-  findAopMethodsMatched<T>(A: Constructable<T>, prop: string): IUseAopMethodPropMetadata[] | undefined;
-  findAopMethodsMatched<K extends keyof IBeanRecord>(beanFullName: K, prop: string): IUseAopMethodPropMetadata[] | undefined;
-  findAopMethodsMatched(beanFullName: string, prop: string): IUseAopMethodPropMetadata[] | undefined;
-  findAopMethodsMatched<T>(beanFullName: Constructable<T> | string, prop: string): IUseAopMethodPropMetadata[] | undefined {
+  async findAopMethodsMatched<T>(A: Constructable<T>): Promise<AopMethodsMatchedAll | undefined>;
+  async findAopMethodsMatched<K extends keyof IBeanRecord>(beanFullName: K): Promise<AopMethodsMatchedAll | undefined>;
+  async findAopMethodsMatched(beanFullName: string): Promise<AopMethodsMatchedAll | undefined>;
+  async findAopMethodsMatched<T>(beanFullName: Constructable<T> | string): Promise<AopMethodsMatchedAll | undefined> {
     // beanOptions
     const beanOptions = appResource.getBean(beanFullName as any);
     if (!beanOptions) return;
-    const aopMethodsMatched: IUseAopMethodPropMetadata[] = [];
+    const aopMethodsMatchedAll: AopMethodsMatchedAll = {};
     const uses = appMetadata.getMetadata<Record<string, IUseAopMethodPropMetadata[]>>(
       SymbolDecoratorUseAopMethod,
       beanOptions.beanClass.prototype,
     );
-    const aopMethods: IUseAopMethodPropMetadata[] | undefined = uses?.[prop];
-    if (aopMethods) {
+    for (const prop in uses) {
       const onionItems: IOnionItem<IDecoratorAopMethodOptions, keyof IAopMethodRecord>[] = [];
+      const aopMethods: IUseAopMethodPropMetadata[] = uses[prop];
       for (const aopMethod of aopMethods) {
         onionItems.push({
           name: aopMethod.onionName,
           options: aopMethod.options,
         });
-        // load onions
-        const onionSlices = await this.$$beanOnion.aopMethod.loadOnions<BeanAopMethodBase>(onionItems);
-        const onionSlice = this.bean.onion.aopMethod.getOnionSlice(aopMethod.onionName!);
-        const options = deepExtend({}, onionSlice.beanOptions.options, aopMethod.options);
-        if (this.bean.onion.checkOnionOptionsEnabled(options)) {
-          const beanFullName = beanFullNameFromOnionName(aopMethod.onionName!, 'aopMethod');
-          const beanInstance = this.app.bean._getBean(beanFullName as any);
-          aopMethodsMatched.push({
-            onionName: aopMethod.onionName,
-            beanInstance,
-            options,
-          });
-        }
       }
+      // load onions
+      const onionSlices = await this.$$beanOnion.aopMethod.loadOnions<BeanAopMethodBase>(onionItems);
+      // aopMethodsMatched
+      const aopMethodsMatched: IUseAopMethodPropMetadata[] = [];
+      for (const onionSlice of onionSlices) {
+        const beanInstance = await this.app.bean._getBean(onionSlice.beanFullName as any, true);
+        aopMethodsMatched.push({
+          onionName: onionSlice.name,
+          beanInstance,
+          options: onionSlice.options,
+        });
+      }
+      aopMethodsMatchedAll[prop] = aopMethodsMatched;
     }
-    return aopMethodsMatched;
+    return aopMethodsMatchedAll;
   }
 }
