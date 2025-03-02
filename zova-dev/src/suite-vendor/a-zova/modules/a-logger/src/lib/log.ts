1bffc40e2b0c0ef2712cd498c040685226a5d2e2
zhennann <zhen.nann@icloud.com>
2025-03-02 19:47:40 +0800

    Update log.ts

--- zova-dev/src/suite-vendor/a-zova/modules/a-logger/src/lib/log.ts
+++ zova-dev/src/suite-vendor/a-zova/modules/a-logger/src/lib/log.ts
@@ -1,6 +1,6 @@
 import type { IAopMethodOptionsLog } from '../bean/aopMethod.log.js';
 import { UseAopMethod } from 'zova-module-a-bean';
 
-export function Transaction(options?: Partial<IAopMethodOptionsLog>): MethodDecorator {
+export function Log(options?: Partial<IAopMethodOptionsLog>): MethodDecorator {
   return UseAopMethod('a-logger:log', options);
 }
