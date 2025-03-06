38951a52efdc6444c5b77973a34c3609a6259825
zhennann <zhen.nann@icloud.com>
2025-03-06 15:43:49 +0800

    Update colorize.ts

--- zova-dev/packages-utils/logger/src/colorize.ts
+++ zova-dev/packages-utils/logger/src/colorize.ts
@@ -1,62 +1,65 @@
 import { LEVEL, MESSAGE } from './types.js';
 
-/*
- * Colorizer format. Wraps the `level` and/or `message` properties
- * of the `info` objects with ANSI color codes based on a few options.
- */
+const codes = {
+  black: [30, 39],
+  red: [31, 39],
+  green: [32, 39],
+  yellow: [33, 39],
+  blue: [34, 39],
+  magenta: [35, 39],
+  cyan: [36, 39],
+  white: [37, 39],
+  gray: [90, 39],
+  grey: [90, 39],
+};
+
+let allColors = {
+  error: 'red',
+  warn: 'yellow',
+  info: 'green',
+  http: 'green',
+  verbose: 'cyan',
+  debug: 'blue',
+  silly: 'magenta',
+};
+
+export function colorize(color, text) {
+  if (!text) return text;
+  const code = codes[allColors[color]];
+  if (!code) return text;
+  const open = `\x1B[${code[0]}m`;
+  const close = `\x1B[${code[1]}m`;
+  return `${open}${text}${close}`;
+}
+
 class Colorizer {
-  options: object;
-  constructor(opts) {
+  options: any;
+  constructor(opts: any = {}) {
+    if (opts.colors) {
+      allColors = Object.assign({}, allColors, opts.colors);
+    }
     this.options = opts;
   }
 
-  /*
-   * function colorize (lookup, level, message)
-   * Performs multi-step colorization using @colors/colors/safe
-   */
-  colorize(lookup, level, message) {
-    return `\\x1b[31m${lookup}\\x1b[39m`;
-    // if (typeof message === 'undefined') {
-    //   message = level;
-    // }
-
-    // //
-    // // If the color for the level is just a string
-    // // then attempt to colorize the message with it.
-    // //
-    // if (!Array.isArray(Colorizer.allColors[lookup])) {
-    //   return colors[Colorizer.allColors[lookup]](message);
-    // }
-
-    // //
-    // // If it is an Array then iterate over that Array, applying
-    // // the colors function for each item.
-    // //
-    // for (let i = 0, len = Colorizer.allColors[lookup].length; i < len; i++) {
-    //   message = colors[Colorizer.allColors[lookup][i]](message);
-    // }
-
-    // return message;
+  colorize(lookup, level, message?) {
+    if (typeof message === 'undefined') {
+      message = level;
+    }
+    return colorize(lookup, message);
   }
 
-  /*
-   * function transform (info, opts)
-   * Attempts to colorize the { level, message } of the given
-   * `logform` info object.
-   */
-  transform(info, _opts) {
-    info[LEVEL] = this.colorize(info[LEVEL], info.level, info[MESSAGE]);
-    // if (opts.all && typeof info[MESSAGE] === 'string') {
-    //   info[MESSAGE] = this.colorize(info[LEVEL], info.level, info[MESSAGE]);
-    // }
+  transform(info, opts) {
+    if (opts.all && typeof info[MESSAGE] === 'string') {
+      info[MESSAGE] = this.colorize(info[LEVEL], info.level, info[MESSAGE]);
+    }
 
-    // if (opts.level || opts.all || !opts.message) {
-    //   info.level = this.colorize(info[LEVEL], info.level);
-    // }
+    if (opts.level || opts.all || !opts.message) {
+      info.level = this.colorize(info[LEVEL], info.level);
+    }
 
-    // if (opts.all || opts.message) {
-    //   info.message = this.colorize(info[LEVEL], info.level, info.message);
-    // }
+    if (opts.all || opts.message) {
+      info.message = this.colorize(info[LEVEL], info.level, info.message);
+    }
 
     return info;
   }
