fecfacf769afa33933c9ccbc8362e5557da73d74
zhennann <zhen.nann@icloud.com>
2025-04-06 16:07:34 +0800

    feat: extendAfterBuild

--- zova-dev/packages-utils/quasar-app-extension-zova/dist/quasar/extendAfterBuild.js
+++ zova-dev/packages-utils/quasar-app-extension-zova/dist/quasar/extendAfterBuild.js
@@ -1,5 +1,12 @@
-export function extendAfterBuild(_context, _flavor) {
+import path from 'node:path';
+import fse from 'fs-extra';
+import { getOutDir } from 'zova-vite';
+export function extendAfterBuild(context, _flavor) {
     return async function extendAfterBuild(_conf, _api) {
+        // remove dist/ssr/server
+        fse.removeSync(path.join(getOutDir(), 'server'));
+        // remove zova/runtime
+        fse.removeSync(path.join(context.configOptions.appDir, context.configOptions.runtimeDir));
     };
 }
 //# sourceMappingURL=extendAfterBuild.js.map
--- zova-dev/packages-utils/quasar-app-extension-zova/src/quasar/extendAfterBuild.ts
+++ zova-dev/packages-utils/quasar-app-extension-zova/src/quasar/extendAfterBuild.ts
@@ -1,7 +1,14 @@
 import type { IndexAPI } from '@quasar/app-vite';
 import type { ConfigContext, QuasarConf } from './types.js';
+import path from 'node:path';
+import fse from 'fs-extra';
+import { getOutDir } from 'zova-vite';
 
-export function extendAfterBuild(_context: ConfigContext, _flavor: string) {
+export function extendAfterBuild(context: ConfigContext, _flavor: string) {
   return async function extendAfterBuild(_conf: QuasarConf, _api: IndexAPI) {
+    // remove dist/ssr/server
+    fse.removeSync(path.join(getOutDir(), 'server'));
+    // remove zova/runtime
+    fse.removeSync(path.join(context.configOptions!.appDir, context.configOptions!.runtimeDir));
   };
 }
