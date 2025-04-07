ab29aa16749147351938a664489359fc5f2cec03
zhennann <zhen.nann@icloud.com>
2025-04-07 20:46:03 +0800

    Update routes.ts

--- zova-dev/src/suite/a-demo/modules/demo-basic/src/routes.ts
+++ zova-dev/src/suite/a-demo/modules/demo-basic/src/routes.ts
@@ -1,24 +1,22 @@
 import type { IModuleRoute } from 'zova-module-a-router';
-import { createZovaComponentPage } from 'zova';
-import { ControllerPageState } from './.metadata/index.js';
-import Component from './.metadata/page/component.vue';
-import Legacy from './.metadata/page/legacy.vue';
-import Locale from './.metadata/page/locale.vue';
-import Pinia from './.metadata/page/pinia.vue';
-import RouteParams from './.metadata/page/routeParams.vue';
-import RouteQuery from './.metadata/page/routeQuery.vue';
-import RouteQueryB from './.metadata/page/routeQueryB.vue';
-// import State from './.metadata/page/state.vue';
-import Style from './.metadata/page/style.vue';
+import { ZPageComponent } from './.metadata/page/component.js';
+import { ZPageLegacy } from './.metadata/page/legacy.js';
+import { ZPageLocale } from './.metadata/page/locale.js';
+import { ZPagePinia } from './.metadata/page/pinia.js';
+import { ZPageRouteParams } from './.metadata/page/routeParams.js';
+import { ZPageRouteQuery } from './.metadata/page/routeQuery.js';
+import { ZPageRouteQueryB } from './.metadata/page/routeQueryB.js';
+import { ZPageState } from './.metadata/page/state.js';
+import { ZPageStyle } from './.metadata/page/style.js';
 
 export const routes: IModuleRoute[] = [
-  { path: 'state', component: createZovaComponentPage(ControllerPageState) },
-  { path: 'component', component: Component },
-  { path: 'locale', component: Locale },
-  { path: 'style', component: Style },
-  { path: 'pinia', component: Pinia },
-  { path: 'routeQuery', component: RouteQuery },
-  { name: 'routeParams', path: 'routeParams/:id?', component: RouteParams },
-  { path: 'routeQueryB', component: RouteQueryB },
-  { path: 'legacy', component: Legacy },
+  { path: 'state', component: ZPageState },
+  { path: 'component', component: ZPageComponent },
+  { path: 'locale', component: ZPageLocale },
+  { path: 'style', component: ZPageStyle },
+  { path: 'pinia', component: ZPagePinia },
+  { path: 'routeQuery', component: ZPageRouteQuery },
+  { name: 'routeParams', path: 'routeParams/:id?', component: ZPageRouteParams },
+  { path: 'routeQueryB', component: ZPageRouteQueryB },
+  { path: 'legacy', component: ZPageLegacy },
 ];
