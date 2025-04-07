3fc104d015a9cdc7612ab29e50f0cd1e5b2f8621
zhennann <zhen.nann@icloud.com>
2025-04-07 22:11:24 +0800

    Update cliCreatePage.ts

--- zova-cli/cli-set-front/src/lib/common/cliCreatePage.ts
+++ zova-cli/cli-set-front/src/lib/common/cliCreatePage.ts
@@ -73,7 +73,7 @@ export class CliCreatePageBase extends BeanCliBase {
     const env = await this.loadEnvs();
     const host = !env.DEV_SERVER_HOST || env.DEV_SERVER_HOST === 'true' ? 'localhost' : env.DEV_SERVER_HOST;
     const port = env.DEV_SERVER_PORT;
-    const url = `http://${host}:${port}/#/${argv.moduleInfo.pid}/${argv.moduleInfo.name}/${argv.pageName}`;
+    const url = `http://${host}:${port}/${argv.moduleInfo.pid}/${argv.moduleInfo.name}/${argv.pageName}`;
     const message = this.helper.boxen({ text: url });
     await this.console.log(message);
   }
