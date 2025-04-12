7aeafa65cd38e694db45cff1ef01ebac40419426
zhennann <zhen.nann@icloud.com>
2025-04-12 09:46:33 +0800

    Update monkeySys.ts

--- zova-dev/src/suite-vendor/a-zova/modules/a-router/src/monkeySys.ts
+++ zova-dev/src/suite-vendor/a-zova/modules/a-router/src/monkeySys.ts
@@ -1,27 +1,28 @@
 import type { IModule } from '@cabloy/module-info';
 import type { IMonkeyModuleSys } from 'zova';
+import type { SysRouter } from './bean/sys.router.js';
 import { BeanSimple } from 'zova';
 
 export class MonkeySys extends BeanSimple implements IMonkeyModuleSys {
   private _moduleSelf: IModule;
-  private _beanRouter: BeanRouter;
+  private _sysRouter: SysRouter;
 
   constructor(moduleSelf: IModule) {
     super();
     this._moduleSelf = moduleSelf;
   }
 
-  async getBeanRouter() {
-    if (!this._beanRouter) {
-      this._beanRouter = (await this.bean._getBean('a-router.bean.router', false, true)) as BeanRouter;
+  async getSysRouter() {
+    if (!this._sysRouter) {
+      this._sysRouter = (await this.bean._getBean('a-router.sys.router', false)) as SysRouter;
     }
-    return this._beanRouter;
+    return this._sysRouter;
   }
 
   async moduleLoading(module: IModule) {
     if (this._moduleSelf === module) return;
-    const beanRouter = await this.getBeanRouter();
-    beanRouter._registerRoutes(module);
+    const sysRouter = await this.getSysRouter();
+    sysRouter._registerRoutes(module);
   }
 
   async moduleLoaded(_module: IModule) {}
