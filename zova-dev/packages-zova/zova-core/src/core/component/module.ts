17750d697d6ea4c5e0da801dcabf13cf74542fe6
zhennann <zhen.nann@icloud.com>
2025-04-15 20:16:23 +0800

    feat: monkey

--- zova-dev/packages-zova/zova-core/src/core/component/module.ts
+++ zova-dev/packages-zova/zova-core/src/core/component/module.ts
@@ -14,6 +14,7 @@ export class AppModule extends BeanSimple {
 
   /** @internal */
   public async initialize() {
+    await this._loadAllMonkeysAndSyncsAndPreloads();
     await this._requireAllSpecifics('preload');
     await this._requireAllSpecifics('monkey');
     await this._requireAllSpecifics('sync');
@@ -70,6 +71,15 @@ export class AppModule extends BeanSimple {
     return this.sys.meta.module.exists(moduleName as any);
   }
 
+  private async _loadAllMonkeysAndSyncsAndPreloads() {
+    for (const moduleName of this.sys.meta.module.modulesMeta.moduleNames) {
+      const module = this.sys.meta.module.modulesMeta.modules[moduleName];
+      if (module.resource.Monkey) {
+        this.monkeyInstances[moduleName] = this.app.bean._newBeanSimple(module.resource.Monkey, false, module);
+      }
+    }
+  }
+
   private async _requireAllSpecifics(capabilityName: 'preload' | 'monkey' | 'sync') {
     for (const moduleName of this.sys.meta.module.modulesMeta.moduleNames) {
       const module = this.sys.meta.module.modulesMeta.modules[moduleName];
@@ -115,12 +125,9 @@ export class AppModule extends BeanSimple {
   }
 
   private async _installInner(moduleName: string, moduleRepo: IModule) {
-    // main / monkey
+    // main
     if (moduleRepo.resource.Main) {
       this.mainInstances[moduleName] = this.app.bean._newBeanSimple(moduleRepo.resource.Main, false, moduleRepo);
-    }
-    if (moduleRepo.resource.Monkey) {
-      this.monkeyInstances[moduleName] = this.app.bean._newBeanSimple(moduleRepo.resource.Monkey, false, moduleRepo);
     }
     // monkey: moduleLoading
     await this._monkeyModule('moduleLoading', moduleRepo);
--- zova-dev/packages-zova/zova-core/src/core/sys/module.ts
+++ zova-dev/packages-zova/zova-core/src/core/sys/module.ts
@@ -110,7 +110,11 @@ export class SysModule extends BeanSimple {
     const modulesResource = await Promise.all(promises);
     for (let i = 0; i < modulesResource.length; i++) {
       const moduleName = moduleNamesLoading[i];
-      this.modulesMeta.modules[moduleName].resource = modulesResource[i];
+      const module = this.modulesMeta.modules[moduleName];
+      module.resource = modulesResource[i];
+      if (module.resource.MonkeySys) {
+        this.monkeyInstances[moduleName] = this.sys.bean._newBeanSimple(module.resource.MonkeySys, false, module);
+      }
     }
   }
 
@@ -168,12 +172,9 @@ export class SysModule extends BeanSimple {
       const moduleResource = moduleRepo.resource as any;
       moduleRepo.resource = await moduleResource();
     }
-    // main / monkey
+    // main
     if (moduleRepo.resource.MainSys) {
       this.mainInstances[moduleName] = this.sys.bean._newBeanSimple(moduleRepo.resource.MainSys, false, moduleRepo);
-    }
-    if (moduleRepo.resource.MonkeySys) {
-      this.monkeyInstances[moduleName] = this.sys.bean._newBeanSimple(moduleRepo.resource.MonkeySys, false, moduleRepo);
     }
     // monkey: moduleLoading
     await this._monkeyModule('moduleLoading', moduleRepo);
