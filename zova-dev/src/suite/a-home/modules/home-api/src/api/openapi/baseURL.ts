6afba829a3407ce977d401b6d8d075f64dc5ee10
zhennann <zhen.nann@icloud.com>
2025-04-15 18:20:34 +0800

    feat: update

--- zova-dev/src/suite/a-home/modules/home-api/src/api/openapi/baseURL.ts
+++ zova-dev/src/suite/a-home/modules/home-api/src/api/openapi/baseURL.ts
@@ -1,5 +1,6 @@
 import type { ZovaSys } from 'zova';
+import { cast } from 'zova';
 
 export const ApiBaseURL = (sys: ZovaSys) => {
-  return sys.env.OPENAPI_BASE_URL_HOME_API || sys.env.OPENAPI_BASE_URL_DEFAULT;
+  return cast(sys.env).OPENAPI_BASE_URL_HOME_API || sys.env.OPENAPI_BASE_URL_DEFAULT;
 };
