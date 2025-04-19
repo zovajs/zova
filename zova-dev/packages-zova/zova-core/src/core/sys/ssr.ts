dd0360b58caf52bd3daac3e2fdbadcdee01dbbfe
zhennann <zhen.nann@icloud.com>
2025-04-19 17:14:21 +0800

    Update ssr.ts

--- zova-dev/packages-zova/zova-core/src/core/sys/ssr.ts
+++ zova-dev/packages-zova/zova-core/src/core/sys/ssr.ts
@@ -1,8 +1,8 @@
 import { BeanSimple } from '../../bean/beanSimple.js';
 
 export class SysSSR extends BeanSimple {
-  public async getSsrHandler() {
-    const ssrHandler = await this.sys.bean._getBean('a-ssrserver.service.ssrHandler' as never, false) as any;
+  public async getSsrHandler(siteAssetDir: string) {
+    const ssrHandler = await this.sys.bean._getBean('a-ssrserver.service.ssrHandler' as never, false, siteAssetDir) as any;
     return ssrHandler;
   }
 }
