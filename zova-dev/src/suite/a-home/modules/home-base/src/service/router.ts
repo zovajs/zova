e868baa875821c7c124e2aa369f5f11af48c22b8
zhennann <zhen.nann@icloud.com>
2025-04-21 19:58:07 +0800

    Update router.ts

--- zova-dev/src/suite/a-home/modules/home-base/src/service/router.ts
+++ zova-dev/src/suite/a-home/modules/home-base/src/service/router.ts
@@ -14,7 +14,8 @@ export class ServiceRouter extends BeanRouterBase {
       if (to.meta.requiresAuth !== false && !this.$$modelPassport.isAuthenticated) {
         await this.$$modelPassport.ensurePassport();
         if (!this.$$modelPassport.isAuthenticated) {
-          return this.sys.config.router.pageLogin;
+          this.app.gotoLogin(to.fullPath);
+          return false;
         }
       }
     });
