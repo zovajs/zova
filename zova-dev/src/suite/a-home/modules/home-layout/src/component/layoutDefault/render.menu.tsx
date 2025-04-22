2ba9778193ef7f9607c8e99cdc3d3331fe2f6bdb
zhennann <zhen.nann@icloud.com>
2025-04-22 23:04:09 +0800

    feat: menu

--- zova-dev/src/suite/a-home/modules/home-layout/src/component/layoutDefault/render.menu.tsx
+++ zova-dev/src/suite/a-home/modules/home-layout/src/component/layoutDefault/render.menu.tsx
@@ -1,45 +1,49 @@
-// import type { VNode } from 'vue';
+import type { VNode } from 'vue';
 import { BeanRenderBase } from 'zova';
 import { Render } from 'zova-module-a-bean';
-// import { ApiSchemaAMenuDtoMenuItem } from 'zova-module-home-api';
-// import { ZEssentialLink } from '../../.metadata/index.js';
+import { TypeMenuItem, TypeMenuTree, ZEssentialLink } from '../../.metadata/index.js';
 
 @Render()
 export class RenderMenu extends BeanRenderBase {
-  // _renderMenuItem(item: ApiSchemaAMenuDtoMenuItem) {
-  //   const titleLocale = this.$text(item.title);
-  //   if (item.separator) {
-  //     return <li></li>;
-  //   }
-  //   if (item.folder) {
-  //     return (
-  //       <li>
-  //         <h2 class="menu-title">{titleLocale}</h2>
-  //         <ul>{this._renderMenuItems(item.children)}</ul>
-  //       </li>
-  //     );
-  //   }
-  //   return (
-  //     <li key={item.title}>
-  //       <ZEssentialLink title={titleLocale} caption={item.caption} icon={item.icon} href={item.href} to={item.to} />
-  //     </li>
-  //   );
-  // }
+  _renderMenuItem(item: TypeMenuItem) {
+    const titleLocale = this.$text(item.title ?? '');
+    if (item.folder) {
+      return (
+        <li>
+          <h2 class="menu-title">{titleLocale}</h2>
+          <ul>{this._renderMenuItems(item.children)}</ul>
+        </li>
+      );
+    }
+    if (item.separator) {
+      return <li></li>;
+    }
+    return (
+      <li key={item.title}>
+        <ZEssentialLink
+          title={titleLocale}
+          caption={item.caption}
+          icon={item.icon}
+          href={item.external ? item.link : undefined}
+          to={!item.external ? item.link : undefined}
+        />
+      </li>
+    );
+  }
 
-  // _renderMenuItems(items?: ApiMenuEntity[]) {
-  //   if (!items) return;
-  //   const domItems: VNode[] = [];
-  //   for (const item of items) {
-  //     domItems.push(this._renderMenuItem(item));
-  //   }
-  //   return domItems;
-  // }
+  _renderMenuItems(items?: TypeMenuTree) {
+    if (!items) return;
+    const domItems: VNode[] = [];
+    for (const item of items) {
+      domItems.push(this._renderMenuItem(item));
+    }
+    return domItems;
+  }
 
   public render() {
     const menuTree = this.$$modelMenu.menuTree;
-    console.log(menuTree);
-    // if (queryMenus.isLoading || !queryMenus.data) return;
-    // const domItems = this._renderMenuItems(queryMenus.data);
-    // return <ul class="menu bg-base-200 text-base-content min-h-full w-80 p-4">{domItems}</ul>;
+    if (!menuTree) return;
+    const domItems = this._renderMenuItems(menuTree);
+    return <ul class="menu bg-base-200 text-base-content min-h-full w-80 p-4">{domItems}</ul>;
   }
 }
--- zova-dev/src/suite/a-home/modules/home-layout/src/model/menu.ts
+++ zova-dev/src/suite/a-home/modules/home-layout/src/model/menu.ts
@@ -1,19 +1,20 @@
 import { useComputed } from 'zova-core';
 import { BeanModelBase, Model } from 'zova-module-a-model';
-import { ApiSchemaAMenuDtoMenuItem } from 'zova-module-home-api';
+import { ApiSchemaAMenuDtoMenuGroup, ApiSchemaAMenuDtoMenuItem, ApiSchemaAMenuDtoMenus } from 'zova-module-home-api';
+
+export type TypeMenuGroup = ApiSchemaAMenuDtoMenuGroup & { folder: true; children: TypeMenuItem[] };
+export type TypeMenuItem = (ApiSchemaAMenuDtoMenuItem & { folder: false }) | TypeMenuGroup;
+export type TypeMenuTree = TypeMenuItem[];
 
 @Model()
 export class ModelMenu extends BeanModelBase {
-  menuTree: any;
+  menuTree?: TypeMenuTree;
 
   protected async __init__() {
     this.menuTree = useComputed(() => {
       const queryMenus = this.retrieveMenus();
       if (!queryMenus.data) return;
-      const items = queryMenus.data.items?.filter(item => {
-        return { ...item, title: `${item.title}!!` };
-      });
-      return { ...queryMenus.data, items };
+      return this._prepareMenuTree(queryMenus.data);
     });
   }
 
@@ -34,5 +35,24 @@ export class ModelMenu extends BeanModelBase {
     const menus = this.retrieveMenus().data;
     if (!menus || !menus.items) return;
     return menus.items.find(item => item.id === search.id || item.link === search.link);
+  }
+
+  private _prepareMenuTree(menus: ApiSchemaAMenuDtoMenus, groupId?: string): TypeMenuTree {
+    let children: TypeMenuItem[] = [];
+    if (menus.items) {
+      children = children.concat(
+        menus.items?.filter(item => item.group === groupId || (Array.isArray(item.group) && item.group.includes(groupId!))).map(item => {
+          return { ...item, folder: false };
+        }),
+      );
+    }
+    if (menus.groups) {
+      const groups = menus.groups.filter(item => item.group === groupId || (Array.isArray(item.group) && item.group.includes(groupId!)))
+        .map(menuGroup => {
+          return Object.assign({}, menuGroup, { folder: true, children: this._prepareMenuTree(menus, menuGroup.id) });
+        });
+      children = children.concat(groups);
+    }
+    return children.sort((a, b) => (a.order ?? 0) - (b.order ?? 0));
   }
 }
