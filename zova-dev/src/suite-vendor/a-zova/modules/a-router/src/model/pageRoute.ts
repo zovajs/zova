deb015c312e97fdb5feb30ec50052c42ef3f49f3
zhennann <zhen.nann@icloud.com>
2025-04-27 19:47:09 +0800

    Update pageRoute.ts

--- zova-dev/src/suite-vendor/a-zova/modules/a-router/src/model/pageRoute.ts
+++ zova-dev/src/suite-vendor/a-zova/modules/a-router/src/model/pageRoute.ts
@@ -1,16 +1,32 @@
+import { useRoute } from '@cabloy/vue-router';
+import { useComputed } from 'zova';
 import { BeanModelBase, Model } from 'zova-module-a-model';
 
 @Model()
 export class ModelPageRoute extends BeanModelBase {
+  protected _pageDataInner: any;
   pagePath?: string;
   pageData?: any;
 
   protected async __init__() {
     this.pagePath = this.$useStateMem({ queryKey: ['pagePath'] });
-    this.pageData = this.$useStateMem({ queryKey: ['pageData', this.pagePath] });
     if (process.env.SERVER) {
-      this.pagePath = this.$ssr.context.pagePath;
-      this.pageData = this.$ssr.context.pageData;
+      const pagePath = this.$ssr.context.pagePath;
+      if (pagePath) {
+        this.pagePath = pagePath;
+        this._pageDataInner = this.getPageData(this.pagePath);
+        this._pageDataInner = this.$ssr.context.pageData;
+      }
+    } else {
+      this._pageDataInner = this.getPageData(this.pagePath);
+      const route = useRoute();
+      this.pageData = useComputed(() => {
+        return this.getPageData(route.fullPath);
+      });
     }
+  }
+
+  getPageData(pagePath: string) {
+    return this.$useStateMem({ queryKey: ['pageData', pagePath] });
   }
 }
