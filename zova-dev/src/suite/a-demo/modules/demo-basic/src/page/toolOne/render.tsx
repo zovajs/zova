80ab8260ec77b4ae6f014b758bb06f9ae3854d4c
zhennann <zhen.nann@icloud.com>
2025-05-08 22:52:26 +0800

    Update render.tsx

--- zova-dev/src/suite/a-demo/modules/demo-basic/src/page/toolOne/render.tsx
+++ zova-dev/src/suite/a-demo/modules/demo-basic/src/page/toolOne/render.tsx
@@ -4,13 +4,25 @@ import { Render } from 'zova-module-a-bean';
 @Render()
 export class RenderPageToolOne extends BeanRenderBase {
   public render() {
-    const queryTest = this.$$modelTest.test();
-    const sdk = this.$$sysSdk.getSdk(this.$query.api, this.$query.apiMethod);
-    console.log(this.$query);
     return (
       <div>
-        <div>{queryTest.data?.name}</div>
-        <div>{Object.keys(sdk?.responses || {})}</div>
+        <form bs-form={{ form: this.form }}>
+          <this.form.Field name="name">
+            {
+              ({ field }) => {
+                console.log(field.state.meta);
+                return (
+                  <input
+                    name={field.name}
+                    value={field.state.value}
+                    onInput={e => field.handleChange((e.target as any).value)}
+                  ></input>
+                );
+              }
+            }
+          </this.form.Field>
+          <button type="submit">Submit</button>
+        </form>
       </div>
     );
   }
