3144630d92356f212d590912cd187bb67718ecdb
zhennann <zhen.nann@icloud.com>
2025-05-08 22:51:19 +0800

    Update behavior.form.ts

--- zova-dev/src/suite-vendor/a-zova/modules/a-form/src/bean/behavior.form.ts
+++ zova-dev/src/suite-vendor/a-zova/modules/a-form/src/bean/behavior.form.ts
@@ -1,13 +1,16 @@
-import { VNode } from 'vue';
+import { createCommentVNode, VNode } from 'vue';
 import { BeanBehaviorBase, Behavior, IDecoratorBehaviorOptions, NextBehavior } from 'zova-module-a-behavior';
 import { ReturnTypeUseForm } from '../types/form.js';
 
-export interface IBehaviorPropsInputForm {}
+export interface IBehaviorPropsInputForm {
+  onSubmit?: ((payload: Event) => void);
+}
 
 export interface IBehaviorPropsOutputForm {}
 
 export interface IBehaviorOptionsForm<TFormData = unknown> extends IDecoratorBehaviorOptions {
-  form: ReturnTypeUseForm<TFormData>;
+  form?: ReturnTypeUseForm<TFormData>;
+  onSubmit?: ((payload: Event) => void) | boolean;
 }
 
 @Behavior<IBehaviorOptionsForm>()
@@ -21,7 +24,24 @@ export class BehaviorForm extends BeanBehaviorBase<
     this.bean._setBean('$$form', this.$options.form);
   }
 
-  protected render(_props: IBehaviorPropsInputForm, next: NextBehavior<IBehaviorPropsOutputForm>): VNode {
-    return next();
+  protected async onOptionsChange(options: IBehaviorOptionsForm) {
+    super.onOptionsChange(options);
+    this.bean._setBean('$$form', this.$options.form);
+  }
+
+  protected render(props: IBehaviorPropsInputForm, next: NextBehavior<IBehaviorPropsOutputForm>): VNode {
+    if (!this.$options.form) return createCommentVNode();
+    if (!props.onSubmit && this.$options.onSubmit !== false) {
+      props = Object.assign({}, props, {
+        onSubmit: typeof this.$options.onSubmit === 'function'
+          ? this.$options.onSubmit
+          : (e: Event) => {
+              e.preventDefault();
+              e.stopPropagation();
+              this.$options.form?.handleSubmit();
+            },
+      });
+    }
+    return next(props);
   }
 }
