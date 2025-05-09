7fee646600003c166782bf40f62fa4bfb346ada6
zhennann <zhen.nann@icloud.com>
2025-05-09 10:58:21 +0800

    Update behavior.formField.ts

--- zova-dev/src/suite-vendor/a-zova/modules/a-form/src/bean/behavior.formField.ts
+++ zova-dev/src/suite-vendor/a-zova/modules/a-form/src/bean/behavior.formField.ts
@@ -5,7 +5,10 @@ import { Use } from 'zova-core';
 import { BeanBehaviorBase, Behavior, IDecoratorBehaviorOptions, NextBehavior } from 'zova-module-a-behavior';
 import { ReturnTypeUseFormField, TypeBehaviorFormFieldOptions } from '../types/form.js';
 
-export interface IBehaviorPropsInputFormField {}
+export interface IBehaviorPropsInputFormField {
+  name?: string;
+  value?: any;
+}
 
 export interface IBehaviorPropsOutputFormField {}
 
@@ -29,7 +32,27 @@ export class BehaviorFormField extends BeanBehaviorBase<
     this.bean._setBean('$$formField', this.$$formField);
   }
 
-  protected render(_props: IBehaviorPropsInputFormField, next: NextBehavior<IBehaviorPropsOutputFormField>): VNode {
-    return next();
+  protected render(props: IBehaviorPropsInputFormField, next: NextBehavior<IBehaviorPropsOutputFormField>): VNode {
+    props = this._patchProps(props);
+    return next(props);
+  }
+
+  private _patchProps(props: IBehaviorPropsInputFormField) {
+    const field = this.$$formField;
+    if (this.$$behaviorTag.component === 'input') {
+      return this._patchProps_input(field, props);
+    }
+    return props;
+  }
+
+  private _patchProps_input(field: ReturnTypeUseFormField, props: IBehaviorPropsInputFormField) {
+    const propsPatch: IBehaviorPropsInputFormField = {};
+    if (!props.name) {
+      propsPatch.name = field.api.name;
+    }
+    if (!props.value) {
+      propsPatch.value = field.state.value;
+    }
+    return Object.assign({}, props, propsPatch);
   }
 }
