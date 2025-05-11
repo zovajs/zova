fc5776d688e3e131c40ce250522518f3ee0b4ce9
zhennann <zhen.nann@icloud.com>
2025-05-11 21:56:30 +0800

    Update monkeySys.ts

--- zova-dev/src/suite-vendor/a-devui/modules/devui-adapter/src/monkeySys.ts
+++ zova-dev/src/suite-vendor/a-devui/modules/devui-adapter/src/monkeySys.ts
@@ -5,8 +5,19 @@ export class MonkeySys extends BeanSimple implements IMonkeySysInitialize {
   async sysInitialize() {
     // defaultThemeHandler
     const scopeStyleConfig = this.sys.util.getModuleConfigSafe('a-style');
-    scopeStyleConfig.defaultThemeHandler = 'devui-adapter.meta.themeHandler';
-    //
-    // this.sys.util.getApiPath
+    if (!scopeStyleConfig.defaultThemeHandler) {
+      scopeStyleConfig.defaultThemeHandler = 'devui-adapter.meta.themeHandler';
+    }
+    // rest
+    const scopeRestConfig = this.sys.util.getModuleConfigSafe('a-rest');
+    if (!scopeRestConfig.defaultRestPage) {
+      scopeRestConfig.defaultRestPage = 'devui-restpage:restPage';
+    }
+    if (!scopeRestConfig.defaultRestTable) {
+      scopeRestConfig.defaultRestTable = 'devui-resttable:restTable';
+    }
+    if (!scopeRestConfig.defaultRestForm) {
+      scopeRestConfig.defaultRestForm = 'devui-restform:restForm';
+    }
   }
 }
