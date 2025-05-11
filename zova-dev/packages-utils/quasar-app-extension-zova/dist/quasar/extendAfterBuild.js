65c761e8e1892f2f63bb2dcb6dca5c7651d9f5a6
zhennann <zhen.nann@icloud.com>
2025-05-11 11:04:45 +0800

    feat: update

--- zova-dev/packages-utils/quasar-app-extension-zova/dist/quasar/extendAfterBuild.js
+++ zova-dev/packages-utils/quasar-app-extension-zova/dist/quasar/extendAfterBuild.js
@@ -3,15 +3,26 @@ import fse from 'fs-extra';
 import { getOutDir, getOutReleasesDir } from 'zova-vite';
 export function extendAfterBuild(context, _flavor) {
     return async function extendAfterBuild(_conf, _api) {
-        const outDir = getOutDir();
+        const outDir = path.join(context.configOptions.appDir, getOutDir());
         // remove dist/ssr/server
-        fse.removeSync(path.join(context.configOptions.appDir, outDir, 'server'));
+        fse.removeSync(path.join(outDir, 'server'));
         // remove zova/runtime
         fse.removeSync(path.join(context.configOptions.appDir, context.configOptions.runtimeDir));
         // copy
         const outReleasesDir = path.join(context.configOptions.appDir, getOutReleasesDir());
         fse.removeSync(outReleasesDir);
-        fse.copySync(path.join(context.configOptions.appDir, outDir), outReleasesDir);
+        fse.copySync(outDir, outReleasesDir);
+        // copy
+        if (process.env.BUILD_COPY_DIST) {
+            const outDirCopy = path.join(process.env.BUILD_COPY_DIST, path.basename(outDir));
+            fse.removeSync(outDirCopy);
+            fse.copySync(outDir, outDirCopy);
+        }
+        if (process.env.BUILD_COPY_RELEASE) {
+            const outReleasesDirCopy = path.join(process.env.BUILD_COPY_RELEASE, path.basename(outReleasesDir));
+            fse.removeSync(outReleasesDirCopy);
+            fse.copySync(outDir, outReleasesDirCopy);
+        }
     };
 }
 //# sourceMappingURL=extendAfterBuild.js.map
--- zova-dev/packages-utils/quasar-app-extension-zova/src/quasar/extendAfterBuild.ts
+++ zova-dev/packages-utils/quasar-app-extension-zova/src/quasar/extendAfterBuild.ts
@@ -6,17 +6,34 @@ import { getOutDir, getOutReleasesDir } from 'zova-vite';
 
 export function extendAfterBuild(context: ConfigContext, _flavor: string) {
   return async function extendAfterBuild(_conf: QuasarConf, _api: IndexAPI) {
-    const outDir = getOutDir();
+    const outDir = path.join(context.configOptions!.appDir, getOutDir());
     // remove dist/ssr/server
-    fse.removeSync(path.join(context.configOptions!.appDir, outDir, 'server'));
+    fse.removeSync(path.join(outDir, 'server'));
     // remove zova/runtime
     fse.removeSync(path.join(context.configOptions!.appDir, context.configOptions!.runtimeDir));
     // copy
     const outReleasesDir = path.join(context.configOptions!.appDir, getOutReleasesDir());
     fse.removeSync(outReleasesDir);
     fse.copySync(
-      path.join(context.configOptions!.appDir, outDir),
+      outDir,
       outReleasesDir,
     );
+    // copy
+    if (process.env.BUILD_COPY_DIST) {
+      const outDirCopy = path.join(process.env.BUILD_COPY_DIST, path.basename(outDir));
+      fse.removeSync(outDirCopy);
+      fse.copySync(
+        outDir,
+        outDirCopy,
+      );
+    }
+    if (process.env.BUILD_COPY_RELEASE) {
+      const outReleasesDirCopy = path.join(process.env.BUILD_COPY_RELEASE, path.basename(outReleasesDir));
+      fse.removeSync(outReleasesDirCopy);
+      fse.copySync(
+        outDir,
+        outReleasesDirCopy,
+      );
+    }
   };
 }
