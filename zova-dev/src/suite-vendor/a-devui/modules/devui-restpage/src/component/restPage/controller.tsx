74f966d15f9a1f14518e22e44a39b32dbd38d85b
zhennann <zhen.nann@icloud.com>
2025-05-12 23:13:40 +0800

    Update controller.tsx

--- zova-dev/src/suite-vendor/a-devui/modules/devui-restpage/src/component/restPage/controller.tsx
+++ zova-dev/src/suite-vendor/a-devui/modules/devui-restpage/src/component/restPage/controller.tsx
@@ -1,7 +1,18 @@
-import { BeanControllerBase } from 'zova';
+import type { ControllerPageResource } from 'zova-module-a-rest';
+import { BeanControllerBase, Use } from 'zova';
 import { Controller } from 'zova-module-a-bean';
 
 @Controller()
 export class ControllerRestPage extends BeanControllerBase {
-  protected async __init__() {}
+  @Use({ injectionScope: 'host' })
+  $$restResource: ControllerPageResource;
+
+  protected async __init__() {
+    // sdkFindAll
+    const querySdkFindMany = this.$$restResource.getQuerySdkFindMany();
+    await querySdkFindMany.suspense();
+    // dataFindAll
+    const queryDataFindMany = this.$$restResource.getQueryDataFindMany();
+    await queryDataFindMany.suspense();
+  }
 }
