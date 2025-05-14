fabaf72228c92e90a2f2ad66a8507febe9394f27
zhennann <zhen.nann@icloud.com>
2025-05-14 22:53:34 +0800

    Update render.tsx

--- zova-dev/src/suite-vendor/a-devui/modules/devui-restform/src/component/restForm/render.tsx
+++ zova-dev/src/suite-vendor/a-devui/modules/devui-restform/src/component/restForm/render.tsx
@@ -1,20 +1,35 @@
+import { SchemaObject } from 'openapi3-ts/oas31';
+import { VNode } from 'vue';
 import { z } from 'zod';
 import { BeanRenderBase } from 'zova';
 import { Render } from 'zova-module-a-bean';
 
 @Render()
 export class RenderRestForm extends BeanRenderBase {
-  public render() {
-    return (
-      <form bs-form={{ form: this.form }}>
+  private _renderSchema(schema: SchemaObject) {
+    const children: VNode[] = [];
+    const properties = schema.properties!;
+    for (const key in properties) {
+      // const property = properties[key];
+      children.push(
         <input
           bs-formField={{
-            name: 'quantity',
+            name: key,
             validators: {
               onChange: z.string().min(3),
             },
           }}
-        ></input>
+        ></input>,
+      );
+    }
+    return children;
+  }
+
+  public render() {
+    if (!this.$props.schema) return;
+    return (
+      <form bs-form={{ form: this.form }}>
+        {this._renderSchema(this.$props.schema)}
       </form>
     );
   }
