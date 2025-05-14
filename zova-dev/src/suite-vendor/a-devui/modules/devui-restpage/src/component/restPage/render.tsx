d4bf0ae10629d04131cc79fa0031c80235ac92bf
zhennann <zhen.nann@icloud.com>
2025-05-14 10:20:58 +0800

    Update render.tsx

--- zova-dev/src/suite-vendor/a-devui/modules/devui-restpage/src/component/restPage/render.tsx
+++ zova-dev/src/suite-vendor/a-devui/modules/devui-restpage/src/component/restPage/render.tsx
@@ -1,4 +1,4 @@
-import { BeanRenderBase } from 'zova';
+import { BeanRenderBase, ClientOnly } from 'zova';
 import { Render } from 'zova-module-a-bean';
 
 @Render()
@@ -6,15 +6,21 @@ export class RenderRestPage extends BeanRenderBase {
   public render() {
     // form
     const ComponentRestForm = this.$zovaComponent(this.$$restResource.defaultRestForm);
+    const querySdkFindOne = this.$$restResource.getQuerySdkFindOne();
+    const schemaFormCreate = this.$$restResource.getSchemaOfFormCreate(querySdkFindOne.data?.operationObject);
     // table
     const ComponentRestTable = this.$zovaComponent(this.$$restResource.defaultRestTable);
     const querySdkBootstrap = this.$$restResource.getQuerySdkBootstrap();
-    const schema = this.$$restResource.getSchemaOfTableRow(querySdkBootstrap.data?.operationObject);
+    const schemaTableRow = this.$$restResource.getSchemaOfTableRow(querySdkBootstrap.data?.operationObject);
     const queryDataFindAll = this.$$restResource.getQueryDataFindAll();
     return (
       <div>
-        <ComponentRestForm></ComponentRestForm>
-        <ComponentRestTable data={queryDataFindAll.data} schema={schema} onOperationCreate={() => this.onOperationCreate()}></ComponentRestTable>
+        <ClientOnly><ComponentRestForm schema={schemaFormCreate}></ComponentRestForm></ClientOnly>
+        <ComponentRestTable
+          data={queryDataFindAll.data}
+          schema={schemaTableRow}
+          onOperationCreate={() => this.onOperationCreate()}
+        ></ComponentRestTable>
       </div>
     );
   }
