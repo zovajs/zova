9ec319907b5d54d732bc93121c56173085937e3f
zhennann <zhen.nann@icloud.com>
2025-05-16 22:29:43 +0800

    feat: update

--- zova-dev/src/suite-vendor/a-zova/modules/a-form/src/.metadata/component/formField.ts
+++ zova-dev/src/suite-vendor/a-zova/modules/a-form/src/.metadata/component/formField.ts
@@ -1,12 +1,21 @@
+import type { TypeControllerInnerProps } from 'zova';
+import type { ControllerFormFieldProps } from '../../component/formField/controller.jsx';
 import { defineComponent } from 'vue';
 import { prepareComponentOptions, useController } from 'zova';
 import { ControllerFormField } from '../../component/formField/controller.jsx';
 import { RenderFormField } from '../../component/formField/render.jsx';
 
-export interface TypeControllerFormFieldPublicProps {
+export type TypeControllerFormFieldPublicProps = {
   controllerRef?: (ref: ControllerFormField) => void;
-}
+} & ControllerFormFieldProps;
 
+type ControllerInnerProps =
+      TypeControllerInnerProps<ControllerFormFieldProps, keyof typeof ControllerFormField.$propsDefault>;
+declare module 'zova-module-a-form' {
+  export interface ControllerFormField {
+    $props: ControllerInnerProps;
+  }
+}
 declare module 'zova-module-a-form' {
   export interface RenderFormField extends ControllerFormField {}
 }
--- zova-dev/src/suite-vendor/a-zova/modules/a-form/src/component/formField/controller.tsx
+++ zova-dev/src/suite-vendor/a-zova/modules/a-form/src/component/formField/controller.tsx
@@ -1,7 +1,11 @@
 import { BeanControllerBase } from 'zova';
 import { Controller } from 'zova-module-a-bean';
 
+export interface ControllerFormFieldProps {}
+
 @Controller()
 export class ControllerFormField extends BeanControllerBase {
+  static $propsDefault = {};
+
   protected async __init__() {}
 }
