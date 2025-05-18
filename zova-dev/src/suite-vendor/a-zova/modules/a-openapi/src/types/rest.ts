27355705539dd344c6a44f81f5e4d09cbdf5fdf1
zhennann <zhen.nann@icloud.com>
2025-05-18 10:04:05 +0800

    Update rest.ts

--- zova-dev/src/suite-vendor/a-zova/modules/a-openapi/src/types/rest.ts
+++ zova-dev/src/suite-vendor/a-zova/modules/a-openapi/src/types/rest.ts
@@ -3,20 +3,20 @@ import type { Constructable, IComponentRecord } from 'zova';
 import 'openapi3-ts/oas30';
 import 'openapi3-ts/oas31';
 
-export interface ISchemaObjectExtensionRest {
+export interface ISchemaObjectExtensionFieldRest {
   render?: TypeRenderComponent;
 }
 
-export interface ISchemaObjectExtension {
-  rest?: ISchemaObjectExtensionRest;
+export interface ISchemaObjectExtensionField {
+  rest?: ISchemaObjectExtensionFieldRest;
 }
 
 declare module 'openapi3-ts/oas30' {
-  export interface SchemaObject extends ISchemaObjectExtension {}
+  export interface SchemaObject extends ISchemaObjectExtensionField {}
 }
 
 declare module 'openapi3-ts/oas31' {
-  export interface SchemaObject extends ISchemaObjectExtension {}
+  export interface SchemaObject extends ISchemaObjectExtensionField {}
 }
 
 export type TypeRenderComponent = Constructable<ComponentPublicInstance> | (keyof IComponentRecord) | string;
