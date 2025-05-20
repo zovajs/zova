25d5c677c07341344d95d0567f49c755bfb1c461
zhennann <zhen.nann@icloud.com>
2025-05-20 11:33:37 +0800

    Update tableCellFormat.ts

--- zova-dev/src/suite-vendor/a-zova/modules/a-table/src/service/tableCellFormat.ts
+++ zova-dev/src/suite-vendor/a-zova/modules/a-table/src/service/tableCellFormat.ts
@@ -1,13 +1,33 @@
 import { SchemaObject } from 'openapi3-ts/oas31';
 import { BeanBase, Use } from 'zova';
-import { Service, SysOnion } from 'zova-module-a-bean';
+import { Service, SysOnion, TypeComposer } from 'zova-module-a-bean';
 
 @Service()
 export class ServiceTableCellFormat extends BeanBase {
   @Use()
   $$sysOnion: SysOnion;
 
-  async loadTableCellFormatsMatched(_schema: SchemaObject, _propertiesCustom: Record<string, SchemaObject>) {
-    // const properties:Record<string, = [];
+  async loadTableCellFormatsMatched(
+    schema: SchemaObject | undefined,
+    propertiesCustom: Record<string, SchemaObject> | undefined,
+  ): Promise<Record<string, TypeComposer | undefined>> {
+    const properties: Record<string, TypeComposer | undefined> = {};
+    if (schema?.properties) {
+      for (const key in schema.properties) {
+        const property = schema.properties[key] as SchemaObject;
+        properties[key] = await this._cretePropertyComposer(property);
+      }
+    }
+    if (propertiesCustom) {
+      for (const key in propertiesCustom) {
+        const property = propertiesCustom[key];
+        properties[key] = await this._cretePropertyComposer(property);
+      }
+    }
+    return properties;
+  }
+
+  async _cretePropertyComposer(property?: SchemaObject): Promise<TypeComposer | undefined> {
+    if (!property) return undefined;
   }
 }
