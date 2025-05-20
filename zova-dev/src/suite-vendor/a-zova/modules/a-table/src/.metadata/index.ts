b635c046e69bb3d5f31beac681341718721027e0
zhennann <zhen.nann@icloud.com>
2025-05-20 10:53:28 +0800

    feat: update

--- zova-dev/src/suite-vendor/a-zova/modules/a-table/src/.metadata/index.ts
+++ zova-dev/src/suite-vendor/a-zova/modules/a-table/src/.metadata/index.ts
@@ -3,10 +3,6 @@ import type { BeanScopeUtil } from 'zova';
 /** scope: begin */
 import { BeanScopeBase } from 'zova';
 import { Scope } from 'zova-module-a-bean';
-
-/** bean: end */
-/** bean: begin */
-import { BeanTableCellFormatBase } from '../bean/bean.tableCellFormatBase.js';
 import { ITableCellFormatOptionsCurrency } from '../bean/tableCellFormat.currency.js';
 /** tableCellFormat: end */
 /** tableCellFormat: begin */
@@ -16,30 +12,15 @@ import { TableCellFormatFallback } from '../bean/tableCellFormat.fallback.js';
 /** service: end */
 /** service: begin */
 import { ServiceTableCellFormat } from '../service/tableCellFormat.js';
-/** bean: begin */
-import 'zova';
-import 'zova';
-import 'zova';
-import 'zova';
-import 'zova';
-import 'zova';
-
-import 'zova';
-
-export * from '../bean/bean.tableCellFormatBase.js';
-declare module 'zova' {
-
-}
-declare module 'zova-module-a-table' {
-
-}
-declare module 'zova' {
-  export interface IBeanRecordGeneral {
-    'a-table.bean.tableCellFormatBase': BeanTableCellFormatBase;
-  }
-}
 /** service: end */
 /** tableCellFormat: begin */
+import 'zova';
+import 'zova';
+import 'zova';
+import 'zova';
+
+import 'zova';
+
 export * from '../bean/tableCellFormat.currency.js';
 declare module 'zova-module-a-bean' {
 
@@ -61,7 +42,6 @@ declare module 'zova' {
   }
 }
 export * from '../bean/tableCellFormat.fallback.js';
-/** bean: end */
 /** service: begin */
 export * from '../service/tableCellFormat.js';
 declare module 'zova-module-a-table' {
--- zova-dev/src/suite-vendor/a-zova/modules/a-table/src/bean/tableCellFormat.fallback.ts
+++ zova-dev/src/suite-vendor/a-zova/modules/a-table/src/bean/tableCellFormat.fallback.ts
@@ -1,8 +1,14 @@
+import { CellContext } from '@tanstack/table-core';
+import { VNode } from 'vue';
+import { BeanBase } from 'zova';
 import { TableCellFormat } from '../lib/tableCellFormat.js';
-import { IDecoratorTableCellFormatOptions } from '../types/tableCellFormat.js';
-import { BeanTableCellFormatBase } from './bean.tableCellFormatBase.js';
+import { IDecoratorTableCellFormatOptions, ITableCellFormatRender, NextTableCellFormat } from '../types/tableCellFormat.js';
 
 export interface ITableCellFormatOptionsFallback extends IDecoratorTableCellFormatOptions {}
 
 @TableCellFormat<ITableCellFormatOptionsFallback>()
-export class TableCellFormatFallback extends BeanTableCellFormatBase<ITableCellFormatOptionsFallback> {}
+export class TableCellFormatFallback extends BeanBase implements ITableCellFormatRender {
+  render(_props: CellContext<{}, unknown>, _options: ITableCellFormatOptionsFallback, next: NextTableCellFormat): VNode | string {
+    return next();
+  }
+}
