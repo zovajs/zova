f5cec69c1f620c6ddbe78db75060ce895333e322
zhennann <zhen.nann@icloud.com>
2025-05-21 17:05:35 +0800

    Update render.actions.tsx

--- zova-dev/src/suite-vendor/a-devui/modules/devui-restpage/src/component/wrapperTable/render.actions.tsx
+++ zova-dev/src/suite-vendor/a-devui/modules/devui-restpage/src/component/wrapperTable/render.actions.tsx
@@ -1,10 +1,20 @@
 import { CellContext } from '@tanstack/table-core';
 import { BeanRenderBase } from 'zova';
 import { Render } from 'zova-module-a-bean';
+import { icon, ZovaIcon } from 'zova-module-a-icon';
 
 @Render()
 export class RenderActions extends BeanRenderBase {
   public renderActions(_props: CellContext<unknown, unknown>) {
-    return <button>sss</button>;
+    return (
+      <div class="flex gap-2">
+        <button class="btn btn-outline btn-primary">
+          <ZovaIcon name={icon('::draft')} height={24}></ZovaIcon>
+        </button>
+        <button class="btn btn-outline btn-error">
+          <ZovaIcon name={icon('::delete')} height={24}></ZovaIcon>
+        </button>
+      </div>
+    );
   }
 }
