eb64e143ea0083d4615aa34e113c067cd5958e45
zhennann <zhen.nann@icloud.com>
2025-05-21 15:43:05 +0800

    feat: update

--- zova-dev/src/suite-vendor/a-zova/modules/a-tablefeature/src/bean/tableFeature.schema.ts
+++ zova-dev/src/suite-vendor/a-zova/modules/a-tablefeature/src/bean/tableFeature.schema.ts
@@ -1,9 +1,49 @@
-import { CellContext } from '@tanstack/table-core';
-import { VNode } from 'vue';
-import { BeanBase } from 'zova';
-import { IDecoratorTableFeatureOptions, TableFeature } from 'zova-module-a-table';
+import { RowData } from '@tanstack/table-core';
+import { SchemaObject } from 'openapi3-ts/oas31';
+import { BeanTableFeatureBase, IDecoratorTableFeatureOptions, TableFeature } from 'zova-module-a-table';
+
+export interface TableFeatureSchemaOptions<_TData extends RowData> {
+  schema?: SchemaObject;
+}
+
+export interface TableFeatureSchemaInstance<_TData extends RowData> {
+  getProperty: (accessorKey: string) => SchemaObject | undefined;
+}
+
+export interface TableFeatureSchemaRow<_TData extends RowData> {
+  getProperty: (accessorKey: string) => SchemaObject | undefined;
+}
+
+export interface TableFeatureSchemaColumn<_TData extends RowData, _TValue> {
+  get property(): SchemaObject | undefined;
+}
+
+export interface TableFeatureSchemaHeader<_TData extends RowData, _TValue> {
+  get property(): SchemaObject | undefined;
+}
+
+export interface TableFeatureSchemaCell<_TData extends RowData, _TValue> {
+  get property(): SchemaObject | undefined;
+}
+
+declare module '@tanstack/vue-table' {
+
+  export interface TableOptionsResolved<TData extends RowData>
+    extends TableFeatureSchemaOptions<TData> {}
+
+  export interface Table<TData extends RowData> extends TableFeatureSchemaInstance<TData> {}
+
+  export interface Row<TData extends RowData> extends TableFeatureSchemaRow<TData> {}
+
+  export interface Column<TData extends RowData, TValue> extends TableFeatureSchemaColumn<TData, TValue> {}
+
+  export interface Header<TData extends RowData, TValue> extends TableFeatureSchemaHeader<TData, TValue> {}
+
+  export interface Cell<TData extends RowData, TValue> extends TableFeatureSchemaCell<TData, TValue> {}
+
+}
 
 export interface ITableFeatureOptionsSchema extends IDecoratorTableFeatureOptions {}
 
 @TableFeature<ITableFeatureOptionsSchema>()
-export class TableFeatureSchema extends BeanBase {}
+export class TableFeatureSchema extends BeanTableFeatureBase {}
