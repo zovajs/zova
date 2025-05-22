7c3eab2dd5ef83a7ca74bc0e7c5204b2e48bc6e0
zhennann <zhen.nann@icloud.com>
2025-05-22 15:31:26 +0800

    feat: update

--- zova-dev/src/suite-vendor/a-zova/modules/a-tableformat/src/bean/tableCellFormat.currency.ts
+++ zova-dev/src/suite-vendor/a-zova/modules/a-tableformat/src/bean/tableCellFormat.currency.ts
@@ -6,7 +6,7 @@ import { IDecoratorTableCellFormatOptions, ITableCellFormatRender, NextTableCell
 
 export interface ITableCellFormatOptionsCurrency extends IDecoratorTableCellFormatOptions {}
 
-@TableCellFormat<ITableCellFormatOptionsCurrency>({ dependencies: 'a-tableformat:fallback', match: '!!context.rest?.currency' })
+@TableCellFormat<ITableCellFormatOptionsCurrency>({ match: '!!context.rest?.currency' })
 export class TableCellFormatCurrency extends BeanBase implements ITableCellFormatRender {
   render(props: CellContext<{}, unknown>, _options: ITableCellFormatOptionsCurrency, next: NextTableCellFormat): VNode | string {
     const value = next();
