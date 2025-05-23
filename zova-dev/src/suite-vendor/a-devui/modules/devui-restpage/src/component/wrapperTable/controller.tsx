f894850c356b2d2faa02fa4f762fa48d735c32a4
zhennann <zhen.nann@icloud.com>
2025-05-23 11:14:30 +0800

    feat: update

--- zova-dev/src/suite-vendor/a-devui/modules/devui-restpage/src/component/wrapperTable/controller.tsx
+++ zova-dev/src/suite-vendor/a-devui/modules/devui-restpage/src/component/wrapperTable/controller.tsx
@@ -4,7 +4,7 @@ import { createColumnHelper, getCoreRowModel, Row } from '@tanstack/table-core';
 import { SchemaObject } from 'openapi3-ts/oas31';
 import { cast, deepExtend, Use } from 'zova';
 import { Controller } from 'zova-module-a-bean';
-import { TypeResourceActionRowRecord, TypeResourceActionTableRecord } from 'zova-module-a-openapi';
+import { OrderUnknownBase, TypeResourceActionRowRecord, TypeResourceActionTableRecord } from 'zova-module-a-openapi';
 import { BeanControllerTableBase, BeanTableFeatureBase, ServiceTableCellFormat, ServiceTableFeature, TypeColumn, TypeTable, TypeTableCellFormatsMatched } from 'zova-module-a-table';
 import { RenderActions } from './render.actions.jsx';
 
@@ -84,6 +84,11 @@ export class ControllerWrapperTable<T extends {} = {}> extends BeanControllerTab
         result.push(property);
       }
     }
+    // sort
+    result.sort((a, b) => {
+      return (a.rest?.order ?? OrderUnknownBase) - (b.rest?.order ?? OrderUnknownBase);
+    });
+    // ok
     this.properties = result;
   }
 
