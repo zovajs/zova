c8a9f0edd2d2684e24bef57890138108a9b10ae4
zhennann <zhen.nann@icloud.com>
2025-06-10 10:57:23 +0800

    Update interceptor.performAction.ts

--- zova-dev/src/suite-vendor/a-zova/modules/a-interceptor/src/bean/interceptor.performAction.ts
+++ zova-dev/src/suite-vendor/a-zova/modules/a-interceptor/src/bean/interceptor.performAction.ts
@@ -1,4 +1,6 @@
+import { catchError } from '@cabloy/utils';
 import { AxiosRequestConfig } from 'axios';
+import { cast } from 'zova';
 import { BeanInterceptorBase, IDecoratorInterceptorOptions, IInterceptorRequest, Interceptor, NextInterceptorRequest } from 'zova-module-a-fetch';
 import { ISsrSitePerformActionOptions } from 'zova-module-a-ssr';
 
@@ -21,7 +23,13 @@ export class InterceptorPerformAction extends BeanInterceptorBase<IInterceptorOp
       body: config.data,
       headers: config.headers,
     };
-    const result = await performAction(data);
+    const [result, error] = await catchError(() => {
+      return performAction(data);
+    });
+    if (error) {
+      cast(error).config = config;
+      throw error;
+    }
     throw result;
   }
 }
