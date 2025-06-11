0927583b22c6dc25ad35da4c7a82a4732d791e24
zhennann <zhen.nann@icloud.com>
2025-06-11 16:46:07 +0800

    Update utils.ts

--- zova-dev/packages-utils/zova-vite/src/utils.ts
+++ zova-dev/packages-utils/zova-vite/src/utils.ts
@@ -79,9 +79,11 @@ export function setModuleAlias() {
 export function getAbsolutePathOfModule(id: string, postfix: string = 'index.js') {
   const require = createRequire(import.meta.url);
   let modulePath = require.resolve(id);
-  const pos = modulePath.lastIndexOf(postfix);
-  if (pos > -1) {
-    modulePath = modulePath.substring(0, modulePath.length - postfix.length - 1);
+  if (postfix) {
+    const pos = modulePath.lastIndexOf(postfix);
+    if (pos > -1) {
+      modulePath = modulePath.substring(0, modulePath.length - postfix.length - 1);
+    }
   }
   return modulePath;
 }
