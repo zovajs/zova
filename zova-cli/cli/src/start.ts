5c3ffd0e6d431edc26d72d6762e1144dae537f3d
zhennann <zhen.nann@icloud.com>
2025-07-31 10:21:26 +0800

    Update start.ts

--- zova-cli/cli/src/start.ts
+++ zova-cli/cli/src/start.ts
@@ -1,7 +1,15 @@
+import { createRequire } from 'node:module';
 import { CabloyCommand } from '@cabloy/cli';
 
 export class ZovaCommand extends CabloyCommand {
   constructor(rawArgv?) {
     super('zova', rawArgv);
+    this.version = this._extractPackageVersion();
+  }
+
+  private _extractPackageVersion() {
+    const require = createRequire(import.meta.url);
+    const pkg = require('../package.json');
+    return pkg.version;
   }
 }
