bef7b053160aaa707b8e162a72d8083dcaa0208a
zhennann <zhen.nann@icloud.com>
2025-10-04 21:31:52 +0800

    Update test.ts

--- zova-dev/src/suite/a-demo/modules/demo-basic/src/model/test.ts
+++ zova-dev/src/suite/a-demo/modules/demo-basic/src/model/test.ts
@@ -6,8 +6,8 @@ export class ModelTest extends BeanModelBase {
     return this.$useStateData({
       queryKey: ['test'],
       queryFn: async () => {
-        const data = await this.$api.testSsrToolOne.test({ id: 1, name: 'Tom', married: true, details: [] }, {
-          params: { id: 1 },
+        const data = await this.$api.testSsrToolOne.test({ id: '1', name: 'Tom', married: true, details: [] }, {
+          params: { id: '1' },
           query: { name: 'Tom' },
         });
         return data;
