2995f51c59d12ec83621fa3339a76df32b66382f
zhennann <zhen.nann@icloud.com>
2025-10-15 18:25:00 +0800

    Update tailwind.config.js

--- zova-dev/tailwind.config.js
+++ zova-dev/tailwind.config.js
@@ -1,32 +1,7 @@
-import typography from '@tailwindcss/typography';
-import daisyui from 'daisyui';
-import themes from 'daisyui/src/theming/themes';
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
-  },
-  plugins: [typography, daisyui],
-  daisyui: {
-    themes: [
-      'light',
-      'dark',
-      {
-        'orange-light': {
-          ...themes.light,
-          'primary': '#f28238',
-          'base-100': '#ffffff',
-        },
-      },
-      {
-        'orange-dark': {
-          ...themes.dark,
-          'primary': '#f28238',
-          'base-100': '#121212',
-        },
-      },
-    ],
   },
 };
