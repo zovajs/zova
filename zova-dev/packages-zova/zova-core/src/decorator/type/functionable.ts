213655817a2eae9b97479b5dd094f28b9ea660d2
zhennann <zhen.nann@icloud.com>
2025-10-19 17:52:23 +0800

    feat: update

--- zova-dev/packages-zova/zova-core/src/decorator/type/functionable.ts
+++ zova-dev/packages-zova/zova-core/src/decorator/type/functionable.ts
@@ -1,5 +1,7 @@
 export type Functionable<T = void> = (...args: any[]) => T;
-export type FunctionAsync<RESULT = void> = () => Promise<RESULT>;
-export type Next = (data?: any) => Promise<any>;
-export type NextSync = (data?: any) => any;
-export type NextGeneral = (data?: any) => Promise<any> | any;
+export type FunctionAsync<RESULT> = () => Promise<RESULT>;
+export type Next<DATA = any, RESULT = any> = (data?: DATA) => Promise<RESULT>;
+export type NextSync<DATA = any, RESULT = any> = (data?: DATA) => RESULT;
+export type NextGeneral<DATA = any, RESULT = any> = (data?: DATA) => Promise<RESULT> | RESULT;
+export const functionNoop = () => {};
+export type FunctionAny = (...args: any[]) => any;
--- zova-dev/src/suite-vendor/a-zova/modules/a-bean/src/types/aop.ts
+++ zova-dev/src/suite-vendor/a-zova/modules/a-bean/src/types/aop.ts
@@ -1,6 +1,31 @@
-import type { IBeanRecord, OmitNever } from 'zova';
+import type { IBeanRecord, Next, NextSync, OmitNever } from 'zova';
 import type { ServiceOnion } from '../service/onion_.js';
 import type { IOnionOptionsDeps, IOnionOptionsEnable, IOnionOptionsMatch } from './onion';
+
+export type AopActionNext<P, R> = R extends Promise<any> ? Next<P, Awaited<R>> : NextSync<P, R>;
+
+// @ts-ignore ignore
+export type AopActionInit<T extends {}> = AopAction<T, '__init__'>;
+// @ts-ignore ignore
+export type AopActionDispose<T extends {}> = AopAction<T, '__dispose__'>;
+
+export type AopAction<T extends {}, NAME extends keyof T, RESULT = undefined> =
+  // @ts-ignore ignore
+  (args: Parameters<T[NAME]>, next: AopActionNext<Parameters<T[NAME]>, ReturnType<T[NAME]>>, _receiver: T)
+  // @ts-ignore ignore
+  => RESULT extends undefined ? ReturnType<T[NAME]> : ReturnType<T[NAME]> extends Promise<any> ? Promise<RESULT> : RESULT;
+
+export type AopActionGetter<T extends {}, NAME extends keyof T, RESULT = undefined> =
+  // @ts-ignore ignore
+  (next: AopActionNext<void, T[NAME]>, _receiver: T)
+  // @ts-ignore ignore
+  => RESULT extends undefined ? T[NAME] : T[NAME] extends Promise<any> ? Promise<RESULT> : RESULT;
+
+export type AopActionSetter<T extends {}, NAME extends keyof T, DATA = undefined> =
+  // @ts-ignore ignore
+  (value: DATA extends undefined ? T[NAME] : T[NAME] extends Promise<any> ? Promise<DATA> : DATA, next: AopActionNext<T[NAME], boolean>, _receiver: T)
+  // @ts-ignore ignore
+  => boolean;
 
 export interface IAopRecord {}
 
