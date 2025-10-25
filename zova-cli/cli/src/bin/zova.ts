32666afd1ec05d2c89eccc4925bac671d3fd09e1
zhennann <zhen.nann@icloud.com>
2025-10-25 10:59:40 +0800

    feat: update

--- zova-cli/cli/src/bin/zova.ts
+++ zova-cli/cli/src/bin/zova.ts
@@ -3,17 +3,43 @@
 import path from 'node:path';
 import { ProcessHelper } from '@cabloy/process-helper';
 import fse from 'fs-extra';
+import semver from 'semver';
+
+const pnpm_version = '10.19.0';
 
 const processHelper = new ProcessHelper(process.cwd());
 
-let args: string[] = [];
-// bootstrapFile
-let bootstrapFile = path.join(import.meta.dirname, '../bootstrap.ts');
-if (!fse.existsSync(bootstrapFile)) {
-  bootstrapFile = path.join(import.meta.dirname, '../bootstrap.js');
-}
-args.push(bootstrapFile);
-const rawArgv = process.argv.slice(2);
-args = args.concat(rawArgv);
+checkPnpm().then(() => {
+  main();
+});
 
-processHelper.spawnCmd({ cmd: 'tsx', args });
+async function checkPnpm() {
+  const res = await processHelper.spawnCmd({
+    cmd: 'pnpm',
+    args: ['--version'],
+    options: {
+      stdio: 'pipe',
+      shell: true,
+      dummy: true,
+    },
+  });
+  const version = res.trimEnd();
+  const lt = semver.lt(version, pnpm_version);
+  if (lt) {
+    throw new Error(`pnpm should >= ${pnpm_version}, current: ${version}`);
+  }
+}
+
+function main() {
+  let args: string[] = [];
+  // bootstrapFile
+  let bootstrapFile = path.join(import.meta.dirname, '../bootstrap.ts');
+  if (!fse.existsSync(bootstrapFile)) {
+    bootstrapFile = path.join(import.meta.dirname, '../bootstrap.js');
+  }
+  args.push(bootstrapFile);
+  const rawArgv = process.argv.slice(2);
+  args = args.concat(rawArgv);
+
+  processHelper.spawnCmd({ cmd: 'tsx', args });
+}
