32695d35153d805d29692959df57ec6ef233e81e
zhennann <zhen.nann@icloud.com>
2025-11-03 19:54:22 +0800

    feat: update

--- zova-dev/src/suite/a-home/modules/home-user/src/page/login/render.tsx
+++ zova-dev/src/suite/a-home/modules/home-user/src/page/login/render.tsx
@@ -1,4 +1,4 @@
-import { withModifiers } from 'vue';
+import type { IBehaviorPropsOutputFormFieldModel, TypeFormField } from 'zova-module-a-form';
 import { BeanRenderBase } from 'zova';
 import { Render } from 'zova-module-a-bean';
 import { ZForm, ZFormField } from 'zova-module-a-form';
@@ -38,72 +38,72 @@ export class RenderPageLogin extends BeanRenderBase {
 
   _renderForm() {
     return (
-      <ZForm data={this.user}>
-        <ZFormField name="username">
-          <label class="input input-bordered flex items-center gap-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 16 16"
-              fill="currentColor"
-              class="h-4 w-4 opacity-70"
-            >
-              <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
-            </svg>
-            <input
-              type="text"
-              class="grow"
-              placeholder={this.scope.locale.YourUsername()}
-              v-model={this.user.username}
-            />
-          </label>
-        </ZFormField>
-      </ZForm>
-    );
-  }
-
-  _renderForm2() {
-    return (
-      <form
-        onSubmit={withModifiers(() => {
-          this.login();
-        }, ['prevent'])}
+      <ZForm
+        data={this.user}
+        formProvider={{ behaviors: { formFieldLayout: 'home-user:formFieldLayoutLogin' } }}
+        onSubmit={data => {
+          return this.onSubmitLogin(data);
+        }}
       >
-        <label class="input input-bordered flex items-center gap-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            class="h-4 w-4 opacity-70"
-          >
-            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
-          </svg>
-          <input
-            type="text"
-            class="grow"
-            placeholder={this.scope.locale.YourUsername()}
-            v-model={this.user.username}
-          />
-        </label>
-        <label class="input input-bordered flex items-center gap-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            class="h-4 w-4 opacity-70"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-              clip-rule="evenodd"
-            />
-          </svg>
-          <input type="password" class="grow" v-model={this.user.password} />
-        </label>
-
+        <ZFormField name="username">
+          {
+            (_fields: TypeFormField, props: IBehaviorPropsOutputFormFieldModel) => {
+              return (
+                <>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 16 16"
+                    fill="currentColor"
+                    class="h-4 w-4 opacity-70"
+                  >
+                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+                  </svg>
+                  <input
+                    type="text"
+                    class="grow"
+                    placeholder={this.scope.locale.YourUsername()}
+                    name={props.name}
+                    value={props.value}
+                    onInput={props.onInput}
+                  />
+                </>
+              );
+            }
+          }
+        </ZFormField>
+        <ZFormField name="password">
+          {
+            (_fields: TypeFormField, props: IBehaviorPropsOutputFormFieldModel) => {
+              return (
+                <>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 16 16"
+                    fill="currentColor"
+                    class="h-4 w-4 opacity-70"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                  <input
+                    type="password"
+                    class="grow"
+                    name={props.name}
+                    value={props.value}
+                    onInput={props.onInput}
+                  />
+                </>
+              );
+            }
+          }
+        </ZFormField>
         <button type="submit" class="btn mt-2 w-full btn-primary">
           {this.scope.locale.Login()}
         </button>
-      </form>
+      </ZForm>
     );
   }
 }
