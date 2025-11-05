5a76c5c97b118a60deb2daa2bf301657c2c3a5d9
zhennann <zhen.nann@icloud.com>
2025-11-05 18:41:19 +0800

    Create button.vue

---
+++ zova-dev/src/legacy/components/button.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const props = defineProps({
+  onPerform: Function,
+});
+const emits = defineEmits<{
+  (e: 'click', message: string): void;
+}>();
+function onClick() {
+  props.onPerform?.('onPerform');
+  emits('click', 'onClick');
+}
+</script>
+
+<template>
+  <button class="btn btn-primary" @click="onClick">
+    Click
+  </button>
+</template>
