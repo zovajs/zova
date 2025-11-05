fd56196b00d54892c473f97f982c3cd4be548296
zhennann <zhen.nann@icloud.com>
2025-11-05 11:28:46 +0800

    feat: icon

--- zova-dev/src/suite-vendor/a-zova/modules/a-icon/src/lib/utils.ts
+++ zova-dev/src/suite-vendor/a-zova/modules/a-icon/src/lib/utils.ts
@@ -1,14 +1,10 @@
 import type { VNode } from 'vue';
 import type { IIconRecord } from '../types/icon.js';
 import { createVNode } from 'vue';
-import { ZovaIcon } from './zovaIcon.js';
+import { ZIcon } from '../.metadata/index.js';
 
-// should not provide name?: string
-// export function iconh(name?: string, color?: string, size?: string | number);
-export function iconh<K extends keyof IIconRecord>(name: K, color?: string, size?: string | number): VNode;
-export function iconh(name?, color?, size?): VNode {
-  size = size ?? 24;
-  return createVNode(ZovaIcon, {
+export function $iconh<K extends keyof IIconRecord>(name: K, color?: string, size?: string | number): VNode {
+  return createVNode(ZIcon, {
     name,
     color,
     width: size,
