c9bbc37ba8c067300cd9cde61d0054451d1ee94d
zhennann <zhen.nann@icloud.com>
2025-11-11 16:34:53 +0800

    Update controller.tsx

--- zova-dev/src/suite/a-home/modules/home-user/src/component/captcha/controller.tsx
+++ zova-dev/src/suite/a-home/modules/home-user/src/component/captcha/controller.tsx
@@ -1,15 +1,20 @@
+import z from 'zod';
 import { BeanControllerBase } from 'zova';
 import { Controller } from 'zova-module-a-bean';
 import { ZFormField } from 'zova-module-a-form';
 
 @Controller()
 export class ControllerCaptcha extends BeanControllerBase {
-  protected async __init__() {}
+  zodSchema: z.ZodString;
+
+  protected async __init__() {
+    this.zodSchema = z.string();
+  }
 
   protected render() {
     return (
       <>
-        <ZFormField name="captcha"></ZFormField>
+        <ZFormField name="captcha.token" validators={{ onDynamic: this.zodSchema }}></ZFormField>
       </>
     );
   }
