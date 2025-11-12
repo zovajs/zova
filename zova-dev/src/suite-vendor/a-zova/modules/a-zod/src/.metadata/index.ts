d7b7e162bf13d0724beafc7c80095ea1ba6016e3
zhennann <zhen.nann@icloud.com>
2025-11-12 15:19:06 +0800

    feat: update

--- zova-dev/src/suite-vendor/a-zova/modules/a-zod/src/.metadata/index.ts
+++ zova-dev/src/suite-vendor/a-zova/modules/a-zod/src/.metadata/index.ts
@@ -1,6 +1,45 @@
 /* eslint-disable */
+/** tool: begin */
+export * from '../bean/tool.v.js';
+
+import 'zova';
+declare module 'zova' {
+  
+  
+}
+declare module 'zova-module-a-zod' {
+  
+        export interface ToolV {
+          /** @internal */
+          get scope(): ScopeModuleAZod;
+        }
+
+        export interface ToolV {
+          get $beanFullName(): 'a-zod.tool.v';
+          get $onionName(): 'a-zod:v';
+          
+        } 
+}
+/** tool: end */
+/** tool: begin */
+import { ToolV } from '../bean/tool.v.js';
+import 'zova';
+declare module 'zova' {
+  export interface IBeanRecordGeneral {
+    'a-zod.tool.v': ToolV;
+  }
+}
+/** tool: end */
+/** locale: begin */
+import locale_en_us from '../config/locale/en-us.js';
+import locale_zh_cn from '../config/locale/zh-cn.js';
+export const locales = {
+  'en-us': locale_en_us,
+'zh-cn': locale_zh_cn,
+};
+/** locale: end */
 /** scope: begin */
-import { BeanScopeBase, type BeanScopeUtil } from 'zova';
+import { BeanScopeBase, type BeanScopeUtil, TypeModuleLocales, TypeLocaleBase } from 'zova';
 import { Scope } from 'zova-module-a-bean';
 
 @Scope()
@@ -8,6 +47,7 @@ export class ScopeModuleAZod extends BeanScopeBase {}
 
 export interface ScopeModuleAZod {
   util: BeanScopeUtil;
+locale: TypeModuleLocales<(typeof locales)[TypeLocaleBase]>;
 }
 
 import 'zova';
@@ -18,9 +58,14 @@ declare module 'zova' {
   
   
 
-  
+  export interface IBeanScopeLocale {
+    'a-zod': (typeof locales)[TypeLocaleBase];
+  }
 
   
 }
-  
+
+export function locale<K extends keyof (typeof locales)[TypeLocaleBase]>(key: K): `a-zod::${K}` {
+  return `a-zod::${key}`;
+}  
 /** scope: end */
--- zova-dev/src/suite/a-home/modules/home-user/src/component/captcha/controller.tsx
+++ zova-dev/src/suite/a-home/modules/home-user/src/component/captcha/controller.tsx
@@ -1,14 +1,18 @@
 import z from 'zod';
-import { BeanControllerBase } from 'zova';
+import { BeanControllerBase, Use } from 'zova';
 import { Controller } from 'zova-module-a-bean';
 import { ZFormField } from 'zova-module-a-form';
+import { ToolV } from 'zova-module-a-zod';
 
 @Controller()
 export class ControllerCaptcha extends BeanControllerBase {
   zodSchema: z.ZodString;
 
+  @Use()
+  $$v: ToolV;
+
   protected async __init__() {
-    this.zodSchema = z.string();
+    this.zodSchema = this.$$v.required(z.string());
   }
 
   protected render() {
