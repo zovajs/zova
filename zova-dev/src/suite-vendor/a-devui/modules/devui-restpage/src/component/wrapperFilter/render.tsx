b57c334cfeef0a1dc75f5dfc0fe66e37b32e9ce9
zhennann <zhen.nann@icloud.com>
2025-11-26 20:03:32 +0800

    Update render.tsx

--- zova-dev/src/suite-vendor/a-devui/modules/devui-restpage/src/component/wrapperFilter/render.tsx
+++ zova-dev/src/suite-vendor/a-devui/modules/devui-restpage/src/component/wrapperFilter/render.tsx
@@ -6,14 +6,16 @@ export class RenderWrapperFilter extends BeanRenderBase {
   public render() {
     const ComponentForm = this.$zovaComponent(this.$$restResource.componentForm);
     return (
-      <ComponentForm
-        controllerRef={ref => { this.controllerForm = ref; }}
-        data={this.$props.formData}
-        schema={this.schema}
-        formMeta={this.formMeta}
-        formProvider={this.$props.formProvider}
-        onSubmit={data => this.onSubmit(data)}
-      ></ComponentForm>
+      <>
+        <ComponentForm
+          controllerRef={ref => { this.controllerForm = ref; }}
+          data={this.$props.formData}
+          schema={this.schema}
+          formMeta={this.formMeta}
+          formProvider={this.$props.formProvider}
+          onSubmit={data => this.onSubmit(data)}
+        ></ComponentForm>
+      </>
     );
   }
 }
