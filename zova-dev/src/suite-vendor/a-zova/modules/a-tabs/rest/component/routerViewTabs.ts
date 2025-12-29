4f9c07d4207600bc35694a6f9da2dce4b4f564f2
zhennann <zhen.nann@icloud.com>
2025-12-29 22:47:40 +0800

    feat: update

---
+++ zova-dev/src/suite-vendor/a-zova/modules/a-tabs/rest/component/routerViewTabs.ts
@@ -0,0 +1,5 @@
+import type { ControllerRouterViewTabsProps } from '../../src/component/routerViewTabs/controller.jsx';
+
+export function ZATabsRouterViewTabs(_props: ControllerRouterViewTabsProps) {
+  return 'a-tabs:routerViewTabs';
+}
