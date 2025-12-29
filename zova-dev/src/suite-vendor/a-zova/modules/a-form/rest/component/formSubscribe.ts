413ceb3c5a20324355308d2e0410aed7ee8b1a38
zhennann <zhen.nann@icloud.com>
2025-12-29 22:29:35 +0800

    feat: update

---
+++ zova-dev/src/suite-vendor/a-zova/modules/a-form/rest/component/formSubscribe.ts
@@ -0,0 +1,5 @@
+import type { ControllerFormSubscribeProps } from '../../src/component/formSubscribe/controller.jsx';
+
+export function ZAFormFormSubscribe<TFormData extends {} = {}, TSubmitMeta = never>(_props: ControllerFormSubscribeProps<TFormData,TSubmitMeta>) {
+  return 'a-form:formSubscribe';
+}
