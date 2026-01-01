b2cdcadef3787ed7633c44d11e323b6e01d79628
zhennann <zhen.nann@icloud.com>
2026-01-01 11:54:39 +0800

    feat: update

--- zova-dev/src/suite-vendor/a-zova/modules/a-ssrhmr/src/monkeySys.ts
+++ zova-dev/src/suite-vendor/a-zova/modules/a-ssrhmr/src/monkeySys.ts
@@ -4,11 +4,9 @@ import { BeanSimple } from 'zova';
 export class MonkeySys extends BeanSimple implements IMonkeySysReady, IMonkeySysClose {
   async sysReady(): Promise<void> {
     if (this.sys.env.SSR_HMR !== 'true') return;
-    console.log('-----------ssr hmr ready', new Date());
   }
 
   async sysClose(): Promise<void> {
     if (this.sys.env.SSR_HMR !== 'true') return;
-    console.log('-----------ssr hmr close', new Date());
   }
 }
