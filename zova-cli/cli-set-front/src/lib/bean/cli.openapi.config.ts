cc1c3098b4fe713d338740c2f537aacf4d5941df
zhennann <zhen.nann@icloud.com>
2026-01-02 21:21:43 +0800

    Update cli.openapi.config.ts

--- zova-cli/cli-set-front/src/lib/bean/cli.openapi.config.ts
+++ zova-cli/cli-set-front/src/lib/bean/cli.openapi.config.ts
@@ -49,23 +49,5 @@ export class CliOpenapiConfig extends BeanCliBase {
       snippetsPath: null,
       boilerplatePath: 'openapi/config/boilerplate/module',
     });
-    // files: cli
-    await this._setPackageInfo(targetDir);
-  }
-
-  async _setPackageInfo(modulePath: string) {
-    const pkgFile = path.join(modulePath, 'package.json');
-    const pkg = await this.helper.loadJSONFile(pkgFile);
-    if (!pkg.files) pkg.files = [];
-    let changed;
-    // cli
-    if (!pkg.files.includes('cli')) {
-      pkg.files.push('cli');
-      changed = true;
-    }
-    // save
-    if (changed) {
-      await this.helper.saveJSONFile(pkgFile, pkg);
-    }
   }
 }
