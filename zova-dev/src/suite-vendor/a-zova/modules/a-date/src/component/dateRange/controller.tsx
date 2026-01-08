39fecad10cfb020ca8883045eaf25dc6b27e82b4
zhennann <zhen.nann@icloud.com>
2026-01-08 21:29:37 +0800

    feat: update

--- zova-dev/src/suite-vendor/a-zova/modules/a-date/src/component/dateRange/controller.tsx
+++ zova-dev/src/suite-vendor/a-zova/modules/a-date/src/component/dateRange/controller.tsx
@@ -6,7 +6,7 @@ export interface ControllerDateRangeProps {
 }
 
 export interface ControllerDateRangeModels {
-  vModel?: string;
+  vModel?: string | undefined;
 }
 
 @Controller()
@@ -18,7 +18,7 @@ export class ControllerDateRange extends BeanControllerBase {
   private cSeparator: string;
 
   @ModelValue()
-  modelValue: string;
+  modelValue: string | undefined;
 
   protected async __init__() {
     this.cSeparator = this.$style({
@@ -29,12 +29,41 @@ export class ControllerDateRange extends BeanControllerBase {
   }
 
   protected render() {
+    const [dateStartStr, dateEndStr] = this._parseValue(this.modelValue);
     return (
       <div>
-        <input style={{ width: '130px' }} type="date" />
+        <input
+          style={{ width: '130px' }}
+          type="date"
+          value={dateStartStr}
+          onInput={e => {
+            const value = (e.target as HTMLInputElement).value;
+            this.modelValue = this._combineValue(value, dateEndStr);
+            console.log(this.modelValue);
+          }}
+        />
         <div class={this.cSeparator}>~</div>
-        <input style={{ width: '130px' }} type="date" />
+        <input
+          style={{ width: '130px' }}
+          type="date"
+          value={dateEndStr}
+          onInput={e => {
+            const value = (e.target as HTMLInputElement).value;
+            this.modelValue = this._combineValue(dateStartStr, value);
+            console.log(this.modelValue);
+          }}
+        />
       </div>
     );
+  }
+
+  _parseValue(value?: string) {
+    if (!value) return [];
+    return value.split(this.$props.separator);
+  }
+
+  _combineValue(dateStartStr: string | undefined, dateEndStr: string | undefined) {
+    if (!dateStartStr && !dateEndStr) return undefined;
+    return `${dateStartStr ?? ''}${this.$props.separator}${dateEndStr ?? ''}`;
   }
 }
