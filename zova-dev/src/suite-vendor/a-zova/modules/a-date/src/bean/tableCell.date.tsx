98e9256dc47c9df8bbc6a5c21a22426754b7a599
zhennann <zhen.nann@icloud.com>
2026-01-08 20:30:20 +0800

    Update tableCell.date.tsx

--- zova-dev/src/suite-vendor/a-zova/modules/a-date/src/bean/tableCell.date.tsx
+++ zova-dev/src/suite-vendor/a-zova/modules/a-date/src/bean/tableCell.date.tsx
@@ -1,3 +1,4 @@
+import { DateTime } from 'luxon';
 import { BeanBase } from 'zova';
 import { TypeDateFormat } from 'zova-module-a-openapi';
 import { IDecoratorTableCellOptions, ITableCellRender, ITableCellRenderContext, NextTableCellRender, TableCell } from 'zova-module-a-table';
@@ -6,9 +7,21 @@ export interface ITableCellOptionsDate extends IDecoratorTableCellOptions {
   dateFormat?: TypeDateFormat;
 }
 
-@TableCell<ITableCellOptionsDate>()
+@TableCell<ITableCellOptionsDate>({
+  dateFormat: { preset: 'DATETIME_SHORT' },
+})
 export class TableCellDate extends BeanBase implements ITableCellRender {
-  render(_renderContext: ITableCellRenderContext, _options: ITableCellOptionsDate, next: NextTableCellRender) {
-    return next();
+  render(_renderContext: ITableCellRenderContext, options: ITableCellOptionsDate, next: NextTableCellRender) {
+    const value = next();
+    if (!value) return;
+    const dateFormat = options.dateFormat;
+    if (!dateFormat) return value;
+    const datetime = DateTime.fromJSDate(value);
+    if (typeof dateFormat === 'string') {
+      return datetime.toFormat(dateFormat);
+    } else if (typeof dateFormat === 'object' && dateFormat.preset) {
+      return datetime.toLocaleString(DateTime[dateFormat.preset]);
+    }
+    return value;
   }
 }
