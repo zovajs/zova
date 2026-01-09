6c00bd830437481d67a883a5d51be75e0728d53d
zhennann <zhen.nann@icloud.com>
2026-01-09 12:07:33 +0800

    Update formFieldDateRange.ts

--- zova-dev/src/suite-vendor/a-zova/modules/a-date/src/.metadata/component/formFieldDateRange.ts
+++ zova-dev/src/suite-vendor/a-zova/modules/a-date/src/.metadata/component/formFieldDateRange.ts
@@ -21,5 +21,5 @@ export const ZFormFieldDateRange = defineComponent(
     useController(ControllerFormFieldDateRange, undefined, undefined);
     return () => {};
   },
-  prepareComponentOptions(ControllerFormFieldDateRange.$componentOptions),
+  prepareComponentOptions(),
 );
