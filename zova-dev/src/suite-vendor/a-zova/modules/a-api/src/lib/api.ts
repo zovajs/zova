970c79a58985ecb6062d5be3335edfeb3a60b6e6
zhennann <zhen.nann@icloud.com>
2026-01-10 20:03:44 +0800

    feat: update

--- zova-dev/src/suite-vendor/a-zova/modules/a-api/src/lib/api.ts
+++ zova-dev/src/suite-vendor/a-zova/modules/a-api/src/lib/api.ts
@@ -3,3 +3,7 @@ import { createBeanDecorator } from 'zova';
 export function Api(): ClassDecorator {
   return createBeanDecorator('api', 'app');
 }
+
+export function ApiMeta(): ClassDecorator {
+  return createBeanDecorator('apiMeta', 'app');
+}
