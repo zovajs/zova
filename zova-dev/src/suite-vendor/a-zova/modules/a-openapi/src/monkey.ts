73cc45595498ad3e32cdd549ecd3ff357f92bf8e
zhennann <zhen.nann@icloud.com>
2026-01-11 00:33:10 +0800

    Update monkey.ts

--- zova-dev/src/suite-vendor/a-zova/modules/a-openapi/src/monkey.ts
+++ zova-dev/src/suite-vendor/a-zova/modules/a-openapi/src/monkey.ts
@@ -1,11 +1,13 @@
 import type { IModule } from '@cabloy/module-info';
+import type { Ref } from 'vue';
 import type { BeanBase, BeanContainer, IMonkeyBeanInit, IMonkeyModule } from 'zova';
 import type { ModelSdk } from './model/sdk.js';
+import { ref, watch } from 'vue';
 import { BeanSimple } from 'zova';
 
 export class Monkey extends BeanSimple implements IMonkeyModule, IMonkeyBeanInit {
   private _moduleSelf: IModule;
-  private _modelSdk: ModelSdk;
+  private _modelSdk: Ref<ModelSdk | undefined> = ref();
 
   constructor(moduleSelf: IModule) {
     super();
@@ -16,7 +18,14 @@ export class Monkey extends BeanSimple implements IMonkeyModule, IMonkeyBeanInit
   async moduleLoaded(module: IModule) {
     // self
     if (this._moduleSelf === module) {
-      this._modelSdk = await this.app.bean._getBeanSelector('a-openapi.model.sdk', true, this.app.meta.locale.current);
+      await this._loadSdk();
+      this.ctx.util.instanceScope(() => {
+        return watch(() => {
+          return this.app.meta.locale.current;
+        }, async () => {
+          await this._loadSdk();
+        });
+      });
     }
   }
 
@@ -30,5 +39,9 @@ export class Monkey extends BeanSimple implements IMonkeyModule, IMonkeyBeanInit
         return self._modelSdk;
       },
     });
+  }
+
+  private async _loadSdk() {
+    this._modelSdk.value = await this.app.bean._getBeanSelector('a-openapi.model.sdk', true, this.app.meta.locale.current);
   }
 }
