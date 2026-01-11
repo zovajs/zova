549d8e4b223cf8ef8672838f8a157de03c4487e3
zhennann <zhen.nann@icloud.com>
2026-01-11 12:46:28 +0800

    Update captcha.ts

--- zova-dev/src/suite/a-home/modules/home-api/src/apiSchema/captcha.ts
+++ zova-dev/src/suite/a-home/modules/home-api/src/apiSchema/captcha.ts
@@ -1,22 +1,10 @@
-import { BeanBase, Use, usePrepareArg } from "zova";
-import { ApiSchema } from "zova-module-a-api";
-import { getSchemaNameOfRequestBody, ModelSdk } from "zova-module-a-openapi";
-import { ApiApiCaptchacreatePath } from "../api/captcha.js";
+import { ApiSchema } from 'zova-module-a-api';
+import { BeanApiSchemaBase } from 'zova-module-a-openapi';
+import { ApiApiCaptchacreatePath } from '../api/captcha.js';
 
 @ApiSchema()
-export class ApiSchemaCaptcha extends BeanBase {
-  @Use({ beanFullName: 'a-openapi.model.sdk' })
-  get $$modelSdk(): ModelSdk {
-    return usePrepareArg(
-      this.app.meta.locale.current,
-      true,
-    );
-  }
-  
+export class ApiSchemaCaptcha extends BeanApiSchemaBase {
   get create() {
-    const querySdk= this.$$modelSdk.getSdk(ApiApiCaptchacreatePath,'post');
-    const schemaName=getSchemaNameOfRequestBody(querySdk?.data?.operationObject);
-    if(!schemaName) return;
-    return this.$$modelSdk.getSchema(schemaName)
+    return this.$createApiSchemas(ApiApiCaptchacreatePath, 'post');
   }
 }
