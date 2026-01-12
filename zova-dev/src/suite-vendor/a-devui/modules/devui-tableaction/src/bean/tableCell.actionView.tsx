dbde3fb0e5473bb96c438d50b68bd23d8eb9d130
zhennann <zhen.nann@icloud.com>
2026-01-12 20:17:35 +0800

    Update tableCell.actionView.tsx

--- zova-dev/src/suite-vendor/a-devui/modules/devui-tableaction/src/bean/tableCell.actionView.tsx
+++ zova-dev/src/suite-vendor/a-devui/modules/devui-tableaction/src/bean/tableCell.actionView.tsx
@@ -1,11 +1,15 @@
 import { BeanBase } from 'zova';
 import { IDecoratorTableCellOptions, ITableCellRender, ITableCellRenderContext, NextTableCellRender, TableCell } from 'zova-module-a-table';
 
-export interface ITableCellOptionsActionView extends IDecoratorTableCellOptions {}
+export interface ITableCellOptionsActionView extends IDecoratorTableCellOptions {
+  openTarget: 'popup' | 'page';
+}
 
-@TableCell<ITableCellOptionsActionView>()
+@TableCell<ITableCellOptionsActionView>({
+  openTarget: 'popup',
+})
 export class TableCellActionView extends BeanBase implements ITableCellRender {
-  render(renderContext: ITableCellRenderContext, _options: ITableCellOptionsActionView, next: NextTableCellRender) {
+  render(renderContext: ITableCellRenderContext, options: ITableCellOptionsActionView, next: NextTableCellRender) {
     const { $$table, cellContext } = renderContext;
     const value = next();
     return (
@@ -13,8 +17,16 @@ export class TableCellActionView extends BeanBase implements ITableCellRender {
         class="hover:text-blue-500"
         href="#"
         onClick={e => {
+          e.preventDefault();
           e.stopPropagation();
-          $$table.onActionRow('view', cellContext.row);
+          if (options.openTarget === 'page') {
+            const url = $$table.$router.getPagePath('/rest/resource/:resource/:id/:formScene?', {
+              params: { resource: 'test-rest:product', id: cellContext.row.id },
+            });
+            $$table.$router.push(url);
+          } else {
+            $$table.onActionRow('view', cellContext.row);
+          }
         }}
       >
         {value}
