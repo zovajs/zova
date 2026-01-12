f934b11c5070f86dda8b19b896132cd995e93fae
zhennann <zhen.nann@icloud.com>
2026-01-12 09:33:05 +0800

    Update utils.ts

--- zova-dev/src/suite-vendor/a-zova/modules/a-model/src/lib/utils.ts
+++ zova-dev/src/suite-vendor/a-zova/modules/a-model/src/lib/utils.ts
@@ -12,21 +12,26 @@ export async function $QueriesAutoLoad<T1 = any, T2 = any, T3 = any, T4 = any, T
   fn3?: TypeQueryAutoLoadFn<T3>,
   fn4?: TypeQueryAutoLoadFn<T4>,
   fn5?: TypeQueryAutoLoadFn<T5>,
-) {
-  const promises = [
+  ...fns: any[]
+): Promise<[
+DataQuery<T1> | undefined,
+DataQuery<T2> | undefined,
+DataQuery<T3> | undefined,
+DataQuery<T4> | undefined,
+DataQuery<T5> | undefined,
+...any[],
+  ]> {
+  let promises: any[] = [
     _QueryAutoLoadInner<T1>(fn1),
     _QueryAutoLoadInner<T2>(fn2),
     _QueryAutoLoadInner<T3>(fn3),
     _QueryAutoLoadInner<T4>(fn4),
     _QueryAutoLoadInner<T5>(fn5),
   ];
-  return await Promise.all(promises) as [
-    DataQuery<T1> | undefined,
-DataQuery<T2> | undefined,
-DataQuery<T3> | undefined,
-DataQuery<T4> | undefined,
-DataQuery<T5> | undefined,
-  ];
+  if (fns.length > 0) {
+    promises = promises.concat(fns.map(fn => _QueryAutoLoadInner(fn)));
+  }
+  return await Promise.all(promises) as any;
 }
 
 async function _QueryAutoLoadInner<T = any>(fn?: TypeQueryAutoLoadFn<T>) {
