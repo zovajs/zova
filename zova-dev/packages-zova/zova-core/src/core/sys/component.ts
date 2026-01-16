5e313c4ef451a14c652ec6b3c94eacc717acd2c0
zhennann <zhen.nann@icloud.com>
2026-01-16 22:10:44 +0800

    Update component.ts

--- zova-dev/packages-zova/zova-core/src/core/sys/component.ts
+++ zova-dev/packages-zova/zova-core/src/core/sys/component.ts
@@ -5,9 +5,9 @@ export class SysComponent extends BeanSimple {
   /** @internal */
   public async initialize() {}
 
-  public createAsyncComponent(componentName: string) {
+  public createAsyncComponent(module: string, name?: string) {
     return () => {
-      return this.use(componentName);
+      return this.use(module, name);
     };
     // should not use defineAsyncComponent
     // return defineAsyncComponent(() => {
@@ -15,9 +15,13 @@ export class SysComponent extends BeanSimple {
     // });
   }
 
-  public async use(componentName: string): Promise<Component> {
-    const [moduleName, componentName2] = componentName.split(':');
-    const module = await this.sys.meta.module.use(moduleName);
-    return module.resource.components[componentName2];
+  public async use(module: string, name?: string): Promise<Component> {
+    if (module.includes(':')) {
+      const parts = module.split(':');
+      module = parts[0];
+      name = parts[1];
+    }
+    const _module = await this.sys.meta.module.use(module);
+    return _module.resource.components[name!];
   }
 }
