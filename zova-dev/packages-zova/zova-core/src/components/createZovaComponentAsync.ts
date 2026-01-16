5fcf51690033bc3a4096f0281a50fd2bd388ba01
zhennann <zhen.nann@icloud.com>
2026-01-16 22:04:49 +0800

    Update createZovaComponentAsync.ts

--- zova-dev/packages-zova/zova-core/src/components/createZovaComponentAsync.ts
+++ zova-dev/packages-zova/zova-core/src/components/createZovaComponentAsync.ts
@@ -1,17 +1,17 @@
 import type { ZovaApplication } from '../core/app/application.js';
 import { defineAsyncComponent } from 'vue';
-import { useApp } from '../composables/useApp.js';
+import { useSys } from '../composables/useSys.js';
 
 export function createZovaComponentAsync(module: string, name?: string) {
   return defineAsyncComponent(() => {
     return new Promise(resolve => {
-      const app = useApp();
+      const sys = useSys();
       if (module.includes(':')) {
         const parts = module.split(':');
         module = parts[0];
         name = parts[1];
       }
-      app.meta.module.use(module).then(_module => {
+      sys.meta.module.use(module).then(_module => {
         resolve(_module.resource.components[name!] as any);
       });
     });
