1836a4bf1b06b5ff1ae6b986577754e3f3f380c5
zhennann <zhen.nann@icloud.com>
2026-01-19 18:48:00 +0800

    feat: update

--- zova-dev/src/suite-vendor/a-devui/modules/devui-actions/src/.metadata/index.ts
+++ zova-dev/src/suite-vendor/a-devui/modules/devui-actions/src/.metadata/index.ts
@@ -1,39 +1,4 @@
 /* eslint-disable */
-/** action: begin */
-export * from '../bean/action.view.jsx';
-import { IActionOptionsView } from '../bean/action.view.jsx';
-import 'zova-module-a-action';
-declare module 'zova-module-a-action' {
-  
-    export interface IActionRecord {
-      'devui-actions:view': IActionOptionsView;
-    }
-
-  
-}
-declare module 'zova-module-devui-actions' {
-  
-        export interface ActionView {
-          /** @internal */
-          get scope(): ScopeModuleDevuiActions;
-        }
-
-        export interface ActionView {
-          get $beanFullName(): 'devui-actions.action.view';
-          get $onionName(): 'devui-actions:view';
-          get $onionOptions(): IActionOptionsView;
-        } 
-}
-/** action: end */
-/** action: begin */
-import { ActionView } from '../bean/action.view.jsx';
-import 'zova';
-declare module 'zova' {
-  export interface IBeanRecordGeneral {
-    'devui-actions.action.view': ActionView;
-  }
-}
-/** action: end */
 /** scope: begin */
 import { BeanScopeBase, type BeanScopeUtil } from 'zova';
 import { Scope } from 'zova-module-a-bean';
