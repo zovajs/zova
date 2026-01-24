05b58f60fa04a8f7e0eaba0191140b38889332fb
zhennann <zhen.nann@icloud.com>
2026-01-24 21:21:44 +0800

    Update controller.tsx

--- zova-dev/src/suite-vendor/a-devui/modules/devui-restpageentry/src/component/restPageEntry/controller.tsx
+++ zova-dev/src/suite-vendor/a-devui/modules/devui-restpageentry/src/component/restPageEntry/controller.tsx
@@ -1,18 +1,52 @@
-import type { ControllerPageEntry } from 'zova-module-rest-resource';
+import type { ControllerPageEntry, ModelResource } from 'zova-module-rest-resource';
+import { isNil } from '@cabloy/utils';
+import { SchemaObject } from 'openapi3-ts/oas31';
 import { classes } from 'typestyle';
 import { BeanControllerBase, Use } from 'zova';
 import { Controller } from 'zova-module-a-bean';
 import { ControllerForm } from 'zova-module-a-form';
+import { $QueriesAutoLoad } from 'zova-module-a-model';
 import { ZWrapperForm } from '../../.metadata/index.js';
 
 @Controller()
 export class ControllerRestPageEntry extends BeanControllerBase {
   controllerForm: ControllerForm;
 
+  formSchema?: SchemaObject;
+  formData?: any;
+
   @Use({ injectionScope: 'host' })
   $$restPageEntry: ControllerPageEntry;
 
-  protected async __init__() {}
+  @Use({ injectionScope: 'host' })
+  $$modelResource: ModelResource;
+
+  protected async __init__() {
+    this.formSchema = this.$useComputed(() => {
+      return this.$$modelResource.getFormSchema(this.formMeta);
+    });
+    this.formData = this.$useComputed(() => {
+      return this.$$modelResource.getFormData(this.formMeta, this.entryId);
+    });
+    // load schema/data
+    await $QueriesAutoLoad(
+      () => this.$$modelResource.getFormApiSchemas(this.formMeta)?.sdk,
+      () => this.queryData,
+    );
+  }
+
+  get entryId() {
+    return this.$$restPageEntry.entryId;
+  }
+
+  get formMeta() {
+    return this.$$restPageEntry.formMeta;
+  }
+
+  get queryData() {
+    if (isNil(this.entryId)) return;
+    return this.$$modelResource.view(this.entryId);
+  }
 
   protected render() {
     const { formMeta } = this.$$restPageEntry;
