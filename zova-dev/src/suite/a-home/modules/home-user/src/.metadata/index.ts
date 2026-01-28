c561053705a1e506acfcd572509a6357e07f06bf
zhennann <zhen.nann@icloud.com>
2026-01-28 11:44:43 +0800

    feat: update

--- zova-dev/src/suite/a-home/modules/home-user/src/.metadata/index.ts
+++ zova-dev/src/suite/a-home/modules/home-user/src/.metadata/index.ts
@@ -1,39 +1,4 @@
 /* eslint-disable */
-/** model: begin */
-export * from '../model/passport.js';
-import { IModelOptionsPassport } from '../model/passport.js';
-import 'zova-module-a-model';
-declare module 'zova-module-a-model' {
-  
-    export interface IModelRecord {
-      'home-user:passport': IModelOptionsPassport;
-    }
-
-  
-}
-declare module 'zova-module-home-user' {
-  
-        export interface ModelPassport {
-          /** @internal */
-          get scope(): ScopeModuleHomeUser;
-        }
-
-        export interface ModelPassport {
-          get $beanFullName(): 'home-user.model.passport';
-          get $onionName(): 'home-user:passport';
-          get $onionOptions(): IModelOptionsPassport;
-        } 
-}
-/** model: end */
-/** model: begin */
-import { ModelPassport } from '../model/passport.js';
-import 'zova';
-declare module 'zova' {
-  export interface IBeanRecordGeneral {
-    'home-user.model.passport': ModelPassport;
-  }
-}
-/** model: end */
 /** controller: begin */
 export * from '../component/formFieldCaptcha/controller.jsx';
 export * from '../page/login/controller.jsx';
@@ -186,15 +151,11 @@ declare module 'vue/jsx-runtime' {
   }
 }
 /** behaviors: end */
-/** config: begin */
-export * from '../config/config.js';
-import { config } from '../config/config.js';
-/** config: end */
 /** locale: begin */
 import { locales } from './locales.js';
 /** locale: end */
 /** scope: begin */
-import { BeanScopeBase, type BeanScopeUtil, TypeModuleConfig, TypeModuleLocales, TypeLocaleBase } from 'zova';
+import { BeanScopeBase, type BeanScopeUtil, TypeModuleLocales, TypeLocaleBase } from 'zova';
 import { Scope } from 'zova-module-a-bean';
 
 @Scope()
@@ -202,7 +163,6 @@ export class ScopeModuleHomeUser extends BeanScopeBase {}
 
 export interface ScopeModuleHomeUser {
   util: BeanScopeUtil;
-config: TypeModuleConfig<typeof config>;
 locale: TypeModuleLocales<(typeof locales)[TypeLocaleBase]>;
 }
 
@@ -212,9 +172,7 @@ declare module 'zova' {
     'home-user': ScopeModuleHomeUser;
   }
   
-  export interface IBeanScopeConfig {
-    'home-user': ReturnType<typeof config>;
-  }
+  
 
   export interface IBeanScopeLocale {
     'home-user': (typeof locales)[TypeLocaleBase];
