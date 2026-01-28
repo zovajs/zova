4d0e2190781b93f96f245f61b3e39889e40d53b8
zhennann <zhen.nann@icloud.com>
2026-01-28 11:41:28 +0800

    Update jwtAdapter.ts

--- zova-dev/src/suite/a-home/modules/home-api/src/service/jwtAdapter.ts
+++ zova-dev/src/suite/a-home/modules/home-api/src/service/jwtAdapter.ts
@@ -1,20 +1,16 @@
 import type { IJwtAdapter, IJwtInfo } from 'zova-module-a-interceptor';
-import { BeanBase, Use } from 'zova';
+import { BeanBase } from 'zova';
 import { Service } from 'zova-module-a-bean';
-import { ModelPassport } from 'zova-module-home-user';
 
 @Service()
 export class ServiceJwtAdapter extends BeanBase implements IJwtAdapter {
-  @Use()
-  $$modelPassport: ModelPassport;
-
   protected async __init__() {}
 
   async getJwtInfo(): Promise<IJwtInfo | undefined> {
-    return await this.$$modelPassport.getJwtInfo();
+    return await this.$passport.getJwtInfo();
   }
 
   async refreshAuthToken(refreshToken: string): Promise<IJwtInfo> {
-    return await this.$$modelPassport.refreshAuthToken(refreshToken);
+    return await this.$passport.refreshAuthToken(refreshToken);
   }
 }
