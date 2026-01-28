2ad7aa366a3d98bda421b3cd7d6e2580f4e4fc28
zhennann <zhen.nann@icloud.com>
2026-01-28 22:06:19 +0800

    Update sdk.ts

--- zova-dev/src/suite-vendor/a-zova/modules/a-openapi/src/model/sdk.ts
+++ zova-dev/src/suite-vendor/a-zova/modules/a-openapi/src/model/sdk.ts
@@ -3,8 +3,10 @@ import { cast, ILocaleRecord, TypeEventOff, Use, usePrepareArg } from 'zova';
 import { BeanModelBase, IDecoratorModelOptions, Model } from 'zova-module-a-model';
 import { SysSdk } from '../bean/sys.sdk.js';
 import { getSchemaOfRequestBody, getSchemaOfRequestQuery, getSchemaOfRequestQueryFilter, getSchemaOfResponseBody, schemaToZodSchema } from '../lib/schema.js';
+import { IOpenapiPermissions } from '../types/permissions.js';
 import { IOpenapiSchemas, TypeOpenapiSchemasSdk } from '../types/schema.js';
 import { TypeRequestMethod } from '../types/sdk.js';
+import { TypeOpenapiPermissions } from '../types/resourceMeta.js';
 
 const __schemaRefPrefix = '#/components/schemas/';
 
@@ -62,7 +64,7 @@ export class ModelSdk extends BeanModelBase {
         const resourceMeta = this.getBootstrap(resource);
         const permissions = resourceMeta.data?.resourceMeta.permissions;
         if (!isNil(permissions)) return permissions;
-        return await this.$api.homeBasePermission.retrievePermissions({ params: { resource } });
+        return await this.$api.homeBasePermission.retrievePermissions({ params: { resource } }) as unknown as TypeOpenapiPermissions;
       },
     });
   }
