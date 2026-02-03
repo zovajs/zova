9942ca0ab9ef50193f2c50f67861437d54e63c75
zhennann <zhen.nann@icloud.com>
2026-02-03 16:10:29 +0800

    feat: update

--- zova-ui-quasar/src/suite-vendor/a-quasar/modules/quasar-adapter/src/monkeySys.ts
+++ zova-ui-quasar/src/suite-vendor/a-quasar/modules/quasar-adapter/src/monkeySys.ts
@@ -1,6 +1,7 @@
 import type { IMonkeySysInitialize } from 'zova';
-import { BeanSimple } from 'zova';
+import { BeanSimple, deepExtend } from 'zova';
 import { SysIcon } from './bean/sys.icon.js';
+import { IOpenapiOptionsResourceMeta } from 'zova-module-a-openapi';
 
 export class MonkeySys extends BeanSimple implements IMonkeySysInitialize {
   async sysInitialize() {
@@ -12,5 +13,48 @@ export class MonkeySys extends BeanSimple implements IMonkeySysInitialize {
     // icon
     const localIcon = await this.bean._newBean(SysIcon, false);
     await localIcon.initialize();
+    // config custom
+    const configCustom: IOpenapiOptionsResourceMeta = {
+      provider: {
+        components: {
+          // restPage: 'devui-restpage:restPage',
+          // restPageEntry: 'devui-restpage:restPageEntry',
+          // table: 'devui-table:table',
+        },
+      },
+      form: {
+        provider: {
+          components: {
+            // captcha: 'devui-form:formFieldCaptcha',
+          },
+          behaviors: {
+            //  formFieldLayout: 'devui-form:formFieldLayout',
+          },
+        },
+      },
+      table: {
+        provider: {
+          components: {
+            // actionOperationsTable: 'devui-table:actionOperationsTable',
+            // actionOperationsRow: 'devui-table.tableCell.actionOperationsRow',
+            // actionView: 'devui-table.tableCell.actionView',
+          },
+          actions: {
+            // actionCreate: 'rest-actions:create',
+            // actionView: 'rest-actions:view',
+            // actionEdit: 'rest-actions:edit',
+            // actionDelete: 'rest-actions:delete',
+          },
+        },
+      },
+    };
+    // rest
+    const scopeRestConfig = this.sys.util.getModuleConfigSafe('a-openapi');
+    scopeRestConfig.resourceMeta = deepExtend(
+      {},
+      scopeRestConfig.base,
+      configCustom,
+      scopeRestConfig.resourceMeta,
+    );
   }
 }
