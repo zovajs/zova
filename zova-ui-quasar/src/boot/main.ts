08b04feb197093fab972629288dbb1bb7aaf0d1f
zhennann <zhen.nann@icloud.com>
2026-02-07 21:16:41 +0800

    Update main.ts

--- zova-ui-quasar/src/boot/main.ts
+++ zova-ui-quasar/src/boot/main.ts
@@ -1,10 +1,10 @@
 import { defineBoot } from '@quasar/app-vite/wrappers';
-import { cast } from 'zova';
 import { Quasar } from 'quasar';
+import { cast } from 'zova';
 import 'quasar/dist/quasar.sass';
 import '../css/app.scss';
 
-export default defineBoot(({app, ssrContext}) => {
+export default defineBoot(({ app, ssrContext }) => {
   const quasarUserOptions = {
     config: {
       dark: process.env.CLIENT ? window.ssr_themedark : 'auto',
