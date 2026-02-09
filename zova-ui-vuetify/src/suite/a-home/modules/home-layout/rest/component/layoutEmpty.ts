b17d69b6639e6b71216e2e1fe355920b0d8f2c9f
zhennann <zhen.nann@icloud.com>
2026-02-09 21:24:56 +0800

    feat: update

---
+++ zova-ui-vuetify/src/suite/a-home/modules/home-layout/rest/component/layoutEmpty.ts
@@ -0,0 +1,8 @@
+import type { TypeRenderComponentJsxPropsPublic } from 'zova-jsx';
+
+type TypeControllerLayoutEmptyPublicProps = TypeRenderComponentJsxPropsPublic;
+export function ZZHomeLayoutEmpty(
+  _props: TypeControllerLayoutEmptyPublicProps,
+) {
+  return 'home-layout:layoutEmpty';
+}
