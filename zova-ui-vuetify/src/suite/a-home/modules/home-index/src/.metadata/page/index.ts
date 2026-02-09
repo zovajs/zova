35af353489321b268cf83d871c9c1eada5f4280a
zhennann <zhen.nann@icloud.com>
2026-02-09 21:11:35 +0800

    feat: update

--- zova-ui-vuetify/src/suite/a-home/modules/home-index/src/.metadata/page/index.ts
+++
@@ -1,4 +0,0 @@
-import { createZovaComponentPage } from 'zova';
-import { ControllerPageIndex } from '../../page/index/controller.jsx';
-
-export const ZPageIndex = createZovaComponentPage(ControllerPageHome, undefined, undefined);
