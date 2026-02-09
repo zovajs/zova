f2b49e48ec64d2b7438072a3651793db1b6b50d9
zhennann <zhen.nann@icloud.com>
2026-02-09 21:02:20 +0800

    feat: update

--- zova-ui-vuetify/src/suite/a-home/modules/home-base/src/monkeySys.ts
+++ zova-ui-vuetify/src/suite/a-home/modules/home-base/src/monkeySys.ts
@@ -1,6 +1,9 @@
-import type { IMonkeySysInitialize } from 'zova';
+import type { BeanBase, BeanContainer, IMonkeyBeanInit } from 'zova';
 import { BeanSimple } from 'zova';
+import { definePropertyScopeBase } from './lib/utils.js';
 
-export class MonkeySys extends BeanSimple implements IMonkeySysInitialize {
-  async sysInitialize() {}
+export class MonkeySys extends BeanSimple implements IMonkeyBeanInit {
+  async beanInit(bean: BeanContainer, beanInstance: BeanBase) {
+    definePropertyScopeBase(bean, beanInstance);
+  }
 }
