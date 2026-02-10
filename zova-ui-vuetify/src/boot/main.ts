5907ca128908f41775cdec3e25e847006a703783
zhennann <zhen.nann@icloud.com>
2026-02-10 21:47:34 +0800

    feat: update

--- zova-ui-vuetify/src/boot/main.ts
+++ zova-ui-vuetify/src/boot/main.ts
@@ -1,6 +1,5 @@
 import { defineBoot } from '@quasar/app-vite/wrappers';
 import vuetify from './vuetify.js';
-import '../css/settings.scss';
 
 export default defineBoot(({ app, ssrContext: _ssrContext }) => {
   app.use(vuetify);
