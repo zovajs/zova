19f0d59f720e384336e8324a465a6cfec44f8ccf
zhennann <zhen.nann@icloud.com>
2026-02-22 09:56:03 +0800

    feat: update

--- zova-ui-quasar/src/suite-vendor/a-zova/modules/a-currency/src/component/formFieldCurrency/controller.tsx
+++ zova-ui-quasar/src/suite-vendor/a-zova/modules/a-currency/src/component/formFieldCurrency/controller.tsx
@@ -13,22 +13,40 @@ export class ControllerFormFieldCurrency extends BeanControllerBase {
   static $propsDefault = {};
   static $componentOptions: IComponentOptions = { inheritAttrs: false };
 
+  private _valueKeyboardInput: string | undefined;
+
   protected async __init__() {}
 
   protected render() {
     const currencyOptions = this.$props.currency;
-    const displayValue = currencyFormat(this.$props.displayValue, currencyOptions);
+    const displayValue = this._displayValuePatch(currencyOptions);
     return (
       <ZFormField
         {...this.$props}
         render="text"
         displayValue={displayValue}
-        displayValueUpdateTiming="change"
+        displayValueUpdateTiming="input"
         onSetDisplayValue={value => {
+          this._valueKeyboardInput = value;
           const valuePatch = currencyUpdate(value, currencyOptions);
           return valuePatch !== undefined ? valuePatch : value;
         }}
       ></ZFormField>
     );
+  }
+
+  private _displayValuePatch(currencyOptions?: CurrencyOptions) {
+    if (this._valueKeyboardInput === undefined) return this._getDisplayValue(currencyOptions);
+    // hold the current input value if invalid
+    const valueInputPatch = currencyUpdate(this._valueKeyboardInput, currencyOptions);
+    if (valueInputPatch === undefined) return this._valueKeyboardInput;
+    // hold the current input value if equal
+    if (valueInputPatch === this.$props.displayValue) return this._valueKeyboardInput;
+    // default
+    return this._getDisplayValue(currencyOptions);
+  }
+
+  private _getDisplayValue(currencyOptions?: CurrencyOptions) {
+    return currencyFormat(this.$props.displayValue, currencyOptions);
   }
 }
