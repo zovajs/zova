a4b1e576c3c5cd1885b2e8aa2dcfeb99cd39073c
zhennann <zhen.nann@icloud.com>
2026-03-02 23:07:00 +0800

    Update routerGuards.ts

--- zova-ui-vuetify/src/suite/a-home/modules/home-base/src/service/routerGuards.ts
+++ zova-ui-vuetify/src/suite/a-home/modules/home-base/src/service/routerGuards.ts
@@ -7,7 +7,6 @@ import { BeanRouterGuardsBase } from 'zova-module-a-router';
 export class ServiceRouterGuards extends BeanRouterGuardsBase {
   protected onRouterGuards(router: BeanRouter) {
     router.beforeEach(async to => {
-      console.log(to);
       if (to.meta.requiresAuth !== false && !this.$passport.isAuthenticated) {
         const [_res, err] = await catchError(() => {
           return this.$passport.ensurePassport();
