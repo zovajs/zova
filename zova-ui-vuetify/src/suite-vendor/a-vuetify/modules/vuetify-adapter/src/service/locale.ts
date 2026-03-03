e401e919753c09ee39b76169c32cd6410f49b307
zhennann <zhen.nann@icloud.com>
2026-03-03 20:09:47 +0800

    feat: update

--- zova-ui-vuetify/src/suite-vendor/a-vuetify/modules/vuetify-adapter/src/service/locale.ts
+++ zova-ui-vuetify/src/suite-vendor/a-vuetify/modules/vuetify-adapter/src/service/locale.ts
@@ -1,19 +1,13 @@
-import { inject } from 'vue';
-import { LocaleSymbol } from 'vuetify/lib/composables/locale.mjs';
-import { en, zhHans } from 'vuetify/locale';
-import { BeanBase } from 'zova';
+import { BeanBase, Use } from 'zova';
 import { Service } from 'zova-module-a-bean';
+import { ServiceLocale as ServiceLocaleB } from 'zova-module-home-base';
 
 @Service()
 export class ServiceLocale extends BeanBase {
+  @Use()
+  $$serviceLocale: ServiceLocaleB;
+
   public async initialize() {
-    if (!this.sys.config.locale.cookieLocale) return;
-    this.app.ctx.util.instanceScope(() => {
-      const locale = inject(LocaleSymbol);
-      if (locale) {
-        locale.messages.value = { 'en-us': en, 'zh-cn': zhHans };
-        locale.current.value = this.app.meta.locale.current;
-      }
-    });
+    this.$$serviceLocale.initVuetifyLocale();
   }
 }
---
+++ zova-ui-vuetify/src/suite/a-home/modules/home-base/src/service/locale.ts
@@ -0,0 +1,31 @@
+import { inject } from 'vue';
+import { LocaleSymbol } from 'vuetify/lib/composables/locale.mjs';
+import { en, zhHans } from 'vuetify/locale';
+import { BeanBase, ILocaleRecord } from 'zova';
+import { Service } from 'zova-module-a-bean';
+
+@Service()
+export class ServiceLocale extends BeanBase {
+  protected async __init__() {}
+
+  public initVuetifyLocale() {
+    if (!this.sys.config.locale.cookieLocale) return;
+    this.app.ctx.util.instanceScope(() => {
+      const localeVuetify = inject(LocaleSymbol);
+      if (localeVuetify) {
+        localeVuetify.messages.value = { 'en-us': en, 'zh-cn': zhHans };
+        localeVuetify.current.value = this.app.meta.locale.current;
+      }
+    });
+  }
+
+  public setLocale(locale: keyof ILocaleRecord) {
+    this.app.meta.locale.current = locale;
+    this.app.ctx.util.instanceScope(() => {
+      const localeVuetify = inject(LocaleSymbol);
+      if (localeVuetify) {
+        localeVuetify.current.value = locale;
+      }
+    });
+  }
+}
