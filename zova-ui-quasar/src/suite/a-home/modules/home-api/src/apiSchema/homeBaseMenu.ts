ee7735063c904d386b474cf36596a7d56f03bd41
zhennann <zhen.nann@icloud.com>
2026-03-05 11:03:18 +0800

    feat: update

--- zova-ui-quasar/src/suite/a-home/modules/home-api/src/apiSchema/homeBaseMenu.ts
+++ zova-ui-quasar/src/suite/a-home/modules/home-api/src/apiSchema/homeBaseMenu.ts
@@ -1,10 +1,10 @@
 import { BeanBase } from 'zova';
-import { ApiSchema } from 'zova-module-a-api';
+import { ApiSchema, IApiSchemaFetchOptions } from 'zova-module-a-api';
 import { ApiApiHomeBaseMenuretrieveMenusPath } from '../api/homeBaseMenu.js';
 
 @ApiSchema()
 export class ApiSchemaHomeBaseMenu extends BeanBase {
-  get retrieveMenus() {
-    return this.$sdk.createApiSchemas(ApiApiHomeBaseMenuretrieveMenusPath, 'get');
+  retrieveMenus(options?: IApiSchemaFetchOptions) {
+    return this.$sdk.createApiSchemas(ApiApiHomeBaseMenuretrieveMenusPath, 'get', options);
   }
 }
--- zova-ui-quasar/src/suite/a-home/modules/home-api/src/apiSchema/homeBasePermission.ts
+++ zova-ui-quasar/src/suite/a-home/modules/home-api/src/apiSchema/homeBasePermission.ts
@@ -1,10 +1,10 @@
 import { BeanBase } from 'zova';
-import { ApiSchema } from 'zova-module-a-api';
+import { ApiSchema, IApiSchemaFetchOptions } from 'zova-module-a-api';
 import { ApiApiHomeBasePermissionretrievePermissionsPath } from '../api/homeBasePermission.js';
 
 @ApiSchema()
 export class ApiSchemaHomeBasePermission extends BeanBase {
-  get retrievePermissions() {
-    return this.$sdk.createApiSchemas(ApiApiHomeBasePermissionretrievePermissionsPath, 'get');
+  retrievePermissions(options?: IApiSchemaFetchOptions) {
+    return this.$sdk.createApiSchemas(ApiApiHomeBasePermissionretrievePermissionsPath, 'get', options);
   }
 }
