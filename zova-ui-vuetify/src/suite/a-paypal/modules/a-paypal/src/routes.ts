56308d0fcedc2665ce7ff2315fc06efb00ba1f91
zhennann <zhen.nann@icloud.com>
2026-03-07 11:13:54 +0800

    feat: update

--- zova-ui-vuetify/src/suite/a-paypal/modules/a-paypal/src/routes.ts
+++
@@ -1,5 +0,0 @@
-import type { IModuleRoute } from 'zova-module-a-router';
-import { ZPagePaypalCancel } from './.metadata/page/paypalCancel.js';
-import { ZPagePaypalReturn } from './.metadata/page/paypalReturn.js';
-
-export const routes: IModuleRoute[] = [{ path: 'paypalReturn', component: ZPagePaypalReturn }, { path: 'paypalCancel', component: ZPagePaypalCancel }];
