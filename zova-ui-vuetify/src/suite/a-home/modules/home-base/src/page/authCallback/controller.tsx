27e6caf041bfd74fd16702e4070427067feda9f3
zhennann <zhen.nann@icloud.com>
2026-03-08 20:56:33 +0800

    Update controller.tsx

--- zova-ui-vuetify/src/suite/a-home/modules/home-base/src/page/authCallback/controller.tsx
+++ zova-ui-vuetify/src/suite/a-home/modules/home-base/src/page/authCallback/controller.tsx
@@ -3,12 +3,22 @@ import { BeanControllerPageBase } from 'zova';
 import { Controller } from 'zova-module-a-bean';
 
 export const ControllerPageAuthCallbackSchemaQuery = z.object({
-  returnTo: z.string().optional(),
+  'returnTo': z.string().optional(),
+  'x-vona-oauth-code': z.string().optional(),
 });
 
 @Controller()
 export class ControllerPageAuthCallback extends BeanControllerPageBase {
-  protected async __init__() {}
+  protected async __init__() {
+    if (process.env.CLIENT) {
+      // should not use await
+      this._handleAuth();
+    }
+  }
+
+  private _handleAuth() {
+
+  }
 
   protected render() {
     return null;
