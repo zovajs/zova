cb0b57a6d519b7e5470ee4004522dc7846b88dc3
zhennann <zhen.nann@icloud.com>
2026-03-14 11:33:50 +0800

    Update themeToken.ts

--- zova-ui-empty/src/suite/a-home/modules/home-base/src/types/themeToken.ts
+++ zova-ui-empty/src/suite/a-home/modules/home-base/src/types/themeToken.ts
@@ -1,18 +1,6 @@
 import 'zova-module-a-style';
 
-export interface ThemeTokenCustom {
-  color: {
-    'primary': string;
-    'secondary': string;
-    'accent': string;
-    'dark': string;
-    'dark-page': string;
-    'positive': string;
-    'negative': string;
-    'info': string;
-    'warning': string;
-  };
-}
+export interface ThemeTokenCustom {}
 
 declare module 'zova-module-a-style' {
   export interface ThemeToken extends ThemeTokenCustom {}
