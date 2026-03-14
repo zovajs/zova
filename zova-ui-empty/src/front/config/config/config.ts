89cfbbd6a52cf6012095ac4e20ac326507a1fbf6
zhennann <zhen.nann@icloud.com>
2026-03-14 11:42:08 +0800

    feat: update

--- zova-ui-empty/src/front/config/config/config.ts
+++ zova-ui-empty/src/front/config/config/config.ts
@@ -11,7 +11,6 @@ export default function (sys: ZovaSys) {
   config.routes = {
     path: {
       '/home/index': { alias: '/' },
-      '/home/login': { alias: '/login' },
     },
     name: {},
   };
