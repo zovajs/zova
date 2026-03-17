8438b8deb6784481d9eae4dd22a340afd39857f6
zhennann <zhen.nann@icloud.com>
2026-03-17 17:20:07 +0800

    feat: update

--- zova-ui-empty/src/suite-vendor/a-zova/modules/a-routerstack/rest/component/routerViewStack.ts
+++ zova-ui-empty/src/suite-vendor/a-zova/modules/a-routerstack/rest/component/routerViewStack.ts
@@ -1,8 +1,8 @@
 import type { TypeRenderComponentJsxPropsPublic } from 'zova-jsx';
 import type { ControllerRouterViewStackProps } from '../../src/component/routerViewStack/controller.jsx';
 
-type TypeControllerRouterViewStackPublicProps = TypeRenderComponentJsxPropsPublic &
-  ControllerRouterViewStackProps;
+type TypeControllerRouterViewStackPublicProps = TypeRenderComponentJsxPropsPublic
+  & ControllerRouterViewStackProps;
 export function ZZRouterstackRouterViewStack(
   _props: TypeControllerRouterViewStackPublicProps,
 ) {
--- zova-ui-quasar/src/suite-vendor/a-zova/modules/a-behavior/rest/component/behavior.ts
+++ zova-ui-quasar/src/suite-vendor/a-zova/modules/a-behavior/rest/component/behavior.ts
@@ -1,8 +1,8 @@
 import type { TypeRenderComponentJsxPropsPublic } from 'zova-jsx';
 import type { ControllerBehaviorProps } from '../../src/component/behavior/controller.jsx';
 
-type TypeControllerBehaviorPublicProps = TypeRenderComponentJsxPropsPublic &
-  ControllerBehaviorProps;
+type TypeControllerBehaviorPublicProps = TypeRenderComponentJsxPropsPublic
+  & ControllerBehaviorProps;
 export function ZZBehavior(
   _props: TypeControllerBehaviorPublicProps,
 ) {
