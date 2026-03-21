30dffde5d3e89ebdf0f61c80340b74c9e09cc866
zhennann <zhen.nann@icloud.com>
2026-03-21 19:59:26 +0800

    Update style.cjs

--- zova-cli/cli-set-front/cli/templates/refactor/anotherStyle/snippets/style.cjs
+++ zova-cli/cli-set-front/cli/templates/refactor/anotherStyle/snippets/style.cjs
@@ -1,8 +1,9 @@
+const __regUse = /import \{.*? Use[ ,].*?\} from 'zova';/;
 module.exports = {
   file: 'style.ts',
   language: 'plain',
   async transform({ ast, argv }) {
-    const res = ast.match(/import \{.*? Use[ ,].*?\} from 'zova';/);
+    const res = ast.match(__regUse);
     if (!res) {
       ast = ast.replace(" } from 'zova';", ", Use } from 'zova';");
     }
