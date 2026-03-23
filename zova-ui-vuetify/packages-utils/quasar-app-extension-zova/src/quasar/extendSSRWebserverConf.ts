2b8ba0ea388c9cd870adb003f6fb99f89fb728d9
zhennann <zhen.nann@icloud.com>
2026-03-23 21:24:25 +0800

    feat: update

--- zova-ui-vuetify/packages-utils/quasar-app-extension-zova/src/quasar/extendSSRWebserverConf.ts
+++ zova-ui-vuetify/packages-utils/quasar-app-extension-zova/src/quasar/extendSSRWebserverConf.ts
@@ -14,12 +14,22 @@ export function extendSSRWebserverConf(context: ConfigContext) {
     conf.bundle = true;
     conf.external = [];
     // conf.banner = { js: "import { createRequire } from 'module';const require = createRequire(import.meta.url);" };
-    conf.entryPoints = [
-      { in: (api.resolve as any).entry('ssr-prod-webserver.js'), out: 'index' },
-      { in: (api.resolve as any).entry('ssr-prod-handler.js'), out: 'handler' },
-    ];
+    // conf.entryPoints = [
+    //   { in: (api.resolve as any).entry('ssr-prod-webserver.js'), out: 'index' },
+    //   { in: (api.resolve as any).entry('ssr-prod-handler.js'), out: 'handler' },
+    // ];
     conf.outdir = getOutDir();
     delete conf.outfile;
+    // buildsPatch
+    (conf as any).buildsPatch = [
+      {
+        banner: { js: "import { createRequire } from 'module';const require = createRequire(import.meta.url);" },
+        entryPoints: [{ in: (api.resolve as any).entry('ssr-prod-webserver.js'), out: 'index' }],
+      },
+      {
+        entryPoints: [{ in: (api.resolve as any).entry('ssr-prod-handler.js'), out: 'handler' }],
+      },
+    ];
   };
 }
 
