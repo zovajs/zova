12dced5080de6108e71e8ad5bd2eb915cbe71538
zhennann <zhen.nann@icloud.com>
2026-03-23 16:59:19 +0800

    feat: update

--- zova-ui-vuetify/packages-utils/quasar-app-extension-zova/src/quasar/extendSSRWebserverConf.ts
+++ zova-ui-vuetify/packages-utils/quasar-app-extension-zova/src/quasar/extendSSRWebserverConf.ts
@@ -13,7 +13,7 @@ export function extendSSRWebserverConf(context: ConfigContext) {
     conf.keepNames = true;
     conf.bundle = true;
     conf.external = [];
-    conf.banner = { js: "import { createRequire } from 'module';const require = createRequire(import.meta.url);" };
+    // conf.banner = { js: "import { createRequire } from 'module';const require = createRequire(import.meta.url);" };
     conf.entryPoints = [
       { in: (api.resolve as any).entry('ssr-prod-webserver.js'), out: 'index' },
       { in: (api.resolve as any).entry('ssr-prod-handler.js'), out: 'handler' },
