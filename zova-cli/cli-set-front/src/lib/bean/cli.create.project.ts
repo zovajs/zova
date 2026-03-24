6acabe3003686ae5f171d7aabece81860fb07471
zhennann <zhen.nann@icloud.com>
2026-03-24 10:37:47 +0800

    feat: update

--- zova-cli/cli-set-front/src/lib/bean/cli.create.project.ts
+++ zova-cli/cli-set-front/src/lib/bean/cli.create.project.ts
@@ -47,15 +47,14 @@ export class CliCreateProject extends BeanCliBase {
     fse.copySync(templateDir, targetDir);
     // remove LICENSE
     fse.removeSync(path.join(targetDir, 'LICENSE'));
-    /// / copy package.json
-    // fse.copyFileSync(path.join(targetDir, 'package.original.json'), path.join(targetDir, 'package.json'));
-    // // render project boilerplate
-    // await this.template.renderBoilerplateAndSnippets({
-    //   targetDir,
-    //   setName: __ThisSetName__,
-    //   snippetsPath: null,
-    //   boilerplatePath: `create/project/${template}/boilerplate`,
-    // });
+    // copy package.json
+    fse.copyFileSync(path.join(targetDir, 'package.original.json'), path.join(targetDir, 'package.json'));
+    // npm run init
+    await this.helper.spawnCmd({
+      cmd: 'npm',
+      args: ['run', 'init'],
+      options: { cwd: targetDir },
+    });
     // done
     await this.printUsage(targetDir);
   }
