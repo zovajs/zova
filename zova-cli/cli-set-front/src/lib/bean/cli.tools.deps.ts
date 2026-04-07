e4471d3ff8709ad3ba09b2f6d49baa691e5d25f1
zhennann <zhen.nann@icloud.com>
2026-04-07 20:30:19 +0800

    Update cli.tools.deps.ts

--- zova-cli/cli-set-front/src/lib/bean/cli.tools.deps.ts
+++ zova-cli/cli-set-front/src/lib/bean/cli.tools.deps.ts
@@ -1,4 +1,6 @@
 import { BeanCliBase } from '@cabloy/cli';
+import fse from 'fs-extra';
+import path from 'node:path';
 
 declare module '@cabloy/cli' {
   interface ICommandArgv {
@@ -18,9 +20,19 @@ export class CliToolsDeps extends BeanCliBase {
   }
 
   async _generate(projectPath: string) {
+    await this._generate_preparePackage(projectPath);
     // generate package.json
     await this.common._generatePackageJson(projectPath);
     // generate type modules file
     await this.common._generateTypeModulesFile(projectPath);
+  }
+
+  async _generate_preparePackage(projectPath: string) {
+    // copy package.json
+    const filePackage = path.join(projectPath, 'package.json');
+    if (!fse.existsSync(filePackage)) {
+      fse.copyFileSync(path.join(projectPath, 'package.original.json'), filePackage);
+      await this.helper.pnpmInstall();
+    }
   }
 }
