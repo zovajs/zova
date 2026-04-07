a9aca86b254c6b05599f53193893c75d8a4c4069
zhennann <zhen.nann@icloud.com>
2026-04-07 21:56:13 +0800

    Update utils.ts

--- zova-dev/packages-utils/zova-vite/src/utils.ts
+++ zova-dev/packages-utils/zova-vite/src/utils.ts
@@ -76,7 +76,10 @@ export function getAbsolutePathOfModule(id: string, postfix: string = 'index.js'
   const require = createRequire(import.meta.url);
   let modulePath = require.resolve(id);
   if (postfix) {
-    const pos = modulePath.lastIndexOf(postfix);
+    let pos = modulePath.lastIndexOf(postfix);
+    if (pos === -1) {
+      pos = modulePath.lastIndexOf(postfix.replaceAll('/', '\\'));
+    }
     if (pos > -1) {
       modulePath = modulePath.substring(0, modulePath.length - postfix.length - 1);
     }
