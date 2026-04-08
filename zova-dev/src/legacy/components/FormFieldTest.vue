d3732b3f88dfec6cf9d838d03c8841b93fbfc9bd
zhennann <zhen.nann@icloud.com>
2026-04-08 21:39:11 +0800

    feat: update

--- zova-dev/src/legacy/components/FormFieldTest.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <slot name="header" />
-  <slot />
-  <slot name="footer" />
-</template>
--- zova-dev/src/legacy/routes.ts
+++
@@ -1,3 +0,0 @@
-import type { IModuleRoute } from 'zova-module-a-router';
-
-export const routes: IModuleRoute[] = [{ path: '/legacy/counter', component: () => import('./pages/counter.vue') }];
