8a60783cbb65d9392d415ad4aa83a0170f1d04e7
zhennann <zhen.nann@icloud.com>
2026-04-15 22:24:16 +0800

    Update render.header.tsx

--- zova-dev/src/suite/a-home/modules/home-layouttabs/src/component/layoutTabs/render.header.tsx
+++ zova-dev/src/suite/a-home/modules/home-layouttabs/src/component/layoutTabs/render.header.tsx
@@ -10,25 +10,28 @@ export class RenderHeader extends BeanRenderBase {
 
   public render() {
     return (
-      <div class="navbar bg-base-300 w-full">
-        <div class="flex-none">
-          <button class="btn btn-square btn-ghost">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block h-5 w-5 stroke-current">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
-            </svg>
-          </button>
+      <>
+        <div class="navbar bg-base-300 w-full">
+          <div class="flex-none">
+            <button class="btn btn-square btn-ghost">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block h-5 w-5 stroke-current">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+              </svg>
+            </button>
+          </div>
+          <div class="flex-1 text-xl px-4">{this.sys.env.APP_TITLE}</div>
+          <div class="mx-2 flex-1 px-2">{this.$$r.$$renderTabs.render()}</div>
+          <div class="hidden flex-none lg:block">
+            <ul class="menu menu-horizontal">
+              {this.$$r.$$renderLocale.render()}
+              {this.$$r.$$renderTheme.renderThemeDark()}
+              {this.$$r.$$renderTheme.renderThemeName()}
+              {this.$$r.$$renderUser.render()}
+            </ul>
+          </div>
         </div>
-        <div class="flex-1 text-xl px-4">{this.sys.env.APP_TITLE}</div>
-        <div class="mx-2 flex-1 px-2">{this.$$r.$$renderTabs.render()}</div>
-        <div class="hidden flex-none lg:block">
-          <ul class="menu menu-horizontal">
-            {this.$$r.$$renderLocale.render()}
-            {this.$$r.$$renderTheme.renderThemeDark()}
-            {this.$$r.$$renderTheme.renderThemeName()}
-            {this.$$r.$$renderUser.render()}
-          </ul>
-        </div>
-      </div>
+        <div class="navbar bg-base-300 w-full">{this.$$r.$$renderTabs.render()}</div>
+      </>
     );
   }
 }
