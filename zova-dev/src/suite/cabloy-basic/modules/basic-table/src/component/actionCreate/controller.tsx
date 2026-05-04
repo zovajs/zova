8e21a8db484ad9c31fb333b95d67fe466a3c0bc6
zhennann <zhen.nann@icloud.com>
2026-05-04 10:46:29 +0800

    Update controller.tsx

--- zova-dev/src/suite/cabloy-basic/modules/basic-table/src/component/actionCreate/controller.tsx
+++ zova-dev/src/suite/cabloy-basic/modules/basic-table/src/component/actionCreate/controller.tsx
@@ -1,9 +1,9 @@
-import type { IJsxRenderContextPage, IResourceActionBulkPresetOptionsBase } from 'zova-module-a-openapi';
+import type { IJsxRenderContextPage, IResourceActionBulkOptionsCreate } from 'zova-module-a-openapi';
 
 import { BeanControllerBase, IComponentOptions, Use } from 'zova';
 import { Controller } from 'zova-module-a-bean';
 
-export interface ControllerActionCreateProps extends IResourceActionBulkPresetOptionsBase {}
+export interface ControllerActionCreateProps extends IResourceActionBulkOptionsCreate {}
 
 @Controller()
 export class ControllerActionCreate extends BeanControllerBase {
@@ -19,11 +19,11 @@ export class ControllerActionCreate extends BeanControllerBase {
     const { $jsx } = this.$$renderContext;
     return (
       <button
-        class={this.$props.preset?.ActionCreate?.class ?? 'btn  btn-primary join-item'}
+        class={this.$props.class ?? 'btn  btn-primary join-item'}
         type="button"
         onClick={async () => {
           const actionName = $jsx.normalizeAction('ActionCreate');
-          await this.$performAction(actionName, this.$props.preset?.ActionCreate, this.$$renderContext);
+          await this.$performAction(actionName, this.$props, this.$$renderContext);
         }}
       >
         {this.scope.locale.Create()}
