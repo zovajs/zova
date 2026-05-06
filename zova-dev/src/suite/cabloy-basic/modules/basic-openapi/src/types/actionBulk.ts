afc280be35fe4377475798f73b7378f68ad176b0
zhennann <zhen.nann@icloud.com>
2026-05-06 09:30:20 +0800

    Update actionBulk.ts

--- zova-dev/src/suite/cabloy-basic/modules/basic-openapi/src/types/actionBulk.ts
+++ zova-dev/src/suite/cabloy-basic/modules/basic-openapi/src/types/actionBulk.ts
@@ -1,20 +1,14 @@
 import 'zova-module-a-openapi';
-import type { IResourceActionBulkOptionsBase, IResourceComponentActionBulkOptionsAction } from 'zova-module-a-openapi';
+import type { IResourceActionBulkOptionsBase } from 'zova-module-a-openapi';
 
 export interface IResourceActionBulkOptionsCreate extends IResourceActionBulkOptionsBase {}
-
-export interface IResourceActionBulkOptionsOperationsBulk extends IResourceActionBulkOptionsBase {
-  actions?: IResourceComponentActionBulkOptionsAction[];
-}
 
 declare module 'zova-module-a-openapi' {
   export interface IResourceActionBulkRecord {
     create?: IResourceActionBulkOptionsCreate;
-    operationsBulk?: IResourceActionBulkOptionsOperationsBulk;
   }
 
   export interface IResourceComponentActionBulkRecord {
     ActionCreate?: IResourceActionBulkOptionsCreate;
-    ActionOperationsBulk?: IResourceActionBulkOptionsOperationsBulk;
   }
 }
