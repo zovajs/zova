a998ee5f599183f208bd9e9996c942932b585d90
zhennann <zhen.nann@icloud.com>
2026-05-07 22:59:35 +0800

    Update behavior.formField.ts

--- zova-dev/src/suite/cabloy-basic/modules/basic-form/src/bean/behavior.formField.ts
+++ zova-dev/src/suite/cabloy-basic/modules/basic-form/src/bean/behavior.formField.ts
@@ -1,11 +1,8 @@
-import type { ControllerFormField, IFormFieldRenderContext, IFormFieldRenderContextProps, TypeFormField } from 'zova-module-a-form';
+import type { ControllerFormField, IFormFieldRenderContext } from 'zova-module-a-form';
 
-import { isEmptyObject } from '@cabloy/utils';
 import { VNode } from 'vue';
 import { Use } from 'zova';
-import { isJsxComponent } from 'zova-jsx';
 import { BeanBehaviorBase, Behavior, IDecoratorBehaviorOptions, NextBehavior } from 'zova-module-a-behavior';
-import { IFormMeta } from 'zova-module-a-openapi';
 
 export interface IBehaviorPropsInputFormField extends IFormFieldRenderContext {}
 
@@ -19,28 +16,40 @@ export class BehaviorFormField extends BeanBehaviorBase<IBehaviorOptionsFormFiel
   $$formField: ControllerFormField;
 
   protected render(renderContext: IFormFieldRenderContext, next: NextBehavior<IBehaviorPropsOutputFormField>): VNode {
-    this._patchProps(renderContext);
+    // this._patchProps(renderContext);
     return next(renderContext);
   }
 
-  private _patchProps(renderContext: IFormFieldRenderContext) {
-    const $$form = this.$$formField.$$form;
-    const formMeta = this.$$formField.formMeta;
-    const field = this.$$formField.field;
-    const needPatch = !isJsxComponent(renderContext.propsBucket.render) || $$form.isComponentFormField(renderContext.propsBucket.renderProvider);
-    if (!needPatch) return;
-    // propsPatch
-    const propsPatch = this._patchProps_general(formMeta, field, renderContext);
-    // merge
-    if (!isEmptyObject(propsPatch)) {
-      renderContext.props = Object.assign({}, propsPatch, renderContext.props);
-    }
-  }
+  // private _patchProps(renderContext: IFormFieldRenderContext) {
+  //   const $$form = this.$$formField.$$form;
+  //   const formMeta = this.$$formField.formMeta;
+  //   const field = this.$$formField.field;
+  //   const needPatch = !isJsxComponent(renderContext.propsBucket.render) || $$form.isComponentFormField(renderContext.propsBucket.renderProvider);
+  //   if (!needPatch) return;
+  //   // propsPatch
+  //   const propsPatch = this._patchProps_general(formMeta, field, renderContext);
+  //   // merge
+  //   if (!isEmptyObject(propsPatch)) {
+  //     renderContext.props = Object.assign({}, propsPatch, renderContext.props);
+  //   }
+  // }
 
-  private _patchProps_general(formMeta: IFormMeta | undefined, _field: TypeFormField, renderContext: IFormFieldRenderContext) {
-    const { propsBucket } = renderContext;
-    const propsPatch: IFormFieldRenderContextProps = {};
-
-    return propsPatch;
-  }
+  // private _patchProps_general(formMeta: IFormMeta | undefined, _field: TypeFormField, renderContext: IFormFieldRenderContext) {
+  //   const { propsBucket } = renderContext;
+  //   const propsPatch: IFormFieldRenderContextProps = {};
+  //   // class
+  //   const classTemp = (typeof propsBucket.render === 'string' && propsBucket.options?.class) ?? propsBucket.class;
+  //   const styleTemp = (typeof propsBucket.render === 'string' && propsBucket.options?.style) ?? propsBucket.style;
+  //   if (!isNil(classTemp) || !isNil(styleTemp)) {
+  //     propsPatch.class = classes(classTemp, this.$style(styleTemp));
+  //   }
+  //   // readonly
+  //   const readonlyTemp = (typeof propsBucket.render === 'string' && propsBucket.options?.readonly) ?? propsBucket.readonly;
+  //   if (!isNil(readonlyTemp)) {
+  //     propsPatch.readonly = readonlyTemp;
+  //   } else if (formMeta?.formMode === 'view') {
+  //     propsPatch.readonly = true;
+  //   }
+  //   return propsPatch;
+  // }
 }
