ba2779946e24a7c5e62cc70b1a0396db6b1a10b6
zhennann <zhen.nann@icloud.com>
2026-05-13 15:13:26 +0800

    feat: update

--- zova-ui-empty/src/suite/a-demo/modules/demo-basic/src/routes.ts
+++ zova-ui-empty/src/suite/a-demo/modules/demo-basic/src/routes.ts
@@ -4,6 +4,6 @@ import { ZPageComponent } from './.metadata/page/component.js';
 import { ZPageState } from './.metadata/page/state.js';
 
 export const routes: IModuleRoute[] = [
-  { path: 'state', component: ZPageState },
-  { path: 'component', component: ZPageComponent },
+  { path: 'state', component: ZPageState, meta: { requiresAuth: false } },
+  { path: 'component', component: ZPageComponent, meta: { requiresAuth: false } },
 ];
--- zova-ui-vuetify/src/suite/a-demo/modules/demo-basic/src/routes.ts
+++ zova-ui-vuetify/src/suite/a-demo/modules/demo-basic/src/routes.ts
@@ -5,7 +5,7 @@ import { ZPageState } from './.metadata/page/state.js';
 import { ZPageStyle } from './.metadata/page/style.js';
 
 export const routes: IModuleRoute[] = [
-  { path: 'state', component: ZPageState },
-  { path: 'component', component: ZPageComponent },
-  { path: 'style', component: ZPageStyle },
+  { path: 'state', component: ZPageState, meta: { requiresAuth: false } },
+  { path: 'component', component: ZPageComponent, meta: { requiresAuth: false } },
+  { path: 'style', component: ZPageStyle, meta: { requiresAuth: false } },
 ];
