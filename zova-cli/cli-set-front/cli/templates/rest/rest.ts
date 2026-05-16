1681b77124342bb0079fa8b26978013591e3e7b8
zhennann <zhen.nann@icloud.com>
2026-05-16 21:54:14 +0800

    Update rest.ts

--- zova-cli/cli-set-front/cli/templates/rest/rest.ts
+++ zova-cli/cli-set-front/cli/templates/rest/rest.ts
@@ -7,42 +7,61 @@ import { TypeSchemaOrderLevel } from 'zova-module-a-openapi';
 
 import { _generalSchemaRest, _order } from './inner.ts';
 
-export function schemaRenderLayout<T extends z.ZodType>(layoutOptions: ISchemaRenderComponentLayoutOptions, scene?: TypeFormSchemaScene) {
+export function schemaRenderLayout<T extends z.ZodType>(
+  layoutOptions: ISchemaRenderComponentLayoutOptions,
+  scene?: TypeFormSchemaScene,
+) {
   return function (schema: T): T {
     const options = { layout: layoutOptions };
     return _generalSchemaRest(schema, options, scene);
   };
 }
 
-export function schemaRenderVisible<T extends z.ZodType>(visible?: boolean, scene?: TypeSchemaScene) {
+export function schemaRenderVisible<T extends z.ZodType>(
+  visible: boolean = true,
+  scene?: TypeSchemaScene,
+) {
   return function (schema: T): T {
     const options = { visible };
     return _generalSchemaRest(schema, options, scene);
   };
 }
 
-export function schemaRenderReadonly<T extends z.ZodType>(readonly?: boolean, scene?: TypeSchemaScene) {
+export function schemaRenderReadonly<T extends z.ZodType>(
+  readonly: boolean = true,
+  scene?: TypeSchemaScene,
+) {
   return function (schema: T): T {
     const options = { readonly };
     return _generalSchemaRest(schema, options, scene);
   };
 }
 
-export function schemaRenderDisableNotifyChanged<T extends z.ZodType>(disableNotifyChanged?: boolean, scene?: TypeSchemaScene) {
+export function schemaRenderDisableNotifyChanged<T extends z.ZodType>(
+  disableNotifyChanged: boolean = true,
+  scene?: TypeSchemaScene,
+) {
   return function (schema: T): T {
     const options = { disableNotifyChanged };
     return _generalSchemaRest(schema, options, scene);
   };
 }
 
-export function schemaRenderFieldSource<T extends z.ZodType>(fieldSource: string, scene?: TypeSchemaScene) {
+export function schemaRenderFieldSource<T extends z.ZodType>(
+  fieldSource: string,
+  scene?: TypeSchemaScene,
+) {
   return function (schema: T): T {
     const options = { fieldSource };
     return _generalSchemaRest(schema, options, scene);
   };
 }
 
-export function schemaRenderOrder<T extends z.ZodType>(order: number, level?: TypeSchemaOrderLevel, scene?: TypeSchemaScene) {
+export function schemaRenderOrder<T extends z.ZodType>(
+  order: number,
+  level?: TypeSchemaOrderLevel,
+  scene?: TypeSchemaScene,
+) {
   const orderReal = _order(order, level);
   return function (schema: T): T {
     const options = { order: orderReal };
